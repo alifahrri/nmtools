e2b02858098aca7a8c95b12b27eba5a7ff0a1c7e
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-05-11 01:39:55 +0700

    add bisection

---
+++ include/roots/bisection.hpp
@@ -0,0 +1,42 @@
+#ifndef BISECTION_HPP
+#define BISECTION_HPP
+
+#include <cstdio>
+#include <cmath>
+
+namespace numeric {
+    namespace roots {
+        template <typename F, typename Scalar>
+        auto bisection(
+            F& f, 
+            Scalar xu, 
+            Scalar xl, 
+            Scalar es, 
+            Scalar &xr,
+            size_t imax, 
+            size_t &iter, 
+            Scalar &ea, 
+            Scalar zero = Scalar(1e-6)) 
+        {
+            iter = 0; ea = Scalar(100.0);
+            do {
+                auto xrold = xr;
+                xr = (xl + xu) / 2;
+                iter = iter + 1;
+                if((std::abs(xr) > zero) && (iter > 1)) {
+                    ea = std::abs((xr-xrold) / xr) * Scalar(100.0);
+                }
+                auto test = f(xl) * f(xr);
+                if(test < zero) {
+                    xu = xr;
+                } else if(test > zero) {
+                    xl = xr;
+                } else {
+                    ea = zero;
+                }
+            } while(std::abs(ea) > std::abs(es) && iter < imax);
+        }
+    }
+}
+
+#endif // BISECTION_HPP
