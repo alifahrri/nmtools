6f71b1e217e9540150861a6308b1e5e08d747f92
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-05-11 02:41:16 +0700

    add modified false position

--- tests/roots/bisection.cpp
+++ tests/roots/bisection.cpp
@@ -34,3 +34,20 @@ TEST(roots,false_position)
         << "iter = " << iter << "\n"
         << "ea = " << ea;
 }
+
+TEST(roots,modified_false_position)
+{
+    auto f = [](double x) {
+        return x*2 + 3;
+    };
+
+    double xu{10.0}, xl{-10.0}, es{5e-3}, xr{xu}, ea;
+    size_t imax{1000}, iter{0};
+
+    numeric::roots::modified_false_position(f, xu, xl, es, xr, imax, iter, ea);
+
+    EXPECT_NEAR(f(xr),0.0,es)
+        << "xr = " << xr << "\n"
+        << "iter = " << iter << "\n"
+        << "ea = " << ea;
+}
