edd86b1fad614d34a4a461eb2bdea7fc83387a0e
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-05-11 01:51:49 +0700

    add false position

--- include/roots/bisection.hpp
+++ include/roots/bisection.hpp
@@ -36,6 +36,37 @@ namespace numeric {
                 }
             } while(std::abs(ea) > std::abs(es) && iter < imax);
         }
+
+        template <typename F, typename Scalar>
+        auto false_position(
+            F& f, 
+            Scalar xu, 
+            Scalar xl, 
+            Scalar es, 
+            Scalar &xr,
+            size_t imax, 
+            size_t &iter, 
+            Scalar &ea, 
+            Scalar zero = Scalar(1e-6)) 
+        {
+            iter = 0; ea = Scalar(100.0);
+            do {
+                auto xrold = xr;
+                xr = xu - (f(xu) * (xl-xu)) / (f(xl)-f(xu));
+                iter = iter + 1;
+                if((std::abs(xr) > zero) && (iter > 1)) {
+                    ea = std::abs((xr-xrold) / xr) * Scalar(100.0);
+                }
+                auto test = f(xl) * f(xr);
+                if(test < zero) {
+                    xu = xr;
+                } else if(test > zero) {
+                    xl = xr;
+                } else {
+                    ea = zero;
+                }
+            } while(std::abs(ea) > std::abs(es) && iter < imax);
+        }
     }
 }
 
--- tests/roots/bisection.cpp
+++ tests/roots/bisection.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "roots/bisection.hpp"
 
-TEST(bisection,bisection)
+TEST(roots,bisection)
 {
     auto f = [](double x) {
         return x*2 + 3;
@@ -11,6 +11,23 @@ TEST(bisection,bisection)
     size_t imax{1000}, iter{0};
 
     numeric::roots::bisection(f, xu, xl, es, xr, imax, iter, ea);
+
+    EXPECT_NEAR(f(xr),0.0,es)
+        << "xr = " << xr << "\n"
+        << "iter = " << iter << "\n"
+        << "ea = " << ea;
+}
+
+TEST(roots,false_position)
+{
+    auto f = [](double x) {
+        return x*2 + 3;
+    };
+
+    double xu{10.0}, xl{-10.0}, es{5e-3}, xr{xu}, ea;
+    size_t imax{1000}, iter{0};
+
+    numeric::roots::false_position(f, xu, xl, es, xr, imax, iter, ea);
 
     EXPECT_NEAR(f(xr),0.0,es)
         << "xr = " << xr << "\n"
