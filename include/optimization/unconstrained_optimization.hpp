a0adfb721d56b89f5e51d053b62ce472af1c107f
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-05-18 04:43:24 +0700

    fix golden section

--- include/optimization/unconstrained_optimization.hpp
+++ include/optimization/unconstrained_optimization.hpp
@@ -7,16 +7,15 @@
 
 namespace numeric {
     namespace optimization {
-        template <typename F, typename Scalar, template<typename> Op = std::greater>
-        auto gold_section(F &f, Scalar xlow, Scalar xhigh, Scalar &xopt, size_t imax, size_t &iter, Scalar es, Scalar fx) {
+        template <typename F, typename Scalar, typename Op = typename std::greater<Scalar>>
+        auto gold_section(F &f, Scalar xlow, Scalar xhigh, Scalar &xopt, size_t imax, size_t &iter, Scalar es, Scalar &ea, Scalar &fx, const Op &op = Op{}) {
             constexpr Scalar R = (std::sqrt(Scalar(5))-1)/2;
             auto xl = xlow; auto xh = xhigh;
             iter = 1;
-            auto d = R * (xu - xl);
-            auto x1 = xl + d; auto x2 = xu - d;
-            auto f1 = f(x1);
-            auto f2 = f(x2);
-            if(Op(f1,f2)) {
+            auto d = R * (xh - xl);
+            auto x1 = xl + d; auto x2 = xh - d;
+            auto f1 = f(x1); auto f2 = f(x2);
+            if(op(f1,f2)) {
                 xopt = x1;
                 fx = f1;
             } else {
@@ -24,22 +23,18 @@ namespace numeric {
                 fx = f2;
             }
             do {
-                d = R * d; auto xint = xu -xl;
-                if(Op(f1,f2)) {
-                    xl = x2;
-                    x2 = x1;
+                d = R * d; auto xint = xh - xl;
+                if(op(f1,f2)) {
+                    xl = x2; x2 = x1;
                     x1 = xl + d;
-                    f2 = f1;
-                    f1 = f(x1);
+                    f2 = f1; f1 = f(x1);
                 } else {
-                    xu = x1;
-                    x1 = x2;
-                    x2 = xu - d;
-                    f1 = f2;
-                    f2 = f(x2);
+                    xh = x1; x1 = x2;
+                    x2 = xh - d;
+                    f1 = f2; f2 = f(x2);
                 }
                 iter = iter + 1;
-                if(Op(f1,f2)) {
+                if(op(f1,f2)) {
                     xopt = x1;
                     fx = f1;
                 } else {
