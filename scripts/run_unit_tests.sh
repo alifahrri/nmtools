3713427f41715e14125ef3d0061c9765b2bcedaf
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-06-08 07:09:18 +0700

    add docker

---
+++ scripts/run_unit_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
+
+## assuming this script is located in root/scripts & build directory in root/scripts
+$DIR/../build/numeric_tests
