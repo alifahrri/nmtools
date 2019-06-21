00e1851f394d5de5bd171b09a5beadffdf1aefaa
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-06-21 16:03:59 +0700

    add heun method. #8

--- tests/ode/euler.cpp
+++ tests/ode/euler.cpp
@@ -27,3 +27,16 @@ TEST(midpoint, test_0) {
     numeric::ode::midpoint(dydx,yi,xi,xf,dx,yf);
     EXPECT_NEAR(yf,3,1e-6);
 }
+
+TEST(heun, test_0) {
+    auto dydx = [](double x, double y){
+        return -2*std::pow(x,3) + 12*std::pow(x,2) - 20*x + 8.5;
+    };
+    double xi{0};
+    double yi{1};
+    double xf{4};
+    double dx{0.5};
+    double yf;
+    numeric::ode::heun(dydx,yi,xi,xf,dx,yf);
+    EXPECT_NEAR(yf,3,1e-6);
+}
