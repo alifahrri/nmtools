160089946b4d7de074d9e487fea3cd850ac3e761
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-06-21 15:51:19 +0700

    add midpoint. #8

--- include/ode/euler.hpp
+++ include/ode/euler.hpp
@@ -10,6 +10,14 @@ namespace numeric {
                 ynew = y + dydx*h;
                 x = x + h;
             }
+            template <typename DF, typename Scalar>
+            void midpoint(DF &df, Scalar &x, Scalar &y, Scalar h, Scalar &ynew) {
+                auto dydx = df(x,y);
+                auto ym = y + dydx*(h/2);
+                auto dymdx = df(x+h/2,ym);
+                ynew = y + dymdx*h;
+                x = x + h;
+            }
         } // namespace detail
 
         template <typename DF, typename Scalar>
@@ -24,6 +32,21 @@ namespace numeric {
                 }
                 typename std::decay<decltype(y)>::type ynew;
                 detail::euler(df,x,y,h,ynew);
+                y = ynew;
+            } while (x < xend);
+        }
+        template <typename DF, typename Scalar>
+        void midpoint(DF &df, Scalar yi, Scalar xi, Scalar xf, Scalar dx, Scalar &y) {
+            y = yi;
+            auto x = xi;
+            auto &h = dx;
+            const auto &xend = xf;
+            do {
+                if (xend - x < h) {
+                    h = xend - x;
+                }
+                typename std::decay<decltype(y)>::type ynew;
+                detail::midpoint(df,x,y,h,ynew);
                 y = ynew;
             } while (x < xend);
         }
