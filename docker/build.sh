f5bb2595475c5785e21d84042107ae47b4bf324e
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-07-12 00:44:24 +0700

    minor fix (build context)

--- docker/build.sh
+++ docker/build.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 
 ## assuming this script is located in root/scripts & Dockerfile in root/docker
-docker build -t numeric_tools -f ${DIR}/Dockerfile .
+docker build -t numeric_tools -f ${DIR}/Dockerfile ${DIR}/..
