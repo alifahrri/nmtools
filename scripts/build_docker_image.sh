d9934863aa9a132e493e3ebcbbf3d7241d4e7058
Fahri Ali Rahman <fahri@nodeflux.io>
2020-03-25 19:53:28 +0700

    remove unused

--- scripts/build_docker_image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
-
-## assuming this script is located in root/scripts & Dockerfile in root/docker
-docker build -t numeric_tools -f ${DIR}/../docker/Dockerfile .
--- scripts/debug_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
-
-## assuming this script is located in root/scripts & build directory in root/scripts
-gdb $DIR/../build/numeric_tests
