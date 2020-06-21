cc9f44728ea7ab4c5f677fc805a2e52e751738ab
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2020-06-21 15:19:53 +0700

    Assign from initializer list (#34)
    * add size op and initializer list assignment
    
    * add initializer list example and cleanup

--- examples/gla/src/dynamic.cpp
+++ examples/gla/src/dynamic.cpp
@@ -13,55 +13,75 @@ namespace matrix = gla::matrix;
 namespace eigen_backend = gla::backend::eigen::matrix;
 namespace arma_backend = gla::backend::armadillo::matrix;
 
+/* helper macro for printing */
+#define DESC(desc) { std::cout << desc << std::endl; }
+
 int main()
 {
-    std::cout << "creating 2x2 eigen matrix" << std::endl;
+    DESC("creating 2x2 eigen matrix");
     MatrixXd m(2,2);
     m(0,0) = 3;
     m(1,0) = 2.5;
     m(0,1) = -1;
     m(1,1) = m(1,0) + m(0,1);
     std::cout << m << std::endl;
-    std::cout << "resizing eigen matrix to 3x3" << std::endl;
+
+    DESC("resizing eigen matrix to 3x3");
     m.resize(3,3);
     std::cout << m.cols() << "x" << m.rows() << std::endl;
 
-    std::cout << "creating 2x2 armadillo matrix" << std::endl;
+    DESC("creating 2x2 armadillo matrix");
     arma::mat A(2,2);
     A(0,0) = 3;
     A(1,0) = 2.5;
     A(0,1) = -1;
     A(1,1) = A(1,0) + A(0,1);
     std::cout << A;
-    std::cout << "resizing eigen matrix to 3x3" << std::endl;
+    
+    DESC("resizing eigen matrix to 3x3");
     A.resize(3,3);
     std::cout << A.n_cols << "x" << A.n_rows << std::endl;
 
     std::vector<double> test{1.,2.,3.,4.};
     std::vector<double> test3{1.,2.,3.,4.,5.,6.,7.,8.,9.};
 
-    std::cout << "creating 2x2 gla matrix with eigen backend" << std::endl;
+    DESC("creating 2x2 gla matrix with eigen backend");
     eigen_backend::dynamic<double> a(2,2);
     std::cout << a.n_rows() << "x" << a.n_cols() << std::endl;
     std::cout << a << std::endl;
-    std::cout << "setting elements of 2x2 gla matrix with eigen backend" << std::endl;
+
+    DESC("setting elements of 2x2 gla matrix with eigen backend");
     a.set_elements(test);
     std::cout << a << std::endl;
-    std::cout << "resizing gla matrix with eigen backend to 3x3" << std::endl;
+
+    DESC("resizing gla matrix with eigen backend to 3x3");
     a.resize(3,3);
-    std::cout << "setting elements of 2x2 gla matrix with eigen backend" << std::endl;
+
+    DESC("setting elements of 2x2 gla matrix with eigen backend");
     std::cout << a.n_rows() << "x" << a.n_cols() << std::endl;
     a.set_elements(test3);
     
-    std::cout << "creating 2x2 gla matrix with armadillo backend" << std::endl;
+    DESC("creating 2x2 gla matrix with armadillo backend");
     arma_backend::dynamic<double> b(2,2);
     std::cout << b.n_rows() << "x" << b.n_cols() << std::endl;
     std::cout << b;
-    std::cout << "setting elements of 2x2 gla matrix with eigen backend" << std::endl;
+
+    DESC("setting elements of 2x2 gla matrix with eigen backend");
     b.set_elements(test);
     std::cout << b;
-    std::cout << "resizing gla matrix with armadillo backend to 3x3" << std::endl;
+
+    DESC("resizing gla matrix with armadillo backend to 3x3");
     b.resize(3,3);
     std::cout << b.n_rows() << "x" << b.n_cols() << std::endl;
     b.set_elements(test3);
+
+    DESC("set elements from initializer list");
+    b = {
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+    };
+
+    DESC("get matrix size");
+    auto [b_rows, b_cols] = b.size();
 }
