6ad4a02dbde3e13de3047ec14cdb44c9168166ca
Fahri Ali Rahman <fahri@nodeflux.io>
2020-06-23 16:47:33 +0700

    setup codecov

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+CWD=$pwd
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 
 ## assuming this script is located in root/scripts & build directory in root/scripts
-$DIR/../build/numeric_tests
+cd $DIR/../build/
+make numeric_coverage
+cd $CWD
