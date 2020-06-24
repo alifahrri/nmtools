4f267f36b41c139f8d2bdaddcbb1aedaea5a2a00
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2020-06-24 18:22:37 +0700

    Update finite difference (#45)
    * update finite difference
    
    * remove unused lines
    
    * simplify namespace
    
    * update finite diff tests
    
    * add finite diff docs

--- include/nmtools/differentiation/finite.hpp
+++ include/nmtools/differentiation/finite.hpp
@@ -3,72 +3,66 @@
 
 #include <cmath>
 
-namespace nmtools {
-    namespace differentiation {
+namespace nmtools::differentiation {
 
-        enum class Formulas {
-            Centered,
-            Forward,
-            Backward  
-        };
+    enum class Formulas {
+        Centered,
+        Forward,
+        Backward  
+    };
 
-        /* workaround for function template partial specialization */
-        namespace detail
-        {
-            template <size_t order>
-            struct Order{};
-            template <Formulas fomula>
-            struct ComputeType{};
+    /* workaround for function template partial specialization */
+    namespace detail
+    {
+        /* TODO: add second order differentiation */
+        template <size_t order>
+        struct Order{};
+        template <Formulas fomula>
+        struct ComputeType{};
 
-            template <typename F, typename Scalar>
-            void finite_difference(Order<1>, ComputeType<Formulas::Forward>, F &f, Scalar x, Scalar step, Scalar &d)
-            {
-                const auto& h = step;
-                d = (-f(x+2*h) + 4*f(x+h) - 3*f(x)) / (2*h);
-            }
-
-            template <typename F, typename Scalar>
-            void finite_difference(Order<1>, ComputeType<Formulas::Backward>, F &f, Scalar x, Scalar step, Scalar &d)
-            {
-                const auto& h = step;
-                d = ((3*f(x)) - 4*f(x-h) + f(x-2*h)) / (2*h);
-            }
-
-            template <typename F, typename Scalar>
-            void finite_difference(Order<1>, ComputeType<Formulas::Centered>, F &f, Scalar x, Scalar step, Scalar &d)
-            {
-                const auto& h = step;
-                d = (-f(x+2*h) + 8*f(x+h) - 8*f(x-h) + f(x-2*h)) / (12*h);
-            }
-        } // namespace detail
-        
-        template <size_t order, Formulas formula = Formulas::Centered, typename F, typename Scalar>
-        void finite_difference(F &f, Scalar x, Scalar step, Scalar &d)
-        {
-            detail::finite_difference(detail::Order<order>{}, detail::ComputeType<formula>{}, f, x, step, d);
-        }
-
-        /* unfortunately, we cant do this */
-        #if 0
         template <typename F, typename Scalar>
-        void finite_difference<1,Formulas::Forward,F,Scalar>(F &f, Scalar x, Scalar step, Scalar &d)
+        constexpr auto finite_difference(Order<1>, ComputeType<Formulas::Forward>, F &f, Scalar x, Scalar step)
         {
-            const auto& h = step;
-            d = (-f(x+2*h) + 4*f(x+h) - 3*f(x))/(2*h);
+            const auto& h = step; // alias
+            return (-f(x+2*h) + 4*f(x+h) - 3*f(x)) / (2*h);
         }
+
         template <typename F, typename Scalar>
-        void finite_difference<1,Formulas::Backward,F,Scalar>(F &f, Scalar x, Scalar step, Scalar &d)
+        constexpr auto finite_difference(Order<1>, ComputeType<Formulas::Backward>, F &f, Scalar x, Scalar step)
         {
-            const auto& h = step;
-            d = ((3*f(x)) - 4*f(x-h) + -f(x-2*h))/(2*h);
+            const auto& h = step; // alias
+            return ((3*f(x)) - 4*f(x-h) + f(x-2*h)) / (2*h);
         }
+
         template <typename F, typename Scalar>
-        void finite_difference<1,Formulas::Centered,F,Scalar>(F &f, Scalar x, Scalar step, Scalar &d)
+        constexpr auto finite_difference(Order<1>, ComputeType<Formulas::Centered>, F &f, Scalar x, Scalar step)
         {
-            const auto& h = step;
-            d = (-f(x+2*h) + 8*f(x+h) - 8*f(x-h) + f(x-2*h))/(2*h);
+            const auto& h = step; // alias
+            return (-f(x+2*h) + 8*f(x+h) - 8*f(x-h) + f(x-2*h)) / (12*h);
         }
-        #endif
-    } // namespace differentiation
+    } // namespace detail
+    
+    /**
+     * @brief compute numerical differentiation using finite difference method
+     * 
+     * @tparam order=1 order of derivative
+     * @tparam formula=Formulas::Centered derivative formula [Centered,Forward,Backward]
+     * @tparam F type of function to be differentiated
+     * @tparam Scalar real number
+     * @param f function to be differentiated
+     * @param x differentiation point
+     * @param step differentiation step
+     * @return constexpr auto 
+     * @reference @book{Chapra2010,
+            author = {Chapra, Steven C and Canale, Raymond P},
+            title = {{Numerical Methods for Engineers Sixth Edition Chapra Canale}},
+        } 
+     */
+    template <size_t order=1, Formulas formula=Formulas::Centered, typename F, typename Scalar>
+    constexpr auto finite_difference(F &f, Scalar x, Scalar step)
+    {
+        return detail::finite_difference(detail::Order<order>{}, detail::ComputeType<formula>{}, f, x, step);
+    }
+
 } // namespace nmtools
 #endif // NMTOOLS_FINITE_HPP
--- tests/differentiation/finite.cpp
+++ tests/differentiation/finite.cpp
@@ -4,32 +4,49 @@
 
 namespace diff = nmtools::differentiation;
 
+#define NEAR(x,y,eps) (std::fabs(x-y) < eps)
+
 TEST(finite, first_order_centered_0)
 {
-    auto f = [](double x){
+    constexpr auto f = [](double x){
         return -0.1*std::pow(x,4) - 0.15*std::pow(x,3) - 0.5*std::pow(x,2) - 0.25*x + 1.2;
     };
-    double d{0};
-    diff::finite_difference<1>(f,0.5,0.25,d);
-    EXPECT_NEAR(d,-0.9125,1e-6);
+    {
+        auto d = diff::finite_difference(f,0.5,0.25);
+        EXPECT_NEAR(d,-0.9125,1e-6);
+    }
+    {
+        constexpr auto d = diff::finite_difference(f,0.5,0.25);
+        static_assert(NEAR(d,-0.9125,1e-6));
+    }
 }
 
 TEST(finite, first_order_forward_0)
 {
-    auto f = [](double x){
+    constexpr auto f = [](double x){
         return -0.1*std::pow(x,4) - 0.15*std::pow(x,3) - 0.5*std::pow(x,2) - 0.25*x + 1.2;
     };
-    double d{0};
-    diff::finite_difference<1,diff::Formulas::Forward>(f,0.5,0.25,d);
-    EXPECT_NEAR(d,-0.859375,1e-6);
+    {
+        auto d = diff::finite_difference<1,diff::Formulas::Forward>(f,0.5,0.25);
+        EXPECT_NEAR(d,-0.859375,1e-6);
+    }
+    {
+        constexpr auto d = diff::finite_difference<1,diff::Formulas::Forward>(f,0.5,0.25);
+        static_assert(NEAR(d,-0.859375,1e-6));
+    }
 }
 
 TEST(finite, first_order_backward_0)
 {
-    auto f = [](double x){
+    constexpr auto f = [](double x){
         return -0.1*std::pow(x,4) - 0.15*std::pow(x,3) - 0.5*std::pow(x,2) - 0.25*x + 1.2;
     };
-    double d{0};
-    diff::finite_difference<1,diff::Formulas::Backward>(f,0.5,0.25,d);
-    EXPECT_NEAR(d,-0.878125,1e-6);
+    {
+        auto d = diff::finite_difference<1,diff::Formulas::Backward>(f,0.5,0.25);
+        EXPECT_NEAR(d,-0.878125,1e-6);
+    }
+    {
+        constexpr auto d = diff::finite_difference<1,diff::Formulas::Backward>(f,0.5,0.25);
+        static_assert(NEAR(d,-0.878125,1e-6));
+    }
 }
