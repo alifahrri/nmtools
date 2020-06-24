7d28e3480c0ee18ba1b72619d6474c0d77a93e08
Fahri Ali Rahman <fahri@nodeflux.io>
2020-06-24 17:22:09 +0700

    remove unused lines

--- include/nmtools/differentiation/finite.hpp
+++ include/nmtools/differentiation/finite.hpp
@@ -48,28 +48,6 @@ namespace nmtools {
         {
             return detail::finite_difference(detail::Order<order>{}, detail::ComputeType<formula>{}, f, x, step);
         }
-
-        /* unfortunately, we cant do this */
-        #if 0
-        template <typename F, typename Scalar>
-        void finite_difference<1,Formulas::Forward,F,Scalar>(F &f, Scalar x, Scalar step, Scalar &d)
-        {
-            const auto& h = step;
-            d = (-f(x+2*h) + 4*f(x+h) - 3*f(x))/(2*h);
-        }
-        template <typename F, typename Scalar>
-        void finite_difference<1,Formulas::Backward,F,Scalar>(F &f, Scalar x, Scalar step, Scalar &d)
-        {
-            const auto& h = step;
-            d = ((3*f(x)) - 4*f(x-h) + -f(x-2*h))/(2*h);
-        }
-        template <typename F, typename Scalar>
-        void finite_difference<1,Formulas::Centered,F,Scalar>(F &f, Scalar x, Scalar step, Scalar &d)
-        {
-            const auto& h = step;
-            d = (-f(x+2*h) + 8*f(x+h) - 8*f(x-h) + f(x-2*h))/(2*h);
-        }
-        #endif
     } // namespace differentiation
 } // namespace nmtools
 #endif // NMTOOLS_FINITE_HPP
