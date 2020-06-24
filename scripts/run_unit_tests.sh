c4ad2dbea0cd9ebed96ae89b6e707ad5154a977a
Fahri Ali Rahman <fahri@nodeflux.io>
2020-06-24 14:38:59 +0700

    upload coverage reports to codecov

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -6,4 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ## assuming this script is located in root/scripts & build directory in root/scripts
 cd $DIR/../build/
 make numeric_coverage
+
+if [[ ! -z ${CODECOV_TOKEN} ]]; then
+    bash <(curl -s https://codecov.io/bash) -f numeric_coverage.info || echo "Codecov did not collect coverage reports"
+fi
+
 cd $CWD
