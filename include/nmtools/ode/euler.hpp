7def84f669353ad142da1d5d7cdb648f5eb01205
Fahri Ali Rahman <fahri@nodeflux.io>
2020-06-29 20:47:55 +0700

    cleanup ode impl, add docs, constexpr support

--- include/nmtools/ode/euler.hpp
+++ include/nmtools/ode/euler.hpp
@@ -1,68 +1,141 @@
 #ifndef NMTOOLS_EULER_HPP
 #define NMTOOLS_EULER_HPP
 
-namespace nmtools {
-    namespace ode {
-        namespace detail {
-            template <typename DF, typename Scalar>
-            void euler(DF &df, Scalar &x, Scalar &y, Scalar h, Scalar &ynew) {
-                auto dydx = df(x,y);
-                ynew = y + dydx*h;
+#include <type_traits>
+#include <map>
+namespace nmtools::ode {
+    namespace detail {
+        template <typename DF, typename Scalar>
+        constexpr auto euler(DF &df, Scalar &x, Scalar &y, Scalar h) {
+            auto dydx = df(x,y);
+            auto ynew = y + dydx*h;
+            return ynew;
+        }
+        template <typename DF, typename Scalar>
+        constexpr auto midpoint(DF &df, Scalar &x, Scalar &y, Scalar h) {
+            auto dydx = df(x,y);
+            auto ym = y + dydx*(h/2);
+            auto dymdx = df(x+h/2,ym);
+            auto ynew = y + dymdx*h;
+            return ynew;
+        }
+        template <typename DF, typename Scalar>
+        constexpr auto heun(DF &df, Scalar &x, Scalar &y, Scalar h) {
+            auto dy1dx = df(x,y);
+            auto ye = y + dy1dx*h;
+            auto dy2dx = df(x+h,ye);
+            auto slope = (dy1dx + dy2dx) / 2;
+            auto ynew = y + slope*h;
+            return ynew;
+        }
+        template <typename DF, typename Scalar, typename Integrator, typename Logger=std::nullptr_t>
+        constexpr auto common_driver(DF &df, Scalar yi, Scalar xi, Scalar xf, Scalar dx, const Integrator &integrator, Logger *logger=Logger{}) {
+            auto y = yi;
+            auto x = xi;
+            auto &h = dx;
+            const auto &xend = xf;
+            size_t i=0;
+            do {
+                if (xend - x < h) {
+                    h = xend - x;
+                }
+                if constexpr (!std::is_same_v<Logger,std::nullptr_t>) {
+                    static_assert(std::is_pointer_v<decltype(logger)>);
+                    using arg_t = std::map<std::string,Scalar>;
+                    /* TODO: check if callable */
+                    (*logger)(arg_t{{"i",i},{"x",x},{"y",y},{"h",h}});
+                }
+                auto ynew = integrator(df,x,y,h);
                 x = x + h;
-            }
-            template <typename DF, typename Scalar>
-            void midpoint(DF &df, Scalar &x, Scalar &y, Scalar h, Scalar &ynew) {
-                auto dydx = df(x,y);
-                auto ym = y + dydx*(h/2);
-                auto dymdx = df(x+h/2,ym);
-                ynew = y + dymdx*h;
-                x = x + h;
-            }
-            template <typename DF, typename Scalar>
-            void heun(DF &df, Scalar &x, Scalar &y, Scalar h, Scalar &ynew) {
-                auto dy1dx = df(x,y);
-                auto ye = y + dy1dx*h;
-                auto dy2dx = df(x+h,ye);
-                auto slope = (dy1dx + dy2dx) / 2;
-                ynew = y + slope*h;
-                x = x + h;
-            }
-            template <typename DF, typename Scalar, typename Integrator>
-            void common_driver(DF &df, Scalar yi, Scalar xi, Scalar xf, Scalar dx, Scalar &y, const Integrator &integrator) {
-                y = yi;
-                auto x = xi;
-                auto &h = dx;
-                const auto &xend = xf;
-                do {
-                    if (xend - x < h) {
-                        h = xend - x;
-                    }
-                    typename std::decay<decltype(y)>::type ynew;
-                    integrator(df,x,y,h,ynew);
-                    y = ynew;
-                } while (x < xend);
-            }
-        } // namespace detail
+                y = ynew;
+            } while (x < xend);
+            return y;
+        }
+    } // namespace detail
 
-        template <typename DF, typename Scalar>
-        void euler(DF &df, Scalar yi, Scalar xi, Scalar xf, Scalar dx, Scalar &y) {
-            detail::common_driver(df,yi,xi,xf,dx,y,[](DF &df, Scalar &x, Scalar &y, Scalar h, Scalar &ynew){
-                detail::euler(df,x,y,h,ynew);
-            });
-        }
-        template <typename DF, typename Scalar>
-        void midpoint(DF &df, Scalar yi, Scalar xi, Scalar xf, Scalar dx, Scalar &y) {
-            detail::common_driver(df,yi,xi,xf,dx,y,[](DF &df, Scalar &x, Scalar &y, Scalar h, Scalar &ynew){
-                detail::midpoint(df,x,y,h,ynew);
-            });
-        }
-        template <typename DF, typename Scalar>
-        void heun(DF &df, Scalar yi, Scalar xi, Scalar xf, Scalar dx, Scalar &y) {
-            detail::common_driver(df,yi,xi,xf,dx,y,[](DF &df, Scalar &x, Scalar &y, Scalar h, Scalar &ynew){
-                detail::heun(df,x,y,h,ynew);
-            });
-        }
-    } // namespace ode
-} // namespace nmtools
+    /**
+     * @brief evaluate ODE initial value problem using Euler's method
+     * 
+     * @tparam DF differential equation, with signature DF(ScalarX,ScalarY)
+     * @tparam ScalarY real number for y
+     * @tparam ScalarX real number for x
+     * @tparam Logger=std::nullptr_t 
+     * @param df callable differential equation
+     * \f[
+     *      \frac{dy}{dx} = f(x,y)
+     * \f]
+     * @param yi initital dependent variable
+     * @param xi initital independent variable
+     * @param xf final independent variable
+     * @param dx independent variable step
+     * @param logger optional intermediate value logger
+     * no runtime cost (logging block won't be compiled) if left unspecified
+     * @return constexpr auto 
+     * @cite chapra2014numerical_ode_euler
+     */
+    template <typename DF, typename ScalarY, typename ScalarX, typename Logger=std::nullptr_t>
+    constexpr auto euler(DF &df, ScalarY yi, ScalarX xi, ScalarX xf, ScalarX dx, Logger *logger=Logger{}) {
+        constexpr auto integrator = [](DF &df, auto&& x, auto&& y, auto&& h){
+            return detail::euler(df,x,y,h);
+        };
+        return detail::common_driver(df,yi,xi,xf,dx,integrator);
+    }
+
+    /**
+     * @brief evaluate ODE initial value problem using 
+     * modified Euler's (a.k.a the midpoint a.k.a improved polygon) method
+     * 
+     * @tparam DF differential equation, with signature DF(ScalarX,ScalarY)
+     * @tparam ScalarY real number for y
+     * @tparam ScalarX real number for x
+     * @tparam Logger=std::nullptr_t 
+     * @param df callable differential equation
+     * \f[
+     *      \frac{dy}{dx} = f(x,y)
+     * \f]
+     * @param yi initital dependent variable
+     * @param xi initital independent variable
+     * @param xf final independent variable
+     * @param dx independent variable step
+     * @param logger optional intermediate value logger
+     * no runtime cost (logging block won't be compiled) if left unspecified
+     * @return constexpr auto 
+     * @cite chapra2014numerical_ode_midpoint
+     */
+    template <typename DF, typename ScalarY, typename ScalarX, typename Logger=std::nullptr_t>
+    constexpr auto midpoint(DF &df, ScalarY yi, ScalarX xi, ScalarX xf, ScalarX dx, Logger *logger=Logger{}) {
+        constexpr auto integrator = [](DF &df, auto&& x, auto &&y, auto&& h){
+            return detail::midpoint(df,x,y,h);
+        };
+        return detail::common_driver(df,yi,xi,xf,dx,integrator);
+    }
+    /**
+     * @brief evaluate ODE initial value problem using Heun's method
+     * 
+     * @tparam DF differential equation, with signature DF(ScalarX,ScalarY)
+     * @tparam ScalarY real number for y
+     * @tparam ScalarX real number for x
+     * @tparam Logger=std::nullptr_t 
+     * @param df callable differential equation
+     * \f[
+     *      \frac{dy}{dx} = f(x,y)
+     * \f]
+     * @param yi initital dependent variable
+     * @param xi initital independent variable
+     * @param xf final independent variable
+     * @param dx independent variable step
+     * @param logger optional intermediate value logger
+     * no runtime cost (logging block won't be compiled) if left unspecified
+     * @return constexpr auto 
+     * @cite chapra2014numerical_ode_heun
+     */
+    template <typename DF, typename ScalarY, typename ScalarX, typename Logger=std::nullptr_t>
+    constexpr auto heun(DF &df, ScalarY yi, ScalarX xi, ScalarX xf, ScalarX dx, Logger *logger=Logger{}) {
+        constexpr auto integrator = [](DF &df, auto&& x, auto&& y, auto&& h){
+            return detail::heun(df,x,y,h);
+        };
+        return detail::common_driver(df,yi,xi,xf,dx,integrator);
+    }
+} // namespace nmtools::ode
 
 #endif
