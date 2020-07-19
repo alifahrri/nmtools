cb095b7529c51bd14b538660efa999d7948bc4c7
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2020-07-19 20:42:58 +0700

    Cmake cleanup (#60)
    * clean-up cmake files
    
    * add install config files
    
    * update nanobench install script
    
    * build examples & benchmarks
    
    * setup installation
    
    * update coverage target name
    
    * fix typo on coverage name
    
    * update coverage report filename

---
+++ cmake/nmtools.cmake
@@ -0,0 +1,45 @@
+function(apply_nmtools)
+    set(options)
+    set(oneValueArgs TARGET)
+    set(multiValueArgs COMPILE_OPTIONS)
+    cmake_parse_arguments(apply_nmtools
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        "${ARGN}"
+    )
+
+    message(STATUS "apply nmtools ...")
+    message(STATUS "    TARGET: ${apply_nmtools_TARGET}")
+    message(STATUS "    COMPILE_OPTIONS: ${apply_nmtools_COMPILE_OPTIONS}")
+
+    if(NMTOOLS_INCLUDE_DIR)
+        message(STATUS "    NMTOOLS_INCLUDE_DIR: ${NMTOOLS_INCLUDE_DIR}")
+    else()
+        message(STATUS "trying to find nmtools")
+        find_package(nmtools REQUIRED CONFIG)
+        set(NMTOOLS_INCLUDE_DIR ${nmtools_INCLUDE_DIRS})
+        message(STATUS "    NMTOOLS_INCLUDE_DIR: ${NMTOOLS_INCLUDE_DIR}")
+    endif()
+
+    target_compile_features(
+        ${apply_nmtools_TARGET}
+        PRIVATE
+        cxx_std_17
+    )
+
+    target_include_directories(
+        ${apply_nmtools_TARGET}
+        PRIVATE
+        ${NMTOOLS_INCLUDE_DIR}
+    )
+
+    if(apply_nmtools_COMPILE_OPTIONS)
+        target_compile_options(
+            ${apply_nmtools_TARGET}
+            PRIVATE
+            ${apply_nmtools_COMPILE_OPTIONS}
+        )
+    endif()
+
+endfunction()
--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -5,11 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ## assuming this script is located in root/scripts & build directory in root/scripts
 cd $DIR/../build/
-make numeric_coverage
+make numeric-tests-coverage
 
 cd $DIR/..
 if [[ ! -z ${CODECOV_TOKEN} ]]; then
-    bash <(curl -s https://codecov.io/bash) -f build/numeric_coverage.info || echo "Codecov did not collect coverage reports"
+    bash <(curl -s https://codecov.io/bash) -f build/tests/numeric-tests-coverage.info || echo "Codecov did not collect coverage reports"
 fi
 
 cd $CWD
