3a6f68d13183c7fe441ec56afd94e8abcb6bb911
Fahri Ali Rahman <fahri@nodeflux.io>
2020-07-19 20:03:27 +0700

    fix typo on coverage name

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ## assuming this script is located in root/scripts & build directory in root/scripts
 cd $DIR/../build/
-make numeric-coverage
+make numeric-tests-coverage
 
 cd $DIR/..
 if [[ ! -z ${CODECOV_TOKEN} ]]; then
