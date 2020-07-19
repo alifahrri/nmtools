29c6eeb19480de1ca86ce4d838fd2d866f486547
Fahri Ali Rahman <fahri@nodeflux.io>
2020-07-19 20:15:39 +0700

    update coverage report filename

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -9,7 +9,7 @@ make numeric-tests-coverage
 
 cd $DIR/..
 if [[ ! -z ${CODECOV_TOKEN} ]]; then
-    bash <(curl -s https://codecov.io/bash) -f build/numeric_coverage.info || echo "Codecov did not collect coverage reports"
+    bash <(curl -s https://codecov.io/bash) -f build/tests/numeric-tests-coverage.info || echo "Codecov did not collect coverage reports"
 fi
 
 cd $CWD
