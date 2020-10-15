548a61a5c9dd776c6917f371b2a1340eaa63264c
Fahri Ali Rahman <fahri@nodeflux.io>
2020-10-15 18:07:25 +0700

    add doctest coverage

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -6,10 +6,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ## assuming this script is located in root/scripts & build directory in root/scripts
 cd $DIR/../build/
 make numeric-tests-coverage
+make numeric-tests-doctest-coverage
 
 cd $DIR/..
 if [[ ! -z ${CODECOV_TOKEN} ]]; then
     bash <(curl -s https://codecov.io/bash) -f build/tests/numeric-tests-coverage.info || echo "Codecov did not collect coverage reports"
+    bash <(curl -s https://codecov.io/bash) -f build/tests/numeric-tests-doctest-coverage.info || echo "Codecov did not collect coverage reports"
 fi
 
 cd $CWD
