76fb2d413e5ca26e45b3e97d7ac7b3f3bbc73164
Fahri Ali Rahman <fahri@nodeflux.io>
2020-10-31 16:07:31 +0700

    add more metafunction tests

---
+++ tests/src/doctest/meta/blas/mmadd.cpp
@@ -0,0 +1,66 @@
+#include "nmtools/traits.hpp"
+#include "nmtools/meta.hpp"
+#include "nmtools/array/fixed.hpp"
+#include "nmtools/array/dynamic.hpp"
+#include "nmtools/blas.hpp"
+// NOTE: need to include utility for std::array fixed_size specialization
+#include "nmtools/array/utility.hpp"
+#include "testing/testing.hpp"
+
+#include "doctest/doctest.h"
+#include <boost/type_index.hpp>
+#include <array>
+
+namespace blas = nmtools::blas;
+namespace meta = nmtools::meta;
+namespace traits = nmtools::traits;
+
+TEST_CASE("mmadd_select_resizeable_t" * doctest::test_suite("blas::mmadd"))
+{
+    using mvector_t  = std::vector<std::vector<double>>;
+    using marray_t   = std::array<std::array<double,3>,5>;
+    using fvector_t  = nmtools::array::fixed_vector<double,3>;
+    using fmatrix_t  = nmtools::array::fixed_matrix<double,5,3>;
+    using dvector_t  = nmtools::array::dynamic_vector<double>;
+    using dmatrix_t  = nmtools::array::dynamic_matrix<double>;
+    using ndarray_t  = nmtools::array::fixed_ndarray<double,5,3>;
+    using ndarray3_t = nmtools::array::fixed_ndarray<double,5,3,1>;
+    using dndarray_t = nmtools::array::dynamic_ndarray<double>;
+
+    {
+        using lhs_t = dmatrix_t;
+        using rhs_t = fmatrix_t;
+        using result_t = meta::resolve_optype_t<blas::mmadd_select_resizeable_t,lhs_t,rhs_t>;
+        LOG_TYPEINFO(lhs_t);
+        LOG_TYPEINFO(rhs_t);
+        LOG_TYPEINFO(result_t);
+        STATIC_CHECK_IS_SAME( result_t, dmatrix_t );
+    }
+    {
+        using lhs_t = dmatrix_t;
+        using rhs_t = dmatrix_t;
+        using result_t = meta::resolve_optype_t<blas::mmadd_select_resizeable_t,lhs_t,rhs_t>;
+        LOG_TYPEINFO(lhs_t);
+        LOG_TYPEINFO(rhs_t);
+        LOG_TYPEINFO(result_t);
+        STATIC_CHECK_IS_SAME( result_t, dmatrix_t );
+    }
+    {
+        using lhs_t = fmatrix_t;
+        using rhs_t = fmatrix_t;
+        using result_t = meta::resolve_optype_t<blas::mmadd_select_resizeable_t,lhs_t,rhs_t>;
+        LOG_TYPEINFO(lhs_t);
+        LOG_TYPEINFO(rhs_t);
+        LOG_TYPEINFO(result_t);
+        STATIC_CHECK_IS_SAME( result_t, void );
+    }
+    {
+        using lhs_t = mvector_t;
+        using rhs_t = marray_t;
+        using result_t = meta::resolve_optype_t<blas::mmadd_select_resizeable_t,lhs_t,rhs_t>;
+        LOG_TYPEINFO(lhs_t);
+        LOG_TYPEINFO(rhs_t);
+        LOG_TYPEINFO(result_t);
+        STATIC_CHECK_IS_SAME( result_t, mvector_t );
+    }
+}
---
+++ tests/src/doctest/meta/get_container_value_type.cpp
@@ -0,0 +1,30 @@
+#include "nmtools/traits.hpp"
+#include "nmtools/meta.hpp"
+#include "nmtools/array/fixed.hpp"
+#include "nmtools/array/dynamic.hpp"
+// NOTE: need to include utility for std::array fixed_size specialization
+#include "nmtools/array/utility.hpp"
+#include "testing/testing.hpp"
+
+#include "doctest/doctest.h"
+#include <boost/type_index.hpp>
+#include <array>
+
+namespace meta = nmtools::meta;
+namespace traits = nmtools::traits;
+
+TEST_CASE("get_container_value_type_t" * doctest::test_suite("meta"))
+{
+    using vvector_t  = std::vector<double>;
+    using varray_t   = std::array<double,3>;
+    using fvector_t  = nmtools::array::fixed_vector<double,3>;
+    using dvector_t  = nmtools::array::dynamic_vector<double>;
+
+    {
+        using lhs_t = double[3];
+        using result_t = meta::get_container_value_type_t<lhs_t>;
+        LOG_TYPEINFO(lhs_t);
+        LOG_TYPEINFO(result_t);
+        STATIC_CHECK_IS_SAME( result_t, double );
+    }
+}
