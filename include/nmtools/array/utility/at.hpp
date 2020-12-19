6fa329dc09ba50c6184e35fd842af011bb7f0d89
Fahri Ali Rahman <fahri@nodeflux.io>
2020-12-20 04:53:32 +0700

    make template at to work with tuple

--- include/nmtools/array/utility/at.hpp
+++ include/nmtools/array/utility/at.hpp
@@ -22,48 +22,8 @@ namespace nmtools {
     using std::size;
     using std::get;
 
-    /**
-     * @brief access element at i-th index.
-     * Dispatch one of the following expression whenever possible (ordered by priority):
-     * (1) a.at(i);
-     * (2) a[i];
-     * (3) a(i);
-     * 
-     * @tparam Array 
-     * @tparam size_type 
-     * @param a 
-     * @param i 
-     * @return constexpr decltype(auto) 
-     */
-    template <typename Array, typename size_type>
-    constexpr decltype(auto) at(const Array& a, size_type i)
-    {
-        /* TODO: this fn is returning reference,
-            find out how to also support return by value
-         */
-        using namespace meta;
-
-        static_assert(
-            has_at_v<const Array&,size_type>
-            || has_square_bracket_v<const Array&,size_type>
-            || has_bracket_v<const Array&,size_type>,
-            "unsupported type Array for at"
-        );
-
-        if constexpr (has_at_v<const Array&,size_type>)
-            return a.at(i);
-        else if constexpr (has_square_bracket_v<const Array&,size_type>)
-            return a[i];
-        else if constexpr (has_bracket_v<const Array&,size_type>)
-            return a(i);
-        else {
-            /* compile errror */
-            static_assert(has_bracket_v<const Array&,size_type>);
-        }
-    }
-
-    template <auto i, typename Array>
-    constexpr decltype(auto) at(const Array& a)
+    template <auto i, typename array_t>
+    constexpr decltype(auto) at(const array_t& a)
     {
         /* TODO: this fn is returning reference,
             find out how to also support return by value
@@ -72,24 +32,53 @@ namespace nmtools {
         using size_type = decltype(i);
 
         static_assert(
-            has_square_bracket_v<const Array&,size_type>
-            || has_bracket_v<const Array&,size_type>
-            /* TODO : meaningful error message here */
-        );
-        static_assert(
-            is_fixed_size_vector_v<Array>
-            && (i<meta::fixed_vector_size_v<Array>)
+            has_square_bracket_v<const array_t&,size_type>
+            || has_bracket_v<const array_t&,size_type>
+            || has_template_get_v<const array_t&,i>
+            , "unsupported at"
         );
 
-        if constexpr (has_square_bracket_v<const Array&,size_type>)
+        if constexpr (has_template_get_v<const array_t&,i>)
+            return get<i>(a);
+        // fallback
+        else if constexpr (has_square_bracket_v<const array_t&,size_type>)
             return a[i];
-        else if constexpr (has_bracket_v<const Array&,size_type>)
+        else if constexpr (has_bracket_v<const array_t&,size_type>)
             return a(i);
         else {
             /* compile errror */
-            static_assert(has_bracket_v<const Array&,size_type>);
+            static_assert(has_bracket_v<const array_t&,size_type>);
         }
-    }
+    } // at
+
+    template <auto i, typename array_t>
+    constexpr decltype(auto) at(array_t& a)
+    {
+        /* TODO: this fn is returning reference,
+            find out how to also support return by value
+         */
+        using namespace meta;
+        using size_type = decltype(i);
+
+        static_assert(
+            has_square_bracket_v<array_t&,size_type>
+            || has_bracket_v<array_t&,size_type>
+            || has_template_get_v<array_t&,i>
+            , "unsupported at"
+        );
+
+        if constexpr (has_template_get_v<array_t&,i>)
+            return get<i>(a);
+        // fallback
+        else if constexpr (has_square_bracket_v<array_t&,size_type>)
+            return a[i];
+        else if constexpr (has_bracket_v<array_t&,size_type>)
+            return a(i);
+        else {
+            /* compile errror */
+            static_assert(has_bracket_v<array_t&,size_type>);
+        }
+    } // at
 
     /**
      * @brief access element at i-th index.
@@ -98,32 +87,78 @@ namespace nmtools {
      * (2) a[i];
      * (3) a(i);
      * 
-     * @tparam Array 
+     * @tparam array_t 
      * @tparam size_type 
      * @param a 
      * @param i 
      * @return constexpr decltype(auto) 
      */
-    template <typename Array, typename size_type>
-    constexpr decltype(auto) at(Array& a, size_type i)
+    template <typename array_t, typename size_type>
+    constexpr decltype(auto) at(const array_t& a, size_type i)
+    {
+        /* TODO: this fn is returning reference,
+            find out how to also support return by value
+         */
+        using namespace meta;
+
+        static_assert(
+            has_at_v<const array_t&,size_type>
+            || has_square_bracket_v<const array_t&,size_type>
+            || has_bracket_v<const array_t&,size_type>
+            || is_integral_constant_v<size_type>
+            , "unsupported type array_t for at"
+        );
+
+        if constexpr (has_at_v<const array_t&,size_type>)
+            return a.at(i);
+        else if constexpr (has_square_bracket_v<const array_t&,size_type>)
+            return a[i];
+        else if constexpr (has_bracket_v<const array_t&,size_type>)
+            return a(i);
+        else if constexpr (is_integral_constant_v<size_type>)
+            return at<size_type::value>(a);
+        else {
+            /* compile errror */
+            static_assert(has_bracket_v<const array_t&,size_type>);
+        }
+    } // at
+
+    /**
+     * @brief access element at i-th index.
+     * Dispatch one of the following expression whenever possible (ordered by priority):
+     * (1) a.at(i);
+     * (2) a[i];
+     * (3) a(i);
+     * 
+     * @tparam array_t 
+     * @tparam size_type 
+     * @param a 
+     * @param i 
+     * @return constexpr decltype(auto) 
+     */
+    template <typename array_t, typename size_type>
+    constexpr decltype(auto) at(array_t& a, size_type i)
     {
         using namespace meta;
         static_assert(
-            has_at_v<Array&,size_type>
-            || has_square_bracket_v<Array&,size_type>
-            || has_bracket_v<Array&,size_type>
-            /* TODO : meaningful error message here */
+            has_at_v<array_t&,size_type>
+            || has_square_bracket_v<array_t&,size_type>
+            || has_bracket_v<array_t&,size_type>
+            || is_integral_constant_v<size_type>
+            , "unsupported type array_t for at"
         );
 
-        if constexpr (has_at_v<Array&,size_type>)
+        if constexpr (has_at_v<array_t&,size_type>)
             return a.at(i);
-        else if constexpr (has_square_bracket_v<Array&,size_type>)
+        else if constexpr (has_square_bracket_v<array_t&,size_type>)
             return a[i];
-        else if constexpr (has_bracket_v<Array&,size_type>)
+        else if constexpr (has_bracket_v<array_t&,size_type>)
             return a(i);
+        else if constexpr (is_integral_constant_v<size_type>)
+            return at<size_type::value>(a);
         else {
             /* compile errror */
-            static_assert(has_bracket_v<Array&,size_type>);
+            static_assert(has_bracket_v<array_t&,size_type>);
         }
     } // constexpr decltype(auto) at
 
@@ -135,49 +170,49 @@ namespace nmtools {
      * (3) a(i,j);
      * (4) at(at(a,i),j);
      * 
-     * @tparam Array 
+     * @tparam array_t 
      * @tparam size_type 
      * @param a 
      * @param i 
      * @param j 
      * @return constexpr decltype(auto) 
      */
-    template <typename Array, typename size_type, typename...size_types>
-    constexpr decltype(auto) at(const Array& a, size_type i, size_types...indices)
+    template <typename array_t, typename size_type, typename...size_types>
+    constexpr decltype(auto) at(const array_t& a, size_type i, size_types...indices)
     {
-        if constexpr (meta::has_atnd_v<const Array&,size_type,size_types...>)
+        if constexpr (meta::has_atnd_v<const array_t&,size_type,size_types...>)
             return a.at(i,indices...);
-        else if constexpr (meta::has_bracketnd_v<const Array&,size_type,size_types...>)
+        else if constexpr (meta::has_bracketnd_v<const array_t&,size_type,size_types...>)
             return a.operator[](i,indices...);
-        else if constexpr (meta::has_funcnd_v<const Array&,size_type,size_types...>)
+        else if constexpr (meta::has_funcnd_v<const array_t&,size_type,size_types...>)
             return a(i,indices...);
         else
             return at(at(a,i),indices...);
     } // at(a,...)
 
-    template <auto i, auto j, typename Array>
-    constexpr decltype(auto) at(const Array& a)
+    template <auto i, auto j, typename array_t>
+    constexpr decltype(auto) at(const array_t& a)
     {
         /* TODO: this fn is returning reference,
             find out how to also support return by value
          */
         using namespace meta;
         using size_type = std::common_type_t<decltype(i),decltype(j)>;
-        constexpr auto shape = meta::fixed_matrix_size_v<Array>;
+        constexpr auto shape = meta::fixed_matrix_size_v<array_t>;
         constexpr auto rows = get<0>(shape);
         constexpr auto cols = get<1>(shape);
         static_assert(
-            is_fixed_size_matrix_v<Array>
+            is_fixed_size_matrix_v<array_t>
             && (i<rows && j<cols)
         );
-        if constexpr (has_square_bracket2d_v<const Array&,size_type>)
+        if constexpr (has_square_bracket2d_v<const array_t&,size_type>)
             return a[i,j];
-        else if constexpr (has_bracket2d_v<const Array&,size_type>)
+        else if constexpr (has_bracket2d_v<const array_t&,size_type>)
             return a(i,j);
         else {
             return at<j>(at<i>(a));
         }
-    } // constexpr decltype(auto) at(const Array& a, size_type i, size_type j)
+    } // constexpr decltype(auto) at(const array_t& a, size_type i, size_type j)
 
     /**
      * @brief access element at (i,j) index.
@@ -187,21 +222,21 @@ namespace nmtools {
      * (3) a(i,j);
      * (4) at(at(a,i),j);
      * 
-     * @tparam Array 
+     * @tparam array_t 
      * @tparam size_type 
      * @param a 
      * @param i 
      * @param j 
      * @return constexpr decltype(auto) 
      */
-    template <typename Array, typename size_type, typename...size_types>
-    constexpr decltype(auto) at(Array& a, size_type i, size_types...indices)
+    template <typename array_t, typename size_type, typename...size_types>
+    constexpr decltype(auto) at(array_t& a, size_type i, size_types...indices)
     {
-        if constexpr (meta::has_atnd_v<Array&,size_type,size_types...>)
+        if constexpr (meta::has_atnd_v<array_t&,size_type,size_types...>)
             return a.at(i,indices...);
-        else if constexpr (meta::has_bracketnd_v<Array&,size_type,size_types...>)
+        else if constexpr (meta::has_bracketnd_v<array_t&,size_type,size_types...>)
             return a.operator[](i,indices...);
-        else if constexpr (meta::has_funcnd_v<Array&,size_type,size_types...>)
+        else if constexpr (meta::has_funcnd_v<array_t&,size_type,size_types...>)
             return a(i,indices...);
         else
             return at(at(a,i),indices...);
