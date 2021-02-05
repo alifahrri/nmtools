7f1d6398b7dbf35b2ea3c8f851e41d7919854f13
Fahri Ali Rahman <fahri@nodeflux.io>
2021-02-05 21:40:13 +0700

    add ndarray examples

---
+++ examples/nmtools/array/src/broadcast_to.cpp
@@ -0,0 +1,50 @@
+#include "nmtools/array/view/broadcast_to.hpp"
+#include "nmtools/array/ndarray.hpp"
+#include "nmtools/utils/to_string.hpp"
+
+#include <iostream>
+#include <array>
+#include <vector>
+
+#define DESC(x) std::cout << x << std::endl;
+#define PRINT(x) \
+std::cout << #x << ":\n" \
+    << nm::utils::to_string(x) \
+    << std::endl; \
+
+namespace nm = nmtools;
+namespace na = nm::array;
+namespace view = nm::view;
+
+int main()
+{
+    auto shape = std::array{3,2,3};
+    DESC("\nbroadcasting an std::array")
+    {
+        auto array = std::array{1,2,3};
+        auto broadcasted = view::broadcast_to(array,shape);
+        PRINT(array)
+        PRINT(broadcasted)
+    }
+    DESC("\nbroadcasting an std::vector")
+    {
+        auto array = std::vector{1,2,3};
+        auto broadcasted = view::broadcast_to(array,shape);
+        PRINT(array)
+        PRINT(broadcasted)
+    }
+    DESC("\nbroadcasting an na::fixed_ndarray")
+    {
+        auto array = na::fixed_ndarray{{1,2,3}};
+        auto broadcasted = view::broadcast_to(array,shape);
+        PRINT(array)
+        PRINT(broadcasted)
+    }
+    DESC("\nbroadcasting an na::dynamic_ndarray")
+    {
+        auto array = na::dynamic_ndarray({1,2,3});
+        auto broadcasted = view::broadcast_to(array,shape);
+        PRINT(array)
+        PRINT(broadcasted)
+    }
+}
---
+++ examples/nmtools/array/src/squeeze.cpp
@@ -0,0 +1,56 @@
+#include "nmtools/array/view/squeeze.hpp"
+#include "nmtools/array/ndarray.hpp"
+#include "nmtools/utils/to_string.hpp"
+
+#include <iostream>
+#include <array>
+#include <vector>
+
+#define DESC(x) std::cout << x << std::endl;
+#define PRINT(x) \
+std::cout << #x << ":\n" \
+    << nm::utils::to_string(x) \
+    << std::endl; \
+
+namespace nm = nmtools;
+namespace na = nm::array;
+namespace view = nm::view;
+
+int main()
+{
+    DESC("\nsqueezing an std::array")
+    {
+        using array_t = std::array<std::array<int,1>,3>;
+        array_t array = {1,2,3};
+        auto squeezed = view::squeeze(array);
+        PRINT(array)
+        PRINT(squeezed)
+    }
+    DESC("\nsqueezing an std::vector")
+    {
+        using array_t = std::vector<std::vector<int>>;
+        array_t array = {{1},{2},{3}};
+        auto squeezed = view::squeeze(array);
+        PRINT(array)
+        PRINT(squeezed)
+    }
+    DESC("\nsqueezing an na::fixed_ndarray")
+    {
+        auto array = na::fixed_ndarray<int,3,1>{{{1},{2},{3}}};
+        auto squeezed = view::squeeze(array);
+        PRINT(array)
+        PRINT(squeezed)
+    }
+    DESC("\nsqueezing an na::dynamic_ndarray")
+    {
+        int raw[3][1] = {
+            {1},
+            {2},
+            {3}
+        };
+        auto array = na::dynamic_ndarray(std::move(raw));
+        auto squeezed = view::squeeze(array);
+        PRINT(array)
+        PRINT(squeezed)
+    }
+}
