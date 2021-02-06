cbb889e43402726db3a786039e3c802dd7c8adbe
Fahri Ali Rahman <fahri@nodeflux.io>
2021-02-06 21:25:58 +0700

    try to fix emscripten build (5)

--- docker/emscripten.dockerfile
+++ docker/emscripten.dockerfile
@@ -12,6 +12,9 @@ COPY CMakeLists.txt CMakeLists.txt
 COPY nmtools.pc.in nmtools.pc.in
 COPY nmtoolsConfig.cmake.in nmtoolsConfig.cmake.in
 
+# for emscripten use from include
+RUN bash scripts/bash scripts/download_doctest_header.sh -d include
+
 RUN /opt/emsdk/emsdk activate latest \
     && PATH=$PATH:/opt/emsdk:/opt/emsdk/node/12.18.1_64bit/bin:/opt/emsdk/upstream/emscripten \
     && mkdir build && cd build \
---
+++ scripts/download_doctest_header.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CWD=$pwd
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
+doctest_url="https://raw.githubusercontent.com/onqtam/doctest/2.4.0/doctest/doctest.h"
+doctest_file="doctest.h"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -d|--dest)
+    DEST="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ -z "$DEST" -a "$DEST" != " " ]; then
+    DEST="$HOME/tmp"
+    mkdir -p $DEST
+fi
+
+wget -O $DEST/$doctest_file $doctest_url
+
+cd $CWD
