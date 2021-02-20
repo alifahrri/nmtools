ce1cdefb4838101fae62ba15e8ce467e9f46b47b
Fahri Ali Rahman <fahri@nodeflux.io>
2021-02-20 23:13:42 +0700

    generalize broadcast_arrays to n-ary

--- include/nmtools/array/index/broadcast_shape.hpp
+++ include/nmtools/array/index/broadcast_shape.hpp
@@ -1,11 +1,15 @@
 #ifndef NMTOOLS_ARRAY_INDEX_BROADCAST_SHAPE_HPP
 #define NMTOOLS_ARRAY_INDEX_BROADCAST_SHAPE_HPP
 
+#include "nmtools/constants.hpp"
 #include "nmtools/meta.hpp"
 #include "nmtools/array/utility/at.hpp"
+#include "nmtools/array/shape.hpp"
 #include "nmtools/array/index/tuple_at.hpp"
-
-#include <algorithm> // max
+#include "nmtools/array/index/make_array.hpp"
+#include "nmtools/array/index/max.hpp"
+#include "nmtools/array/index/sum.hpp"
+#include "nmtools/array/index/where.hpp"
 
 namespace nmtools::index
 {
@@ -17,6 +21,9 @@ namespace nmtools::index
      */
     struct broadcast_shape_t {};
 
+    template <typename ashape_t, typename bshape_t>
+    using resolve_broadcast_shape = meta::resolve_optype<void,broadcast_shape_t,meta::remove_cvref_t<ashape_t>,meta::remove_cvref_t<bshape_t>>;
+
     /**
      * @brief broadcast two array shape together
      * 
@@ -26,40 +33,105 @@ namespace nmtools::index
      * @param bshape 
      * @return constexpr auto 
      */
-    template <typename ashape_t, typename bshape_t>
-    constexpr auto broadcast_shape(const ashape_t& ashape, const bshape_t& bshape)
+    template <typename ashape_t, typename bshape_t, typename...other_shapes_t>
+    constexpr auto broadcast_shape(const ashape_t& ashape, const bshape_t& bshape, const other_shapes_t&...other_shapes)
     {
-        using return_t = meta::resolve_optype_t<broadcast_shape_t,ashape_t,bshape_t>;
+        using namespace ::nmtools::literals;
+        using shape_pack_t = std::tuple<const ashape_t&, const bshape_t&, const other_shapes_t&...>;
+        using return_t = meta::apply_reduce_t<resolve_broadcast_shape,shape_pack_t>;
 
         auto res = return_t{};
         bool success = true;
 
-        auto m = tuple_size(ashape);
-        auto n = tuple_size(bshape);
-        auto s = std::max(m,n);
+        auto shapes = shape_pack_t{ashape, bshape, other_shapes...};
+        auto dims   = meta::template_map(
+            [](auto shape){
+                return tuple_size(shape);
+            }, shapes
+        );
 
+        constexpr auto N = std::tuple_size_v<decltype(shapes)>;
+        auto rdim = max(dims);
         if constexpr (meta::is_resizeable_v<return_t>)
-            res.resize(s);
+            res.resize(rdim);
         
+        // i will start from 0 to rdim (max)
         auto broadcast_shape_impl = [&](auto i){
             // @todo support type list (tuple of int constant)
-            using idx_t = std::make_signed_t<decltype(m-i-1)>;
-            idx_t ai = m - i - 1;
-            idx_t bi = n - i - 1;
-            idx_t si = s - i - 1;
-            if (ai < 0)
-                at(res,si) = tuple_at(bshape,bi);
-            else if (bi < 0)
-                at(res,si) = tuple_at(ashape,ai);
+            // current working index for results
+            using idx_t = std::make_signed_t<decltype(rdim-i-1)>;
+            idx_t si = rdim - i - 1;
+
+            // current working index for shapes (may be negative, indicating free axis)
+            auto shape_indices = meta::template_map([&](auto dim){
+                using idx_t = std::make_signed_t<decltype(dim-i-1)>;
+                return static_cast<idx_t>(dim-i-1);
+            }, dims);
+
+            // check if index is not free (its idx will be >=0)
+            // if not free axes is exactly one, we are safe, just take shape at that axis
+            // otherwise, axis must all equal, or 1
+            auto not_free = meta::template_map(
+                // must cast to integer to make sum works properly
+                [&](auto i) {
+                    return static_cast<size_t>(i >= 0);
+                }, shape_indices
+            );
+            // using sum instead of where to keep compile-time idx
+            auto sum_ = sum(not_free);
+
+            // consider the following case
+            // shapeA:    (2) idx: (-2,-1, 0)
+            // shapeB:  (3,1) idx: (-1, 0, 1)
+            // shapeC:(2,3,2) idx: ( 0, 1, 2)
+            // result:(2,3,2) idx: ( 0, 1, 2)
+
+            if (sum_==1) {
+                // only single non-free axis, can safely assign to ret
+                // no need to checking for equal shapes or 1
+                meta::template_for<N>([&](auto shape_idx){
+                    // shape of shaped_idx-th shapes for current working indices (i)
+                    auto shape_index = at(shape_indices, shape_idx);
+                    if (shape_index>=0) {
+                        auto shape_ = at(shapes, shape_idx);
+                        at(res, si) = tuple_at(shape_, shape_index);
+                    }
+                });
+            }
             else {
-                auto a = tuple_at(ashape,ai);
-                auto b = tuple_at(bshape,bi);
-                if ((a==b) || (b==1))
-                    at(res,si) = a;
-                else if (a==1)
-                    at(res,si) = b;
-                else
-                    success = false;
+                // multiple non-free axis detected,
+                // must be either same shape or 1
+
+                // TODO: use optional
+                auto broadcasted = std::tuple{false,0};
+                // unrolled for
+                meta::template_for<N>([&](auto shape_idx){
+                    auto shape_index = at(shape_indices, shape_idx);
+                    if (shape_index>=0 && !at(broadcasted,0_ct)) {
+                        // this axis for this shap is not free, broadcasted not set yet set this as initial
+                        auto shape_ = at(shapes, shape_idx);
+                        // size of this shape for this axis
+                        auto size_  = tuple_at(shape_, shape_index);
+                        at(broadcasted,1_ct) = size_;
+                        at(broadcasted,0_ct) = true;
+                    }
+                    else if (shape_index>=0 && at(broadcasted,0_ct)) {
+                        // this axis for this shap is not free, must be the same as other
+                        auto shape_ = at(shapes, shape_idx);
+                        // size of this shape for this axis
+                        auto size_  = tuple_at(shape_, shape_index);
+                        // since current shape exists, make sure to check
+                        auto current_size_ = at(broadcasted,1_ct);
+                        using common_t = std::common_type_t<decltype(size_),decltype(current_size_)>;
+                        // valid if either size_==current_size_ or either size_ or current_size_ is 1
+                        if ((size_==current_size_) || (current_size_==1) || (size_==1))
+                            at(broadcasted,1_ct) = std::max(static_cast<common_t>(size_),static_cast<common_t>(current_size_));
+                        else
+                            success = false;
+                    }
+                }); // template_for
+                if (success)
+                    at(res,si) = at(broadcasted,1_ct);
             }
         }; // broadcast_shape_impl
         for (int i=0; i<size(res); i++) {
