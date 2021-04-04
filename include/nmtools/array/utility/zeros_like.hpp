2d9f904a3ebb336adda54748d4a1d64c48a98b21
Fahri Ali Rahman <fahri@nodeflux.io>
2021-04-04 21:54:48 +0700

    add apply_resize utility function

--- include/nmtools/array/utility/zeros_like.hpp
+++ include/nmtools/array/utility/zeros_like.hpp
@@ -6,6 +6,7 @@
 
 #include "nmtools/array/utility/at.hpp"     // at
 #include "nmtools/array/utility/apply_at.hpp"
+#include "nmtools/array/utility/apply_resize.hpp"
 #include "nmtools/array/shape.hpp"  // matrix_size, vector_size
 
 #include "nmtools/traits.hpp"
@@ -16,41 +17,6 @@
 
 namespace nmtools
 {
-    namespace detail 
-    {
-        template <typename array_t, typename size_type, typename...size_types>
-        auto resize(array_t& array, size_type size_0, size_types...size_n) -> array_t&
-        {
-            if constexpr (meta::is_resizeable_nd_v<array_t,size_type,size_types...>)
-                array.resize(size_0,size_n...);
-            else {
-                array.resize(size_0);
-                for (auto& a : array)
-                    a = resize(a,size_n...);
-            }
-            return array;
-        } // resize
-
-        template <typename array_t, typename shape_t, size_t...Is>
-        auto apply_resize(array_t& array, const shape_t& shape, std::index_sequence<Is...>) -> array_t&
-        {
-            return resize(array, std::get<Is>(shape)...);
-        } // apply_resize
-
-        template <typename array_t, typename shape_t>
-        auto apply_resize(array_t& array, const shape_t& shape) -> array_t&
-        {
-            if constexpr (meta::has_tuple_size_v<shape_t>) {
-                constexpr auto n = std::tuple_size_v<shape_t>;
-                return apply_resize(array,shape,std::make_index_sequence<n>{});
-            }
-            else {
-                array.resize(shape);
-                return array;
-            }
-        } // apply_resize
-    } // namespace detail
-
     struct zeros_like_t {};
 
     template <typename array_t>
