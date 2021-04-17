6d422c0d492fe85f1ac0ea1cebcc70d9a5bd9579
Fahri Ali Rahman <fahri@nodeflux.io>
2021-04-18 00:48:31 +0700

    remove ndindex_t specialization on std::array

--- include/nmtools/array/index/ndindex.hpp
+++ include/nmtools/array/index/ndindex.hpp
@@ -2,8 +2,9 @@
 #define NMTOOLS_ARRAY_INDEX_NDINDEX_HPP
 
 #include "nmtools/array/detail.hpp"
+#include "nmtools/array/index/product.hpp"
+#include "nmtools/array/index/compute_strides.hpp"
 #include "nmtools/array/meta.hpp"
-#include "nmtools/traits.hpp"
 #include "nmtools/meta.hpp"
 
 #include "nmtools/array/utility/at.hpp"
@@ -20,14 +21,13 @@ namespace nmtools::index
      * 
      * @tparam shape_t 
      * @tparam typename 
-     * @todo remove, unify with fixed-dim version
      */
     template <typename shape_t, typename=void>
     struct ndindex_t
     {
-        using shape_type = shape_t;
-        using stride_type = shape_t;
-        using size_type = typename shape_type::value_type;
+        using shape_type = const shape_t&;
+        using stride_type = meta::remove_cvref_t<decltype(compute_strides(std::declval<shape_t>()))>;
+        using size_type = meta::get_element_or_common_type_t<shape_t>;
 
         shape_type shape;
         stride_type stride;
@@ -38,7 +38,7 @@ namespace nmtools::index
          * @param shape original array shape
          */
         constexpr ndindex_t(shape_type shape)
-            : shape(shape), stride(array::detail::compute_strides(shape))
+            : shape(shape), stride(compute_strides(shape))
         {
             // @note: this prevents ndindex_t to be used in constexpr
             // gcc error: must be initialized by mem-initializer
@@ -52,11 +52,7 @@ namespace nmtools::index
          */
         constexpr decltype(auto) size() const noexcept
         {
-            // compute product
-            auto identity = static_cast<size_type>(1);
-            for (const auto s : shape)
-                identity *= s;
-            return identity;
+            return product(shape);
         } // size
 
         /**
@@ -65,77 +61,18 @@ namespace nmtools::index
          * @param i flat index
          * @return auto 
          */
-        constexpr inline decltype(auto) operator[](size_t i)
+        constexpr inline decltype(auto) operator[](size_t i) const
         {
             using array::detail::compute_indices;
-            // map offset (flat index) back to indices
-            return compute_indices(i,shape,stride);
-        } // operator[]
-    }; // ndindex_t
-
-    /**
-     * @brief indices transformer
-     * 
-     * Adapter from N-dimensional indices to packed 1-D indices,
-     * can be used to transform nested N loop to 1 flat loop.
-     * Only support fixed-dimensional array for now.
-     * 
-     * @tparam array_t compile-time array type to represent shape_type and stride_type
-     * @tparam T
-     * @tparam N number of dimension
-     * @see nmtools::apply_at
-     */
-    template <template<typename,size_t> typename array_t, typename T, size_t N>
-    struct ndindex_t<array_t<T,N>>
-    {
-        using shape_type = array_t<T,N>;
-        using stride_type = array_t<T,N>;
-        using size_type = typename shape_type::value_type;
-
-        shape_type shape;
-        stride_type stride;
-
-        /**
-         * @brief Construct a new indices pack t object
-         * 
-         * @param shape original array shape
-         */
-        constexpr ndindex_t(shape_type shape)
-            : shape(shape), stride(array::detail::compute_strides(shape))
-        {
-            // @note: this prevents ndindex_t to be used in constexpr
-            // gcc error: must be initialized by mem-initializer
-            // stride = array::detail::compute_strides(shape);
-        }
-
-        /**
-         * @brief return the number of element
-         * 
-         * @return auto 
-         */
-        constexpr decltype(auto) size() const noexcept
-        {
-            // compute product
-            auto identity = static_cast<size_type>(1);
-            meta::template_for<N>([&](auto index){
-                constexpr auto i = decltype(index)::value;
-                identity *= std::get<i>(shape);
-            });
-            return identity;
-        } // size
-
-        /**
-         * @brief get the packed indices from flat indices
-         * 
-         * @param i flat index
-         * @return auto 
-         */
-        constexpr inline decltype(auto) operator[](size_t i)
-        {
-            using array::detail::compute_indices;
-            using detail::as_tuple;
-            // map offset (flat index) back to indices
-            return as_tuple(compute_indices(i,shape,stride));
+            if constexpr (meta::is_fixed_index_array_v<shape_t>) {
+                using detail::as_tuple;
+                // map offset (flat index) back to indices
+                return as_tuple(compute_indices(i,shape,stride));
+            }
+            else {
+                // map offset (flat index) back to indices
+                return compute_indices(i,shape,stride);
+            }
         } // operator[]
     }; // ndindex_t
 
@@ -149,13 +86,9 @@ namespace nmtools::index
      * @see ndindex_t
      */
     template <typename shape_t>
-    constexpr auto ndindex(shape_t shape)
+    constexpr auto ndindex(const shape_t& shape)
     {
-        if constexpr (meta::is_specialization_v<shape_t,std::tuple> || meta::is_specialization_v<shape_t,std::pair>) {
-            auto shape_ = detail::make_array<std::array>(shape);
-            return ndindex_t(shape_);
-        }
-        else return ndindex_t(shape);
+        return ndindex_t(shape);
     } // ndindex
 }
 
