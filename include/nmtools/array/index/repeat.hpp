94f3385b41e9c1db69c1a2366313eb6d7019d512
Fahri Ali Rahman <fahri@nodeflux.io>
2021-05-30 20:47:16 +0700

    move `index::shape_repeat` from shape_repeat.hpp to repeat.hpp and update `index::repeat` return type resolver

--- include/nmtools/array/index/repeat.hpp
+++ include/nmtools/array/index/repeat.hpp
@@ -9,8 +9,101 @@
 #include "nmtools/array/index/compute_indices.hpp"
 #include "nmtools/array/index/argfilter.hpp"
 #include "nmtools/array/index/cumsum.hpp"
+#include "nmtools/array/index/product.hpp"
+#include "nmtools/array/index/sum.hpp"
 
 #include <type_traits>
+#include <array>
+
+namespace nmtools::index
+{
+    /**
+     * @brief specific tag to resolve repeat return type
+     * 
+     */
+    struct shape_repeat_t {};
+
+    /**
+     * @brief Compute the shape of a repeat view.
+     * 
+     * @tparam shape_t 
+     * @tparam repeats_t 
+     * @tparam axis_t 
+     * @param shape shape of src array
+     * @param repeats number of repetitions of each elements
+     * @param axis 
+     * @return constexpr auto 
+     */
+    template <typename shape_t, typename repeats_t, typename axis_t>
+    constexpr auto shape_repeat(const shape_t shape, const repeats_t& repeats, axis_t axis)
+    {
+        using return_t = meta::resolve_optype_t<shape_repeat_t,shape_t,repeats_t,axis_t>;
+        static_assert (meta::is_index_array_v<return_t>
+            , "unsupported shape_repeat, maybe specialization of resolve_optype_t<shape_repeat_t,...> required"
+        );
+        auto ret = return_t {};
+
+        // when axis is None, repeat the flattened array
+        // so the resulting shape is 1-dimensional
+        if constexpr (is_none_v<axis_t>) {
+            auto p = product(shape);
+            at(ret,0) = p * repeats;
+        }
+        else {
+            auto n = len(shape);
+
+            if constexpr (meta::is_resizeable_v<return_t>)
+                ret.resize(n);
+            
+            for (size_t i=0; i<n; i++)
+                at(ret,i) = tuple_at(shape,i);
+
+            if constexpr (meta::is_array1d_v<repeats_t>) {
+                assert ( at(ret,axis) == vector_size(repeats)
+                    // , "unsupported shape_repeat"
+                    // numpy: ValueError: operands could not be broadcast together with shape
+                );
+                at(ret,axis) = sum(repeats);
+            }
+            else
+                at(ret,axis) = at(ret,axis) * repeats;
+        }
+
+        return ret;
+    } // repeat
+} // namespace nmtools::index
+
+namespace nmtools::meta
+{
+    /**
+     * @brief resolve return type for index::shape_repeat op
+     * 
+     * @tparam shape_t 
+     * @tparam repeats_t 
+     * @tparam axis_t 
+     */
+    template <typename shape_t, typename repeats_t, typename axis_t>
+    struct resolve_optype <
+        void, index::shape_repeat_t, shape_t, repeats_t, axis_t
+    >
+    {
+        using type = tuple_to_array_t<shape_t>;
+    }; // resolve_optype
+
+    /**
+     * @brief resolve return type for index::shape_repeat op
+     * 
+     * @tparam shape_t 
+     * @tparam repeats_t 
+     */
+    template <typename shape_t, typename repeats_t>
+    struct resolve_optype <
+        void, index::shape_repeat_t, shape_t, repeats_t, none_t
+    >
+    {
+        using type = std::array<size_t,1>;
+    }; // resolve_optype
+} // namespace nmtools::meta
 
 namespace nmtools::index
 {
@@ -21,7 +114,7 @@ namespace nmtools::index
     struct repeat_t {};
 
     /**
-     * @brief transform indices from repeated view to indices for origin shape
+     * @brief Transform indices from repeated view (dst) to indices for origin shape (src).
      * 
      * @tparam shape_t 
      * @tparam indices_t 
@@ -29,23 +122,27 @@ namespace nmtools::index
      * @tparam axis_t 
      * @param shape original shape
      * @param indices indices from repeat view
-     * @param repeats
-     * @param axis 
+     * @param repeats number of repetitions
+     * @param axis the axis along which to repeat values
      * @return constexpr auto 
      */
     template <typename shape_t, typename indices_t, typename repeats_t, typename axis_t>
     constexpr auto repeat(const shape_t& shape, const indices_t& indices, const repeats_t& repeats, axis_t axis)
     {
         using return_t = meta::resolve_optype_t<repeat_t,shape_t,indices_t,repeats_t,axis_t>;
+        static_assert (meta::is_index_array_v<return_t>
+            , "unsupported index::repeat, could not deduce return type" );
         auto ret = return_t {};
 
+        // following numpy, when axis is None, repeat the flattened array
+        // and indices must be integral type or array with size exactly 1
         if constexpr (is_none_v<axis_t>) {
             auto i = [&](){
                 if constexpr (std::is_integral_v<indices_t>)
                     return indices / repeats;
                 else {
                     static_assert (std::tuple_size_v<indices_t> == 1
-                        , "unsupported index::repeat"
+                        , "unsupported index::repeat, expect array with size of 1 when axis is None"
                     );
                     return std::get<0>(indices) / repeats;
                 }
@@ -53,7 +150,7 @@ namespace nmtools::index
             ret = compute_indices(i,shape);
         }
         else {
-            auto n = vector_size(indices);
+            auto n = len(indices);
             if constexpr (meta::is_resizeable_v<return_t>)
                 ret.resize(n);
             for (size_t i=0; i<n; i++) {
@@ -83,29 +180,29 @@ namespace nmtools::index
 namespace nmtools::meta
 {
     /**
-     * @brief resolve return type for repeat op
+     * @brief resolve return type for repeat op that transform indices from view (dst) to src
+     * follow shape_t
      * 
      * @tparam shape_t 
      * @tparam indices_t 
      * @tparam repeats_t 
      */
-    template <typename shape_t, typename indices_t, typename repeats_t>
-    struct resolve_optype<
-        void, index::repeat_t, shape_t, indices_t, repeats_t, none_t
-    >
-    {
-        using type = std::conditional_t<
-            std::is_integral_v<repeats_t> && (has_tuple_size_v<indices_t> || std::is_integral_v<indices_t>),
-            resolve_optype_t<index::compute_indices_t,indices_t,shape_t>, void
-        >;
-    }; // resolve_optype
-
     template <typename shape_t, typename indices_t, typename repeats_t, typename axis_t>
     struct resolve_optype<
         void, index::repeat_t, shape_t, indices_t, repeats_t, axis_t
     >
     {
-        using type = transform_bounded_array_t<tuple_to_array_t<indices_t>>;
+        static constexpr auto vtype = [](){
+            using type = transform_bounded_array_t<tuple_to_array_t<shape_t>>;
+            // make sure type is index array
+            if constexpr (!is_index_array_v<type>) {
+                // assume the index type is size_t
+                using result_t = replace_element_type_t<type,size_t>;
+                return as_value_v<result_t>;
+            }
+            else return as_value_v<type>;
+        }();
+        using type = type_t<remove_cvref_t<decltype(vtype)>>;
     }; // resolve_optype
 } // namespace nmtools::meta
 
