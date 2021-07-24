ce763caac000037e0aee109fd0afba879bd5ee0d
Fahri Ali Rahman <fahri@nodeflux.io>
2021-07-25 00:02:10 +0700

    add eager eval for arange, atleast_1d, 2d, and 3d view

---
+++ include/nmtools/array/array/atleast_3d.hpp
@@ -0,0 +1,30 @@
+#ifndef NMTOOLS_ARRAY_ARRAY_ATLEAST_1D_HPP
+#define NMTOOLS_ARRAY_ARRAY_ATLEAST_1D_HPP
+
+#include "nmtools/array/view/atleast_3d.hpp"
+#include "nmtools/array/eval.hpp"
+
+namespace nmtools::array
+{
+    /**
+     * @brief Eagerly compute atleast_3d.
+     * 
+     * @tparam output_t 
+     * @tparam context_t 
+     * @tparam array_t 
+     * @param array   Input array
+     * @param context Evaluation context
+     * @param output 
+     * @return constexpr auto 
+     */
+    template <typename output_t=none_t, typename context_t=none_t,
+        typename array_t>
+    constexpr auto atleast_3d(const array_t& array,
+        context_t&& context=context_t{}, output_t&& output=output_t{})
+    {
+        auto viewed = view::atleast_3d(array);
+        return eval(viewed,context,output);
+    } // atleast_3d
+} // namespace nmtools::array
+
+#endif // NMTOOLS_ARRAY_ARRAY_ATLEAST_1D_HPP
