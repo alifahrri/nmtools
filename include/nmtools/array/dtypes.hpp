7c9eaf90f173afd2dfa900da06cb36d6eea19a01
Fahri Ali Rahman <fahri@nodeflux.io>
2021-08-23 23:39:33 +0700

    minor restructuring of dtype

--- include/nmtools/array/dtypes.hpp
+++ include/nmtools/array/dtypes.hpp
@@ -25,18 +25,31 @@ namespace nmtools
     template <typename T>
     constexpr inline auto is_dtype_v = is_dtype<T>::value;
 
+    namespace dtype {
+        using float32_t = dtype_t<float>;
+        using float64_t = dtype_t<double>;
+        using int8_t    = dtype_t<std::int8_t>;
+        using int16_t   = dtype_t<std::int16_t>;
+        using int32_t   = dtype_t<std::int32_t>;
+        using int64_t   = dtype_t<std::int64_t>;
+        using uint8_t   = dtype_t<std::uint8_t>;
+        using uint16_t  = dtype_t<std::uint16_t>;
+        using uint32_t  = dtype_t<std::uint32_t>;
+        using uint64_t  = dtype_t<std::uint64_t>;
+    } // dtype
+
     // inline variables
     // can be used to specify dtype for ufuncs
-    constexpr inline auto float32 = dtype_t<float>    {};
-    constexpr inline auto float64 = dtype_t<double>   {};
-    constexpr inline auto int8    = dtype_t<int8_t>   {};
-    constexpr inline auto int16   = dtype_t<int16_t>  {};
-    constexpr inline auto int32   = dtype_t<int32_t>  {};
-    constexpr inline auto int64   = dtype_t<int64_t>  {};
-    constexpr inline auto uint8   = dtype_t<uint8_t>  {};
-    constexpr inline auto uint16  = dtype_t<uint16_t> {};
-    constexpr inline auto uint32  = dtype_t<uint32_t> {};
-    constexpr inline auto uint64  = dtype_t<uint64_t> {};
+    constexpr inline auto float32 = dtype::float32_t {};
+    constexpr inline auto float64 = dtype::float64_t {};
+    constexpr inline auto int8    = dtype::int8_t    {};
+    constexpr inline auto int16   = dtype::int16_t   {};
+    constexpr inline auto int32   = dtype::int32_t   {};
+    constexpr inline auto int64   = dtype::int64_t   {};
+    constexpr inline auto uint8   = dtype::uint8_t   {};
+    constexpr inline auto uint16  = dtype::uint16_t  {};
+    constexpr inline auto uint32  = dtype::uint32_t  {};
+    constexpr inline auto uint64  = dtype::uint64_t  {};
 
     /**
      * @brief Helper metafunction to get the type
