fd203b000f5afe33a9589a62f01f009c234c338f
Fahri Ali Rahman <fahri@nodeflux.io>
2021-11-21 16:00:06 +0700

    update tests

--- tests/src/doctest/meta/array/index/logical_not.cpp
+++ tests/src/doctest/meta/array/index/logical_not.cpp
@@ -15,7 +15,8 @@ TEST_CASE("logical_not" * doctest::test_suite("index"))
     {
         using x_t = decltype(tuple{true_type{},false_type{},false_type{},true_type{},false_type{}});
         using res_t = meta::resolve_optype_t<logical_not_t,x_t>;
-        using exp_t = decltype(tuple{false_type{},true_type{},true_type{},false_type{},true_type{}});
+        // using exp_t = decltype(tuple{meta::false_type{},meta::true_type{},meta::true_type{},meta::false_type{},meta::true_type{}});
+        using exp_t = decltype(tuple{meta::integral_constant<bool,false>{},meta::integral_constant<bool,true>{},meta::integral_constant<bool,true>{},meta::integral_constant<bool,false>{},meta::integral_constant<bool,true>{}});
         NMTOOLS_STATIC_CHECK_IS_SAME( res_t, exp_t );
     }
 }
---
+++ tests/src/doctest/meta/forward.cpp
@@ -0,0 +1,64 @@
+#include "nmtools/utility/forward.hpp"
+#include "nmtools/testing/doctest.hpp"
+
+namespace nm = nmtools;
+namespace meta = nm::meta;
+
+struct A {};
+
+A foo() { return A{}; }
+const A const_foo() { return A{}; }
+
+TEST_CASE("forward" * doctest::test_suite("meta"))
+{
+    auto a = A{};
+    const auto ca = A{};
+
+    {
+        using result_t = decltype(nm::forward<A&>(a));
+        using expect_t = A&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+    {
+        using result_t = decltype(nm::forward<A>(a));
+        using expect_t = A&&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+    {
+        using result_t = decltype(nm::forward<A>(foo()));
+        using expect_t = A&&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+
+    {
+        using result_t = decltype(nm::forward<const A&>(a));
+        using expect_t = const A&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+    {
+        using result_t = decltype(nm::forward<const A>(a));
+        using expect_t = const A&&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+    {
+        using result_t = decltype(nm::forward<const A>(foo()));
+        using expect_t = const A&&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+
+    {
+        using result_t = decltype(nm::forward<const A&>(ca));
+        using expect_t = const A&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+    {
+        using result_t = decltype(nm::forward<const A>(ca));
+        using expect_t = const A&&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+    {
+        using result_t = decltype(nm::forward<const A>(const_foo()));
+        using expect_t = const A&&;
+        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+    }
+}
