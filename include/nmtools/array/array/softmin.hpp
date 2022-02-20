99caa21e2190a7d0fce3f3ed0f8e8c04e55d9818
Fahri Ali Rahman <fahri@nodeflux.io>
2022-02-20 12:41:05 +0700

    add softmin view & eval

---
+++ include/nmtools/array/array/softmin.hpp
@@ -0,0 +1,32 @@
+#ifndef NMTOOLS_ARRAY_ARRAY_SOFTMIN_HPP
+#define NMTOOLS_ARRAY_ARRAY_SOFTMIN_HPP
+
+#include "nmtools/array/view/softmin.hpp"
+#include "nmtools/array/eval.hpp"
+
+namespace nmtools::array
+{
+    /**
+     * @brief Eagerly computes softmin function along an axis.
+     * 
+     * @tparam output_t 
+     * @tparam context_t=none_t 
+     * @tparam input_t 
+     * @tparam dim_t 
+     * @param input     input array
+     * @param dim       compute axis
+     * @param context 
+     * @param output 
+     * @return constexpr auto 
+     */
+    template <typename output_t=none_t, typename context_t=none_t
+        , typename input_t, typename dim_t>
+    constexpr auto softmin(const input_t& input, dim_t dim
+        , context_t&& context=context_t{}, output_t&& output=output_t{})
+    {
+        auto softmin_ = view::softmin(input,dim);
+        return eval(softmin_,nmtools::forward<context_t>(context),nmtools::forward<output_t>(output));
+    } // softmin
+} // namespace nmtools::array
+
+#endif // NMTOOLS_ARRAY_ARRAY_SOFTMIN_HPP
