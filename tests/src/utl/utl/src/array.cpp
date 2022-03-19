3a73e0dfc0e6bb2214c3ba783b359c599aaad981
Fahri Ali Rahman <fahri@nodeflux.io>
2022-03-19 18:47:17 +0700

    try to run pyperf system tune before testbench

--- tests/src/utl/utl/src/array.cpp
+++ tests/src/utl/utl/src/array.cpp
@@ -34,13 +34,13 @@ TEST_CASE("array" * doctest::test_suite("utl"))
         {
             constexpr auto array = utl::array{1,2,3};
 
-            #ifdef __clang__
+            // #ifdef __clang__
             const auto& a = utl::get<0>(array);
             const auto& b = utl::get<1>(array);
             const auto& c = utl::get<2>(array);
-            #else
-            const auto& [a,b,c] = array;
-            #endif
+            // #else
+            // const auto& [a,b,c] = array;
+            // #endif
             NMTOOLS_STATIC_ASSERT_EQUAL( array.size(), 3 );
             CHECK( a == 1 );
             CHECK( b == 2 );
@@ -52,13 +52,13 @@ TEST_CASE("array" * doctest::test_suite("utl"))
             auto new_array = utl::array{array};
 
 
-            #ifdef __clang__
+            // #ifdef __clang__
             const auto& a = utl::get<0>(new_array);
             const auto& b = utl::get<1>(new_array);
             const auto& c = utl::get<2>(new_array);
-            #else
-            const auto& [a,b,c] = new_array;
-            #endif
+            // #else
+            // const auto& [a,b,c] = new_array;
+            // #endif
             NMTOOLS_STATIC_ASSERT_EQUAL( array.size(), 3 );
             CHECK( a == 1 );
             CHECK( b == 2 );
