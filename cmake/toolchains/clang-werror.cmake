446a845c162ce5e8ee090c6e51753aad84ba27dc
Fahri Ali Rahman <fahri@nodeflux.io>
2022-03-20 18:49:57 +0700

    add gnu string literal operator template

--- cmake/toolchains/clang-werror.cmake
+++ cmake/toolchains/clang-werror.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-add_compile_options(-W -Wall -Werror -Wextra)
+add_compile_options(-W -Wall -Werror -Wextra -Wno-gnu-string-literal-operator-template)
