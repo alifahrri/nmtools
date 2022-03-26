1b4869f8d7160bc91db70aeb1b8c7b9329cb8ac9
Fahri Ali Rahman <fahri@nodeflux.io>
2022-03-26 23:34:56 +0700

    update CI scripts & ignore files

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -14,6 +14,11 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    -c|--coverage-dir)
+    COVERAGE_DIR="$2"
+    shift # past argument
+    shift # past value
+    ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
@@ -26,18 +31,22 @@ if [ -z "$BUILD_DIR" -a "$BUILD_DIR" != " " ]; then
         BUILD_DIR="$DIR/../build/"
 fi
 
+if [ -z "$COVERAGE_DIR" -a "$COVERAGE_DIR" != " " ]; then
+        COVERAGE_DIR="$BUILD_DIR/coverage/"
+fi
+
 set -e
 
 ## assuming this script is located in root/scripts & build directory in root/scripts
-NMTOOLS_TESTBENCH_EPOCHS=3 NMTOOLS_TESTBENCH_WARMUP=1 NMTOOLS_TESTBENCH_QUIET=1 $BUILD_DIR/tests/numeric-tests-doctest \
-    && $BUILD_DIR/tests/numeric-tests-doctest-optional \
-    && $BUILD_DIR/tests/numeric-tests-doctest-exception \
-    && $BUILD_DIR/tests/src/meta/numeric-tests-doctest-meta \
-    && $BUILD_DIR/tests/src/utl/meta/numeric-tests-utl-meta \
-    && $BUILD_DIR/tests/src/utl/array/numeric-tests-utl-array \
-    && $BUILD_DIR/tests/src/utl/utl/numeric-tests-utl
+NMTOOLS_TESTBENCH_EPOCHS=3 NMTOOLS_TESTBENCH_WARMUP=1 NMTOOLS_TESTBENCH_QUIET=1 \
+       $BUILD_DIR/tests/array/numeric-tests-doctest \
+    && $BUILD_DIR/tests/meta/numeric-tests-doctest-meta \
+    && $BUILD_DIR/tests/utl/meta/numeric-tests-utl-meta \
+    && $BUILD_DIR/tests/utl/array/numeric-tests-utl-array \
+    && $BUILD_DIR/tests/utl/utl/numeric-tests-utl
 
-gcovr -r $DIR/../include/ $BUILD_DIR --html-details=build/nmtools-coverage.html
+mkdir -p ${COVERAGE_DIR}
+gcovr -r $DIR/../include/nmtools $BUILD_DIR --html-details=${COVERAGE_DIR}/nmtools-coverage.html
 
 cd $DIR/..
 if [[ ! -z ${CODECOV_TOKEN} ]]; then
