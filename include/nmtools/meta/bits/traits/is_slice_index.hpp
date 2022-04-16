a4454f54736b7953e395a42d32ad541780c21a01
Fahri Ali Rahman <fahri@nodeflux.io>
2022-04-16 17:56:37 +0700

    specialize is_slice_index for tuple of nones

--- include/nmtools/meta/bits/traits/is_slice_index.hpp
+++ include/nmtools/meta/bits/traits/is_slice_index.hpp
@@ -17,11 +17,16 @@ namespace nmtools::meta
     struct is_slice_index
     {
         static constexpr auto value = [](){
-            // TODO: also deduce "all" aka ":" aka tuple{None,None} as slice_index
             // NOTE: Ellipsis can also be used as ":" but must only single ellipsis is allowed
             return is_index_v<T> || is_ellipsis_v<T>;
         }();
     }; // is_slice_index
+
+    template <template<typename...>typename tuple>
+    struct is_slice_index<tuple<none_t,none_t>> : true_type {};
+
+    template <template<typename...>typename tuple>
+    struct is_slice_index<tuple<none_t,none_t,none_t>> : true_type {};
 
     /**
      * @brief Helper inline variable to check if type T is slicing index.
