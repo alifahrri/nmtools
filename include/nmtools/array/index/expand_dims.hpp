51383a5c89b582da6678bcdd1fe6509c90d635e3
Fahri Ali Rahman <fahri@nodeflux.io>
2022-07-02 12:33:49 +0700

    handle constant index array for index expand_dims

--- include/nmtools/array/index/expand_dims.hpp
+++ include/nmtools/array/index/expand_dims.hpp
@@ -8,6 +8,11 @@
 #include "nmtools/utils/isequal.hpp"
 #include "nmtools/array/ndarray/hybrid.hpp"
 
+// TODO: move to shape.hpp
+#ifdef NMTOOLS_ENABLE_BOOST
+#include "nmtools/array/impl/boost.hpp"
+#endif
+
 namespace nmtools::index
 {
     /**
@@ -59,7 +64,7 @@ namespace nmtools::index
                 auto in_axis = [&](){
                     if constexpr (meta::is_index_array_v<axes_t>)
                         return contains(axes,i);
-                    else return i == axes;
+                    else return i == (size_t)axes;
                 }();
                 at(newshape,i) = (in_axis ? 1 : tuple_at(shape,idx));
                 idx += (!in_axis ? 1 : 0);
@@ -77,20 +82,16 @@ namespace nmtools::index
      * @return constexpr auto 
      */
     template <typename shape_t, typename axes_t>
-    constexpr auto expand_dims(const shape_t shape, const axes_t& axes)
+    constexpr auto expand_dims([[maybe_unused]] const shape_t shape, [[maybe_unused]] const axes_t& axes)
     {
-        if constexpr (meta::is_index_v<axes_t>) {
-            using index_t = meta::make_array_type_t<axes_t,1>;
-            auto axes_    = index_t{axes};
-            return expand_dims(shape,axes_);
-        } else {
-            using return_t = meta::resolve_optype_t<expand_dims_t,shape_t,axes_t>;
-            auto newshape = return_t{};
+        using return_t = meta::resolve_optype_t<expand_dims_t,shape_t,axes_t>;
+        auto newshape = return_t{};
 
+        if constexpr (!meta::is_constant_index_array_v<return_t>) {
             impl::expand_dims(newshape, shape, axes);
-            
-            return newshape;
         }
+
+        return newshape;
     } // expand_dims
 } // namespace nmtools::index
 
@@ -98,11 +99,69 @@ namespace nmtools
 {
     namespace meta
     {
+
+        namespace error
+        {
+            template <typename...>
+            struct EXPAND_DIMS_UNSUPPORTED : detail::fail_t {};
+        } // namespace error
+        
         template <typename shape_t, typename axes_t>
         struct resolve_optype<
             void, index::expand_dims_t, shape_t, axes_t
         >
         {
+            static constexpr auto vtype = [](){
+                if constexpr (is_constant_index_array_v<shape_t> && (is_constant_index_v<axes_t> || is_constant_index_array_v<axes_t>)) {
+                    constexpr auto shape = to_value_v<shape_t>;
+                    constexpr auto axes  = to_value_v<axes_t>;
+                    constexpr auto newshape = index::expand_dims(shape,axes);
+                    return template_reduce<nmtools::len(newshape)-1>([&](auto init, auto index){
+                        using init_t = type_t<decltype(init)>;
+                        constexpr auto I = decltype(index)::value + 1;
+                        using result_t = append_type_t<init_t,ct<nmtools::at(newshape,I)>>;
+                        return as_value_v<result_t>;
+                    }, as_value_v<nmtools_tuple<ct<nmtools::at(newshape,0)>>>);
+                } else if constexpr (is_fixed_index_array_v<shape_t> && (is_index_v<axes_t> || is_fixed_index_array_v<axes_t>)) {
+                    constexpr auto n_axes = [](){
+                        if constexpr (is_index_v<axes_t>) {
+                            return 1;
+                        } else {
+                            return len_v<axes_t>;
+                        }
+                    }();
+                    constexpr auto newdim = len_v<shape_t> + n_axes;
+                    // TODO: try to resize instead of create new type
+                    return as_value_v<nmtools_array<get_element_or_common_type_t<shape_t>,newdim>>;
+                } else if constexpr ((is_hybrid_index_array_v<shape_t> || is_fixed_index_array_v<shape_t>) && (is_index_v<axes_t> || is_fixed_index_array_v<axes_t> || is_hybrid_index_array_v<axes_t>)) {
+                    constexpr auto n_max_axes = [](){
+                        if constexpr (is_index_v<axes_t>) {
+                            return 1;
+                        } else if constexpr (is_fixed_index_array_v<axes_t>) {
+                            return len_v<axes_t>;
+                        } else /* if constexpr (is_bounded_size_v<axes_t>) */ {
+                            return bounded_size_v<axes_t>;
+                        }
+                    }();
+                    constexpr auto max_dim = bounded_size_v<shape_t> + n_max_axes;
+                    using index_t = get_element_or_common_type_t<shape_t>;
+                    // TODO: try to resize instead of create new type
+                    return as_value_v<nmtools_hybrid_ndarray<index_t,max_dim,1>>;
+                } else if constexpr (is_index_array_v<shape_t> && is_index_v<axes_t>) {
+                    return as_value_v<shape_t>;
+                } else if constexpr ((is_fixed_index_array_v<shape_t> || is_hybrid_index_array_v<shape_t>) && is_index_array_v<axes_t>) {
+                    using index_t = get_element_or_common_type_t<shape_t>;
+                    return as_value_v<replace_element_type_t<axes_t,index_t>>;
+                } else if constexpr (is_index_array_v<shape_t> && is_index_array_v<axes_t>) {
+                    return as_value_v<shape_t>;
+                } else {
+                    return as_value_v<error::EXPAND_DIMS_UNSUPPORTED<shape_t,axes_t>>;
+                }
+            }();
+            using type = type_t<decltype(vtype)>;
+
+            // TODO: remove
+            #if 0
             // deduce the type of expand_dim ops prefer dynamic index array when possible,
             // otherwise select hybrid index, choose fixed index only if both are fixed 
             static constexpr auto vtype = [](){
@@ -159,6 +218,7 @@ namespace nmtools
                 transform_bounded_array_t<
                     type_t<remove_cvref_t<decltype(vtype)>>>
             >;
+            #endif
         }; // resolve_optype
     } // namespace meta
 }
