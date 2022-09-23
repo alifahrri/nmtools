b47da8ef6cea9249b100140ac154bbea6d171b3c
Fahri Ali Rahman <fahri@nodeflux.io>
2022-09-23 18:02:04 +0700

    cleanup ones shape metafunctions

--- include/nmtools/array/array/ones.hpp
+++ include/nmtools/array/array/ones.hpp
@@ -19,13 +19,17 @@ namespace nmtools::array
      * @param output 
      * @return constexpr auto 
      */
-    template <typename output_t=none_t, typename context_t=none_t,
+    template <typename output_t=none_t, typename context_t=none_t, typename resolver_t=eval_result_t,
         typename shape_t, typename T>
     constexpr auto ones(const shape_t& shape, dtype_t<T> dtype,
-        context_t&& context=context_t{}, output_t&& output=output_t{})
+        context_t&& context=context_t{}, output_t&& output=output_t{},meta::as_value<resolver_t> resolver=meta::as_value_v<resolver_t>)
     {
         auto a = view::ones(shape,dtype);
-        return eval(a,context,output);
+        return eval(a
+            ,nmtools::forward<context_t>(context)
+            ,nmtools::forward<output_t>(output)
+            ,resolver
+        );
     } // ones
 } // namespace nmtools::array
 
