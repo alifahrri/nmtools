1429844e5f2d03bd1fb5ca6db833e6fc64e84690
Fahri Ali Rahman <fahri@nodeflux.io>
2022-10-23 21:22:20 +0700

    remove unused

--- tests/include/testing/doctest.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TESTING_DOCTEST_HPP
-#define TESTING_DOCTEST_HPP
-
-#include "nmtools/testing/doctest.hpp"
-
-#endif // TESTING_DOCTEST_HPP
--- tests/include/testing/testing.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef TESTING_HPP
-#define TESTING_HPP
-
-#include "nmtools/testing/testing.hpp"
-
-#endif // TESTING_HPP
