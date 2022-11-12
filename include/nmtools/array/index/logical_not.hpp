8dd039e96bb2388a44562cb5bfe3efa98092d588
Fahri Ali Rahman <fahri@nodeflux.io>
2022-11-12 18:34:27 +0700

    carry type args for INDEX_LOGICAL_UNSUPPORTED

--- include/nmtools/array/index/logical_not.hpp
+++ include/nmtools/array/index/logical_not.hpp
@@ -44,7 +44,8 @@ namespace nmtools::meta
 {
     namespace error
     {
-        struct INDEX_LOGICAL_NOT_UNSUPPORTED : detail::fail_t{};
+        template <typename...>
+        struct INDEX_LOGICAL_UNSUPPORTED : detail::fail_t{};
     } // namespace error
 
     template <typename array_t>
@@ -73,7 +74,7 @@ namespace nmtools::meta
             } else if constexpr (is_index_array_v<array_t>) {
                 return as_value_v<array_t>;
             } else {
-                return as_value_v<error::INDEX_LOGICAL_NOT_UNSUPPORTED>;
+                return as_value_v<error::INDEX_LOGICAL_UNSUPPORTED<array_t>>;
             }
         }();
         using type = type_t<decltype(vtype)>;
