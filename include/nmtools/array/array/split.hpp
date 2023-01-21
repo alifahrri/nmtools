f4b95d50394284cbdd87526d16f0cdc8062ee23a
Fahri Ali Rahman <fahri@nodeflux.io>
2023-01-21 16:55:29 +0700

    update split eval to accept type resolver

--- include/nmtools/array/array/split.hpp
+++ include/nmtools/array/array/split.hpp
@@ -23,12 +23,14 @@ namespace nmtools::array
          * @param output 
          * @return constexpr auto 
          */
-        template <typename output_t, typename context_t, typename views_t, template<auto...>typename index_sequence, auto...Is>
-        constexpr auto apply_eval(const views_t& views, context_t&& context, output_t&& output, index_sequence<Is...>)
+        template <typename output_t, typename context_t, typename resolver_t, typename views_t, template<auto...>typename index_sequence, auto...Is>
+        constexpr auto apply_eval(const views_t& views, context_t&& context, output_t&& output, resolver_t resolver, index_sequence<Is...>)
         {
             return nmtools_tuple{array::eval(nmtools::get<Is>(views)
                 , nmtools::forward<context_t>(context)
-                , nmtools::forward<output_t>(output))...
+                , nmtools::forward<output_t>(output)
+                , resolver)
+                ...
             };
         } // apply_eval
     } // namespace detail
@@ -44,22 +46,27 @@ namespace nmtools::array
      * @param output
      * @return constexpr auto 
      */
-    template <typename output_t=none_t, typename context_t=none_t, typename views_t>
-    constexpr auto apply_eval(const views_t& views, context_t&& context=context_t{}, output_t&& output=output_t{})
+    template <typename output_t=none_t, typename context_t=none_t, typename resolver_t=eval_result_t
+        , typename views_t>
+    constexpr auto apply_eval(const views_t& views
+        , context_t&& context=context_t{}, output_t&& output=output_t{}
+        , meta::as_value<resolver_t> resolver=meta::as_value_v<resolver_t>)
     {
         if constexpr (meta::is_list_v<views_t>) {
-            using value_type = meta::get_value_type_t<views_t>;
-            using eval_t     = decltype(eval(meta::declval<value_type>()
+            using eval_t = decltype(eval(at(views,0)
                 , nmtools::forward<context_t>(context)
-                , nmtools::forward<output_t>(output))
-            );
+                , nmtools::forward<output_t>(output)
+                , resolver
+            ));
             using result_t = meta::replace_value_type_t<views_t,eval_t>;
             auto res = result_t {};
             auto n = len(views);
             for (size_t i=0; i<n; i++) {
-                res.push_back(eval(at(views,i)
+                auto view_i = at(views,i);
+                res.push_back(eval(view_i
                     , nmtools::forward<context_t>(context)
                     , nmtools::forward<output_t>(output)
+                    , resolver
                 ));
             }
             return res;
@@ -68,6 +75,7 @@ namespace nmtools::array
             return detail::apply_eval(views
                 , nmtools::forward<context_t>(context)
                 , nmtools::forward<output_t>(output)
+                , resolver
                 , meta::make_index_sequence_v<N>
             );
         }
@@ -88,13 +96,16 @@ namespace nmtools::array
      * @param output 
      * @return constexpr auto 
      */
-    template <typename output_t=none_t, typename context_t=none_t, typename array_t, typename index_t, typename axis_t>
-    constexpr auto split(const array_t& array, const index_t& indices_or_sections, axis_t axis, context_t&& context=context_t{}, output_t&& output=output_t{})
+    template <typename output_t=none_t, typename context_t=none_t, typename resolver_t=eval_result_t
+        , typename array_t, typename index_t, typename axis_t>
+    constexpr auto split(const array_t& array, const index_t& indices_or_sections, axis_t axis
+        , context_t&& context=context_t{}, output_t&& output=output_t{}, meta::as_value<resolver_t> resolver=meta::as_value_v<resolver_t>)
     {
         auto split_ = view::split(array,indices_or_sections,axis);
         return apply_eval(split_
             , nmtools::forward<context_t>(context)
             , nmtools::forward<output_t>(output)
+            , resolver
         );
     }
 } // namespace nmtools::array
