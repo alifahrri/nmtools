3e3ec00ab0373bd75fd5d6713ff8e6adac0dab2c
Fahri Ali Rahman <fahri@nodeflux.io>
2023-07-28 21:42:47 +0700

    add simde avx512 ci

---
+++ scripts/install_simde.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ -z "${SIMDE_VERSION}" ]]; then
+    SIMDE_VERSION="v0.7.6"
+else
+    echo "set SIMDE_VERSION from env"
+fi
+
+echo "using SIMDE_VERSION=${SIMDE_VERSION}"
+
+DIR=simde
+if [[ -d "${DIR}" ]]; then
+    echo "${DIR} exists, skip cloning"
+else
+    git clone --single-branch -b ${SIMDE_VERSION} https://github.com/simd-everywhere/simde.git
+fi
+
+# sudo apt-get install -y ninja-build ninja-build python3-pip
+# pip3 install meson==0.55.0
+
+cd ${DIR}
+meson setup build --prefix=/usr/local -Dtests=false
+ninja -C build install
