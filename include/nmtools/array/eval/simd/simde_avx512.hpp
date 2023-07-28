9fc2a159ca0bd766b54f5ed6db4c8676dcbd4fa2
Fahri Ali Rahman <fahri@nodeflux.io>
2023-07-28 21:39:18 +0700

    initial avx512 support via simde

---
+++ include/nmtools/array/eval/simd/simde_avx512.hpp
@@ -0,0 +1,7 @@
+#ifndef NMTOOLS_ARRAY_EVAL_SIMD_SIMDE_AVX512_HPP
+#define NMTOOLS_ARRAY_EVAL_SIMD_SIMDE_AVX512_HPP
+
+#include "nmtools/array/eval/simd/simde_avx512/simd_op.hpp"
+#include "nmtools/array/eval/simd/simde_avx512/ufunc.hpp"
+
+#endif // NMTOOLS_ARRAY_EVAL_SIMD_SIMDE_AVX512_HPP
