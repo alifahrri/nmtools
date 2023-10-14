521f28c3f238898df6f09fa6f7d289e8fc46e2d1
Fahri Ali Rahman <fahri@nodeflux.io>
2023-10-14 21:14:04 +0700

    add constexpr pad test data

--- include/nmtools/testing/data/array/pad.hpp
+++ include/nmtools/testing/data/array/pad.hpp
@@ -195,6 +195,8 @@ NMTOOLS_TESTING_DECLARE_CASE(index, pad)
     }
 } // NMTOOLS_TESTING_DECLARE_CASE(index, pad)
 
+#ifndef NMTOOLS_TESTING_CONSTEXPR
+
 NMTOOLS_TESTING_DECLARE_CASE(array, pad)
 {
     NMTOOLS_TESTING_DECLARE_ARGS(case1)
@@ -323,5 +325,143 @@ NMTOOLS_TESTING_DECLARE_CASE(array, pad)
         { 0,  0,  0,  0}}};
     }
 }
+
+#else 
+
+NMTOOLS_TESTING_DECLARE_CASE(array, constexpr_pad)
+{
+    NMTOOLS_TESTING_DECLARE_ARGS(case1)
+    {
+        constexpr inline int array[3][2] = {
+            {0,1},
+            {2,3},
+            {4,5},
+        };
+        constexpr inline int pad_width[4] = {0,2,0,0};
+        constexpr inline auto pad_width_ct = nmtools_tuple{0_ct,2_ct,0_ct,0_ct};
+        constexpr inline auto pad_width_cl = to_clipped(pad_width_ct);
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(array);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case1)
+    {
+        constexpr inline int result[3][4] = {
+            {0,0,0,1},
+            {0,0,2,3},
+            {0,0,4,5},
+        };
+    }
+
+    NMTOOLS_TESTING_DECLARE_ARGS(case2)
+    {
+        constexpr inline int array[3][2] = {
+            {0,1},
+            {2,3},
+            {4,5},
+        };
+        constexpr inline int pad_width[4] = {0,2,0,2};
+        constexpr inline auto pad_width_ct = nmtools_tuple{0_ct,2_ct,0_ct,2_ct};
+        constexpr inline auto pad_width_cl = to_clipped(pad_width_ct);
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(array);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case2)
+    {
+        constexpr inline int result[3][6] = {
+            {0,0,0,1,0,0},
+            {0,0,2,3,0,0},
+            {0,0,4,5,0,0},
+        };
+    }
+
+    NMTOOLS_TESTING_DECLARE_ARGS(case3)
+    {
+        constexpr inline int array[3][2] = {
+            {0,1},
+            {2,3},
+            {4,5},
+        };
+        constexpr inline int pad_width[4] = {1,2,0,2};
+        constexpr inline auto pad_width_ct = nmtools_tuple{1_ct,2_ct,0_ct,2_ct};
+        constexpr inline auto pad_width_cl = to_clipped(pad_width_ct);
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(array);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case3)
+    {
+        constexpr inline int result[4][6] = {
+            {0,0,0,0,0,0},
+            {0,0,0,1,0,0},
+            {0,0,2,3,0,0},
+            {0,0,4,5,0,0},
+        };
+    }
+
+    NMTOOLS_TESTING_DECLARE_ARGS(case4)
+    {
+        constexpr inline int array[3][2] = {
+            {0,1},
+            {2,3},
+            {4,5},
+        };
+        constexpr inline int pad_width[4] = {1,2,3,2};
+        constexpr inline auto pad_width_ct = nmtools_tuple{1_ct,2_ct,3_ct,2_ct};
+        constexpr inline auto pad_width_cl = to_clipped(pad_width_ct);
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(array);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case4)
+    {
+        constexpr inline int result[7][6] = {
+            {0,0,0,0,0,0},
+            {0,0,0,1,0,0},
+            {0,0,2,3,0,0},
+            {0,0,4,5,0,0},
+            {0,0,0,0,0,0},
+            {0,0,0,0,0,0},
+            {0,0,0,0,0,0},
+        };
+    }
+
+    NMTOOLS_TESTING_DECLARE_ARGS(case5)
+    {
+        constexpr inline int array[2][3][2] = {
+            {
+                {0,1},
+                {2,3},
+                {4,5},
+            },
+            {
+                { 6, 7},
+                { 8, 9},
+                {10,11},
+            }
+        };
+        constexpr inline int pad_width[6] = {0,1,1,2,0,1};
+        constexpr inline auto pad_width_ct = nmtools_tuple{0_ct,1_ct,1_ct,2_ct,0_ct,1_ct};
+        constexpr inline auto pad_width_cl = to_clipped(pad_width_ct);
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(array);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case5)
+    {
+        constexpr inline int result[4][4][4] = {{{ 0,  0,  0,  0},
+        { 0,  0,  1,  0},
+        { 0,  2,  3,  0},
+        { 0,  4,  5,  0}},
+
+       {{ 0,  0,  0,  0},
+        { 0,  6,  7,  0},
+        { 0,  8,  9,  0},
+        { 0, 10, 11,  0}},
+
+       {{ 0,  0,  0,  0},
+        { 0,  0,  0,  0},
+        { 0,  0,  0,  0},
+        { 0,  0,  0,  0}},
+
+       {{ 0,  0,  0,  0},
+        { 0,  0,  0,  0},
+        { 0,  0,  0,  0},
+        { 0,  0,  0,  0}}};
+    }
+}
+
+#endif // NMTOOLS_TESTING_CONSTEXPR
 
 #endif // NMTOOLS_TESTING_DATA_ARRAY_PAD_HPP
