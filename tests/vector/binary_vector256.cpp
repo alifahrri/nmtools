ac9d3547ab2ce351bb2e89ac3ac91bc2a3647636
Fahri Ali Rahman <fahri@nodeflux.io>
2023-10-18 14:22:23 +0700

    add tests for gcc vector extension

---
+++ tests/vector/binary_vector256.cpp
@@ -0,0 +1,204 @@
+#include "nmtools/array/eval/simd/vector_256.hpp"
+#include "nmtools/array/array/arange.hpp"
+#include "nmtools/array/array/ufuncs/add.hpp"
+#include "nmtools/array/array/ufuncs/multiply.hpp"
+#include "nmtools/array/array/ufuncs/subtract.hpp"
+#include "nmtools/array/array/ufuncs/divide.hpp"
+#include "nmtools/testing/doctest.hpp"
+
+namespace nm = nmtools;
+namespace na = nm::array;
+namespace simd = na::simd;
+
+#define SIMD_TEST_EQUAL(result, expect) \
+{ \
+    NMTOOLS_ASSERT_EQUAL( nm::shape(result), nm::shape(expect) ); \
+    NMTOOLS_ASSERT_CLOSE( result, expect ); \
+}
+
+#define VECTOR_256_TEST(array,fn,...) \
+{ \
+    auto expect = na::fn(array,##__VA_ARGS__); \
+    auto result = na::fn(array,##__VA_ARGS__,nm::casting::same_kind_t{},simd::vector_256); \
+    SIMD_TEST_EQUAL(result,expect); \
+}
+
+/*********************** add ******************************/
+
+#if 1
+TEST_CASE("add(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128);
+    auto rhs  = na::arange(128);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128,nm::float64);
+    auto rhs  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(0,8,0.125);
+    auto rhs  = na::arange(0,8,0.125);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(129);
+    auto rhs  = na::arange(129);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(127);
+    auto rhs  = na::arange(127);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(129,nm::float64);
+    auto rhs  = na::arange(129,nm::float64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case7)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(127,nm::float64);
+    auto rhs  = na::arange(127,nm::float64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case8)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(3);
+    auto rhs  = na::arange(3);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case9)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(5);
+    auto rhs  = na::arange(5);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case10)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(3,nm::float64);
+    auto rhs  = na::arange(3,nm::float64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case11)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(5,nm::float64);
+    auto rhs  = na::arange(5,nm::float64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case12)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(1);
+    auto rhs  = na::arange(1);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case13)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(1,nm::float64);
+    auto rhs  = na::arange(1,nm::float64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case14)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128,nm::uint64);
+    auto rhs  = na::arange(128,nm::uint64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case15)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128,nm::int64);
+    auto rhs  = na::arange(128,nm::int64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case16)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128,nm::uint32);
+    auto rhs  = na::arange(128,nm::uint32);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case17)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128,nm::int32);
+    auto rhs  = na::arange(128,nm::int32);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case18)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128,nm::uint16);
+    auto rhs  = na::arange(128,nm::uint16);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case19)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128,nm::int16);
+    auto rhs  = na::arange(128,nm::int16);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case20)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(128,nm::uint8);
+    auto rhs  = na::arange(128,nm::uint8);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+// TODO: support promotion casting
+TEST_CASE("add(case21)" * doctest::test_suite("simd::vector_256") * doctest::skip())
+{
+    auto lhs  = na::arange(128,nm::int8);
+    auto rhs  = na::arange(128,nm::int8);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case22)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(-128,127,nm::int64);
+    auto rhs  = na::arange(-128,127,nm::int64);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case23)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(-128,127,nm::int32);
+    auto rhs  = na::arange(-128,127,nm::int32);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case24)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(-128,127,nm::int16);
+    auto rhs  = na::arange(-128,127,nm::int16);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+
+TEST_CASE("add(case25)" * doctest::test_suite("simd::vector_256"))
+{
+    auto lhs  = na::arange(-64,63,nm::int8);
+    auto rhs  = na::arange(-64,63,nm::int8);
+    VECTOR_256_TEST(lhs,add,rhs);
+}
+#endif
---
+++ tests/vector/unary_vector256.cpp
@@ -0,0 +1,645 @@
+#include "nmtools/array/eval/simd/vector_256.hpp"
+#include "nmtools/array/array/arange.hpp"
+#include "nmtools/array/array/ufuncs/add.hpp"
+#include "nmtools/array/array/ufuncs/sqrt.hpp"
+#include "nmtools/array/array/ufuncs/ceil.hpp"
+#include "nmtools/array/array/ufuncs/floor.hpp"
+#include "nmtools/array/array/activations/hardtanh.hpp"
+#include "nmtools/array/array/activations/hardshrink.hpp"
+#include "nmtools/array/array/activations/hardswish.hpp"
+#include "nmtools/array/array/activations/leaky_relu.hpp"
+#include "nmtools/array/array/activations/prelu.hpp"
+#include "nmtools/array/array/activations/relu.hpp"
+#include "nmtools/array/array/activations/relu6.hpp"
+#include "nmtools/array/array/activations/softshrink.hpp"
+#include "nmtools/array/array/activations/softsign.hpp"
+#include "nmtools/testing/doctest.hpp"
+
+namespace nm = nmtools;
+namespace na = nm::array;
+namespace simd = na::simd;
+
+#define SIMD_TEST_EQUAL(result, expect) \
+{ \
+    NMTOOLS_ASSERT_EQUAL( nm::shape(result), nm::shape(expect) ); \
+    NMTOOLS_ASSERT_CLOSE( result, expect ); \
+}
+
+#define VECTOR_256_TEST(array,fn,...) \
+{ \
+    auto expect = na::fn(array,##__VA_ARGS__); \
+    auto result = na::fn(array,##__VA_ARGS__,simd::vector_256); \
+    SIMD_TEST_EQUAL(result,expect); \
+}
+
+/*********************** sqrt ******************************/
+
+TEST_CASE("sqrt(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(0,8,0.125);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129,nm::float64);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case7)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127,nm::float64);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case8)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case9)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(5);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case10)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+TEST_CASE("sqrt(case11)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(1);
+    VECTOR_256_TEST(array,sqrt);
+}
+
+/*********************** ceil ******************************/
+
+TEST_CASE("ceil(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(0,8,0.125);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129,nm::float64);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case7)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127,nm::float64);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case8)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case9)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(5);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case10)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,ceil);
+}
+
+TEST_CASE("ceil(case11)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(1);
+    VECTOR_256_TEST(array,ceil);
+}
+
+/*********************** floor ******************************/
+
+TEST_CASE("floor(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(0,8,0.125);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129,nm::float64);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case7)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127,nm::float64);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case8)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case9)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(5);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case10)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,floor);
+}
+
+TEST_CASE("floor(case11)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(1);
+    VECTOR_256_TEST(array,floor);
+}
+
+/*********************** relu ******************************/
+
+TEST_CASE("relu(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(0,8,0.125);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129,nm::float64);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case7)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127,nm::float64);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case8)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case9)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(5);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case10)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,relu);
+}
+
+TEST_CASE("relu(case11)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(1);
+    VECTOR_256_TEST(array,relu);
+}
+
+/*********************** relu6 ******************************/
+
+TEST_CASE("relu6(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(0,8,0.125);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(129,nm::float64);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case7)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(127,nm::float64);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case8)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case9)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(5);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case10)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(3);
+    VECTOR_256_TEST(array,relu6);
+}
+
+TEST_CASE("relu6(case11)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(1);
+    VECTOR_256_TEST(array,relu6);
+}
+
+/*********************** hardtanh ******************************/
+
+TEST_CASE("hardtanh(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128);
+    VECTOR_256_TEST(array,hardtanh,-5.0f,5.0f);
+}
+
+TEST_CASE("hardtanh(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128);
+    VECTOR_256_TEST(array,hardtanh,-5.0,5.0);
+}
+
+TEST_CASE("hardtanh(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128);
+    VECTOR_256_TEST(array,hardtanh,-5,5);
+}
+
+TEST_CASE("hardtanh(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,hardtanh,-5.0f,5.0f);
+}
+
+TEST_CASE("hardtanh(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,hardtanh,-5.0,5.0);
+}
+
+TEST_CASE("hardtanh(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,hardtanh,-5,5);
+}
+
+TEST_CASE("hardtanh(case7)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(128,nm::float64);
+    VECTOR_256_TEST(array,hardtanh,-5,5ul);
+}
+
+/*********************** leaky_relu ******************************/
+
+TEST_CASE("leaky_relu(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,leaky_relu,0.01f);
+}
+
+TEST_CASE("leaky_relu(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,leaky_relu,0.01);
+}
+
+TEST_CASE("leaky_relu(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125);
+    VECTOR_256_TEST(array,leaky_relu,0.01f);
+}
+
+TEST_CASE("leaky_relu(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,leaky_relu,0.01f);
+}
+
+TEST_CASE("leaky_relu(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,leaky_relu,0.01);
+}
+
+TEST_CASE("leaky_relu(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125,nm::float64);
+    VECTOR_256_TEST(array,leaky_relu,0.01f);
+}
+
+/*********************** prelu ******************************/
+
+TEST_CASE("prelu(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,prelu,0.01f);
+}
+
+TEST_CASE("prelu(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,prelu,0.01);
+}
+
+TEST_CASE("prelu(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125);
+    VECTOR_256_TEST(array,prelu,0.01f);
+}
+
+TEST_CASE("prelu(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,prelu,0.01f);
+}
+
+TEST_CASE("prelu(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,prelu,0.01);
+}
+
+TEST_CASE("prelu(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125,nm::float64);
+    VECTOR_256_TEST(array,prelu,0.01f);
+}
+
+/*********************** softshrink ******************************/
+
+TEST_CASE("softshrink(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,softshrink,3.0f);
+}
+
+TEST_CASE("softshrink(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,softshrink,3.0);
+}
+
+TEST_CASE("softshrink(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,softshrink,3);
+}
+
+TEST_CASE("softshrink(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,softshrink,3.0f);
+}
+
+TEST_CASE("softshrink(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,softshrink,3.0);
+}
+
+TEST_CASE("softshrink(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,softshrink,3);
+}
+
+/*********************** softsign ******************************/
+
+TEST_CASE("softsign(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,softsign);
+}
+
+TEST_CASE("softsign(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125);
+    VECTOR_256_TEST(array,softsign);
+}
+
+TEST_CASE("softsign(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125f);
+    VECTOR_256_TEST(array,softsign);
+}
+
+TEST_CASE("softsign(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,softsign);
+}
+
+TEST_CASE("softsign(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125,nm::float64);
+    VECTOR_256_TEST(array,softsign);
+}
+
+TEST_CASE("softsign(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125f,nm::float64);
+    VECTOR_256_TEST(array,softsign);
+}
+
+/*********************** hardshrink ******************************/
+
+TEST_CASE("hardshrink(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,hardshrink,3.0f);
+}
+
+TEST_CASE("hardshrink(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,hardshrink,3.0);
+}
+
+TEST_CASE("hardshrink(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,hardshrink,3);
+}
+
+TEST_CASE("hardshrink(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,hardshrink,3.0f);
+}
+
+TEST_CASE("hardshrink(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,hardshrink,3.0);
+}
+
+TEST_CASE("hardshrink(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,hardshrink,3);
+}
+
+/*********************** hardswish ******************************/
+
+TEST_CASE("hardswish(case1)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64);
+    VECTOR_256_TEST(array,hardswish);
+}
+
+TEST_CASE("hardswish(case2)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125);
+    VECTOR_256_TEST(array,hardswish);
+}
+
+TEST_CASE("hardswish(case3)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125f);
+    VECTOR_256_TEST(array,hardswish);
+}
+
+TEST_CASE("hardswish(case4)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-64,64,nm::float64);
+    VECTOR_256_TEST(array,hardswish);
+}
+
+TEST_CASE("hardswish(case5)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125,nm::float64);
+    VECTOR_256_TEST(array,hardswish);
+}
+
+TEST_CASE("hardswish(case6)" * doctest::test_suite("simd::vector_256"))
+{
+    auto array  = na::arange(-4,4,0.125f,nm::float64);
+    VECTOR_256_TEST(array,hardswish);
+}
