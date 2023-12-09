aba1040e17e7de374c99adc9bbbfe619915316e2
Fahri Ali Rahman <fahri@nodeflux.io>
2023-12-10 02:12:56 +0700

    update tests and kernel_spv

---
+++ tests/opencl/kernels/expand_dims.cpp
@@ -0,0 +1,36 @@
+#include "nmtools/array/eval/opencl/kernels/expand_dims.hpp"
+#ifndef NMTOOLS_OPENCL_BUILD_KERNELS
+#include "nmtools/array/eval/opencl/evaluator.hpp"
+#include "nmtools/array/array/arange.hpp"
+#include "nmtools/array/array/expand_dims.hpp"
+#include "nmtools/array/array/reshape.hpp"
+#include "nmtools/testing/doctest.hpp"
+
+namespace nm = nmtools;
+namespace ix = nm::index;
+namespace na = nm::array;
+namespace meta = nm::meta;
+namespace opencl = na::opencl;
+
+#define OPENCL_TEST(fn,...) \
+{ \
+    auto expect = na::fn(__VA_ARGS__); \
+    auto result = na::fn(__VA_ARGS__,opencl::default_context()); \
+    NMTOOLS_ASSERT_EQUAL( nm::shape(result), nm::shape(expect) ); \
+    NMTOOLS_ASSERT_CLOSE( result, expect ); \
+}
+
+TEST_CASE("expand_dims(case1)" * doctest::test_suite("expand_dims"))
+{
+    auto src_shape = nmtools_array{12,8};
+    auto axis = 0;
+
+    auto numel = ix::product(src_shape);
+    auto start = 1;
+    auto stop  = numel+start;
+    auto input = na::reshape(na::arange(start,stop),src_shape);
+
+    OPENCL_TEST(expand_dims,input,axis);
+}
+
+#endif // NMTOOLS_OPENCL_BUILD_KERNELS
---
+++ tests/opencl/kernels/flip.cpp
@@ -0,0 +1,36 @@
+#include "nmtools/array/eval/opencl/kernels/flip.hpp"
+#ifndef NMTOOLS_OPENCL_BUILD_KERNELS
+#include "nmtools/array/eval/opencl/evaluator.hpp"
+#include "nmtools/array/array/arange.hpp"
+#include "nmtools/array/array/flip.hpp"
+#include "nmtools/array/array/reshape.hpp"
+#include "nmtools/testing/doctest.hpp"
+
+namespace nm = nmtools;
+namespace ix = nm::index;
+namespace na = nm::array;
+namespace meta = nm::meta;
+namespace opencl = na::opencl;
+
+#define OPENCL_TEST(fn,...) \
+{ \
+    auto expect = na::fn(__VA_ARGS__); \
+    auto result = na::fn(__VA_ARGS__,opencl::default_context()); \
+    NMTOOLS_ASSERT_EQUAL( nm::shape(result), nm::shape(expect) ); \
+    NMTOOLS_ASSERT_CLOSE( result, expect ); \
+}
+
+TEST_CASE("flip(case1)" * doctest::test_suite("flip"))
+{
+    auto src_shape = nmtools_array{12,8};
+    auto axis = nmtools_array{0};
+
+    auto numel = ix::product(src_shape);
+    auto start = 1;
+    auto stop  = numel+start;
+    auto input = na::reshape(na::arange(start,stop),src_shape);
+
+    OPENCL_TEST(flip,input,axis);
+}
+
+#endif // NMTOOLS_OPENCL_BUILD_KERNELS
