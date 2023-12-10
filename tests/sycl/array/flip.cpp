79aa12b9fe9e4f6db001001bfd4e1dfd99ec9102
Fahri Ali Rahman <fahri@nodeflux.io>
2023-12-10 18:36:07 +0700

    add more sycl tests

---
+++ tests/sycl/array/flip.cpp
@@ -0,0 +1,151 @@
+#define NMTOOLS_CAST_ARRAYS_EXTRA(name) \
+inline auto name##_cs_fb = nmtools::cast(name, nmtools::array::kind::ndarray_cs_fb); \
+inline auto name##_cs_hb = nmtools::cast(name, nmtools::array::kind::ndarray_cs_hb); \
+inline auto name##_cs_db = nmtools::cast(name, nmtools::array::kind::ndarray_cs_db); \
+inline auto name##_fs_fb = nmtools::cast(name, nmtools::array::kind::ndarray_fs_fb); \
+inline auto name##_fs_hb = nmtools::cast(name, nmtools::array::kind::ndarray_fs_hb); \
+inline auto name##_fs_db = nmtools::cast(name, nmtools::array::kind::ndarray_fs_db); \
+inline auto name##_hs_fb = nmtools::cast(name, nmtools::array::kind::ndarray_hs_fb); \
+inline auto name##_hs_hb = nmtools::cast(name, nmtools::array::kind::ndarray_hs_hb); \
+inline auto name##_hs_db = nmtools::cast(name, nmtools::array::kind::ndarray_hs_db); \
+inline auto name##_ds_fb = nmtools::cast(name, nmtools::array::kind::ndarray_ds_fb); \
+inline auto name##_ds_hb = nmtools::cast(name, nmtools::array::kind::ndarray_ds_hb); \
+inline auto name##_ds_db = nmtools::cast(name, nmtools::array::kind::ndarray_ds_db); \
+inline auto name##_ls_fb = nmtools::cast(name, nmtools::array::kind::ndarray_ls_fb); \
+inline auto name##_ls_hb = nmtools::cast(name, nmtools::array::kind::ndarray_ls_hb); \
+inline auto name##_ls_db = nmtools::cast(name, nmtools::array::kind::ndarray_ls_db);
+
+#include "nmtools/array/array/flip.hpp"
+#include "nmtools/testing/doctest.hpp"
+#include "nmtools/array/eval/sycl.hpp"
+#include "nmtools/testing/data/array/flip.hpp"
+
+namespace nm = nmtools;
+namespace na = nm::array;
+
+#define FLIP_SUBCASE(case_name, ...) \
+SUBCASE(#case_name) \
+{ \
+    NMTOOLS_TESTING_DECLARE_NS(flip, case_name); \
+    using namespace args; \
+    auto result = na::flip(__VA_ARGS__, na::sycl::default_context()); \
+    auto expect = na::flip(__VA_ARGS__); \
+    NMTOOLS_ASSERT_EQUAL( nm::shape(result), nm::shape(expect) ); \
+    NMTOOLS_ASSERT_CLOSE( result, expect ); \
+}
+
+TEST_CASE("flip(case1)" * doctest::test_suite("array::flip"))
+{
+    // FLIP_SUBCASE(case1, array, axis);
+    // FLIP_SUBCASE(case1, array_a, axis);
+    // FLIP_SUBCASE(case1, array_f, axis);
+    // FLIP_SUBCASE(case1, array_h, axis);
+    // FLIP_SUBCASE(case1, array_d, axis);
+
+    // FLIP_SUBCASE(case1, array_cs_fb, axis);
+    // FLIP_SUBCASE(case1, array_cs_hb, axis);
+    // FLIP_SUBCASE(case1, array_cs_db, axis);
+
+    FLIP_SUBCASE(case1, array_fs_fb, axis);
+    FLIP_SUBCASE(case1, array_fs_hb, axis);
+    FLIP_SUBCASE(case1, array_fs_db, axis);
+
+    FLIP_SUBCASE(case1, array_hs_fb, axis);
+    FLIP_SUBCASE(case1, array_hs_hb, axis);
+    FLIP_SUBCASE(case1, array_hs_db, axis);
+
+    FLIP_SUBCASE(case1, array_ds_fb, axis);
+    FLIP_SUBCASE(case1, array_ds_hb, axis);
+    FLIP_SUBCASE(case1, array_ds_db, axis);
+
+    // FLIP_SUBCASE(case1, array_ls_fb, axis);
+    // FLIP_SUBCASE(case1, array_ls_hb, axis);
+    // FLIP_SUBCASE(case1, array_ls_db, axis);
+}
+
+TEST_CASE("flip(case2)" * doctest::test_suite("array::flip"))
+{
+    // FLIP_SUBCASE(case2, array, axis);
+    // FLIP_SUBCASE(case2, array_a, axis);
+    // FLIP_SUBCASE(case2, array_f, axis);
+    // FLIP_SUBCASE(case2, array_h, axis);
+    // FLIP_SUBCASE(case2, array_d, axis);
+
+    // FLIP_SUBCASE(case2, array_cs_fb, axis);
+    // FLIP_SUBCASE(case2, array_cs_hb, axis);
+    // FLIP_SUBCASE(case2, array_cs_db, axis);
+
+    FLIP_SUBCASE(case2, array_fs_fb, axis);
+    FLIP_SUBCASE(case2, array_fs_hb, axis);
+    FLIP_SUBCASE(case2, array_fs_db, axis);
+
+    FLIP_SUBCASE(case2, array_hs_fb, axis);
+    FLIP_SUBCASE(case2, array_hs_hb, axis);
+    FLIP_SUBCASE(case2, array_hs_db, axis);
+
+    FLIP_SUBCASE(case2, array_ds_fb, axis);
+    FLIP_SUBCASE(case2, array_ds_hb, axis);
+    FLIP_SUBCASE(case2, array_ds_db, axis);
+
+    // FLIP_SUBCASE(case2, array_ls_fb, axis);
+    // FLIP_SUBCASE(case2, array_ls_hb, axis);
+    // FLIP_SUBCASE(case2, array_ls_db, axis);
+}
+
+TEST_CASE("flip(case3)" * doctest::test_suite("array::flip"))
+{
+    // FLIP_SUBCASE(case3, array, axis);
+    // FLIP_SUBCASE(case3, array_a, axis);
+    // FLIP_SUBCASE(case3, array_f, axis);
+    // FLIP_SUBCASE(case3, array_h, axis);
+    // FLIP_SUBCASE(case3, array_d, axis);
+
+    // FLIP_SUBCASE(case3, array_cs_fb, axis);
+    // FLIP_SUBCASE(case3, array_cs_hb, axis);
+    // FLIP_SUBCASE(case3, array_cs_db, axis);
+
+    FLIP_SUBCASE(case3, array_fs_fb, axis);
+    FLIP_SUBCASE(case3, array_fs_hb, axis);
+    FLIP_SUBCASE(case3, array_fs_db, axis);
+
+    FLIP_SUBCASE(case3, array_hs_fb, axis);
+    FLIP_SUBCASE(case3, array_hs_hb, axis);
+    FLIP_SUBCASE(case3, array_hs_db, axis);
+
+    FLIP_SUBCASE(case3, array_ds_fb, axis);
+    FLIP_SUBCASE(case3, array_ds_hb, axis);
+    FLIP_SUBCASE(case3, array_ds_db, axis);
+
+    // FLIP_SUBCASE(case3, array_ls_fb, axis);
+    // FLIP_SUBCASE(case3, array_ls_hb, axis);
+    // FLIP_SUBCASE(case3, array_ls_db, axis);
+}
+
+TEST_CASE("flip(case4)" * doctest::test_suite("array::flip"))
+{
+    // FLIP_SUBCASE(case4, array, axis_a);
+    // FLIP_SUBCASE(case4, array_a, axis_a);
+    // FLIP_SUBCASE(case4, array_f, axis_a);
+    // FLIP_SUBCASE(case4, array_h, axis_a);
+    // FLIP_SUBCASE(case4, array_d, axis_a);
+
+    // FLIP_SUBCASE(case4, array_cs_fb, axis_a);
+    // FLIP_SUBCASE(case4, array_cs_hb, axis_a);
+    // FLIP_SUBCASE(case4, array_cs_db, axis_a);
+
+    FLIP_SUBCASE(case4, array_fs_fb, axis_a);
+    FLIP_SUBCASE(case4, array_fs_hb, axis_a);
+    FLIP_SUBCASE(case4, array_fs_db, axis_a);
+
+    FLIP_SUBCASE(case4, array_hs_fb, axis_a);
+    FLIP_SUBCASE(case4, array_hs_hb, axis_a);
+    FLIP_SUBCASE(case4, array_hs_db, axis_a);
+
+    FLIP_SUBCASE(case4, array_ds_fb, axis_a);
+    FLIP_SUBCASE(case4, array_ds_hb, axis_a);
+    FLIP_SUBCASE(case4, array_ds_db, axis_a);
+
+    // FLIP_SUBCASE(case4, array_ls_fb, axis_a);
+    // FLIP_SUBCASE(case4, array_ls_hb, axis_a);
+    // FLIP_SUBCASE(case4, array_ls_db, axis_a);
+}
---
+++ tests/sycl/array/tile.cpp
@@ -0,0 +1,64 @@
+#define NMTOOLS_CAST_ARRAYS_EXTRA(name) \
+inline auto name##_cs_fb = nmtools::cast(name, nmtools::array::kind::ndarray_cs_fb); \
+inline auto name##_cs_hb = nmtools::cast(name, nmtools::array::kind::ndarray_cs_hb); \
+inline auto name##_cs_db = nmtools::cast(name, nmtools::array::kind::ndarray_cs_db); \
+inline auto name##_fs_fb = nmtools::cast(name, nmtools::array::kind::ndarray_fs_fb); \
+inline auto name##_fs_hb = nmtools::cast(name, nmtools::array::kind::ndarray_fs_hb); \
+inline auto name##_fs_db = nmtools::cast(name, nmtools::array::kind::ndarray_fs_db); \
+inline auto name##_hs_fb = nmtools::cast(name, nmtools::array::kind::ndarray_hs_fb); \
+inline auto name##_hs_hb = nmtools::cast(name, nmtools::array::kind::ndarray_hs_hb); \
+inline auto name##_hs_db = nmtools::cast(name, nmtools::array::kind::ndarray_hs_db); \
+inline auto name##_ds_fb = nmtools::cast(name, nmtools::array::kind::ndarray_ds_fb); \
+inline auto name##_ds_hb = nmtools::cast(name, nmtools::array::kind::ndarray_ds_hb); \
+inline auto name##_ds_db = nmtools::cast(name, nmtools::array::kind::ndarray_ds_db); \
+inline auto name##_ls_fb = nmtools::cast(name, nmtools::array::kind::ndarray_ls_fb); \
+inline auto name##_ls_hb = nmtools::cast(name, nmtools::array::kind::ndarray_ls_hb); \
+inline auto name##_ls_db = nmtools::cast(name, nmtools::array::kind::ndarray_ls_db);
+
+#include "nmtools/array/array/tile.hpp"
+#include "nmtools/testing/doctest.hpp"
+#include "nmtools/array/eval/sycl.hpp"
+#include "nmtools/testing/data/array/tile.hpp"
+
+namespace nm = nmtools;
+namespace na = nm::array;
+
+#define TILE_SUBCASE(case_name, ...) \
+SUBCASE(#case_name) \
+{ \
+    NMTOOLS_TESTING_DECLARE_NS(view, tile, case_name); \
+    using namespace args; \
+    auto result = na::tile(__VA_ARGS__, na::sycl::default_context()); \
+    auto expect = na::tile(__VA_ARGS__); \
+    NMTOOLS_ASSERT_EQUAL( nm::shape(result), nm::shape(expect) ); \
+    NMTOOLS_ASSERT_CLOSE( result, expect ); \
+}
+
+TEST_CASE("tile(case1)" * doctest::test_suite("array::tile"))
+{
+    // TILE_SUBCASE(case1, array, reps);
+    // TILE_SUBCASE(case1, array_a, reps);
+    // TILE_SUBCASE(case1, array_f, reps);
+    // TILE_SUBCASE(case1, array_h, reps);
+    // TILE_SUBCASE(case1, array_d, reps);
+
+    // TILE_SUBCASE(case1, array_cs_fb, reps);
+    // TILE_SUBCASE(case1, array_cs_hb, reps);
+    // TILE_SUBCASE(case1, array_cs_db, reps);
+
+    TILE_SUBCASE(case1, array_fs_fb, reps);
+    TILE_SUBCASE(case1, array_fs_hb, reps);
+    TILE_SUBCASE(case1, array_fs_db, reps);
+
+    TILE_SUBCASE(case1, array_hs_fb, reps);
+    TILE_SUBCASE(case1, array_hs_hb, reps);
+    TILE_SUBCASE(case1, array_hs_db, reps);
+
+    TILE_SUBCASE(case1, array_ds_fb, reps);
+    TILE_SUBCASE(case1, array_ds_hb, reps);
+    TILE_SUBCASE(case1, array_ds_db, reps);
+
+    // TILE_SUBCASE(case1, array_ls_fb, reps);
+    // TILE_SUBCASE(case1, array_ls_hb, reps);
+    // TILE_SUBCASE(case1, array_ls_db, reps);
+}
