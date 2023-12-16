6ab92d8fb5af12d21dfc6d71d7465084b6d07f9f
Fahri Ali Rahman <fahri@nodeflux.io>
2023-12-16 17:48:55 +0700

    try to fix metafunction tests

--- tests/meta/array/index/arange.cpp
+++ tests/meta/array/index/arange.cpp
@@ -15,7 +15,7 @@ TEST_CASE("arange" * doctest::test_suite("index"))
         using step_t  = nm::none_t;
         using result_t = meta::resolve_optype_t<ix::arange_shape_t,start_t,stop_t,step_t>;
         using expect_t = nmtools_array<size_t,1>;
-        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
+        NMTOOLS_STATIC_CHECK_SAME_INDEX_TRAIT( result_t, expect_t );
     }
     {
         using start_t = decltype(0_ct);
@@ -23,7 +23,6 @@ TEST_CASE("arange" * doctest::test_suite("index"))
         using step_t  = nm::none_t;
         using result_t = meta::resolve_optype_t<ix::arange_shape_t,start_t,stop_t,step_t>;
         using expect_t = nmtools_tuple<meta::ct<4ul>>;
-        NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
-        NMTOOLS_STATIC_CHECK_TRAIT( meta::is_constant_index_array, result_t );
+        NMTOOLS_STATIC_CHECK_SAME_INDEX_TRAIT( result_t, expect_t );
     }
 }
