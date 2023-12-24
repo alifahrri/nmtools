bfb9a0a7938d313a6269e9228fb9b3f8878bbd12
Fahri Ali Rahman <fahri@nodeflux.io>
2023-12-24 19:26:33 +0700

    update cuda context to support mean

--- include/nmtools/array/eval/cuda/context.hpp
+++ include/nmtools/array/eval/cuda/context.hpp
@@ -17,24 +17,33 @@ __global__ void nm_cuda_run_function(const function_t fun
 ) {
     namespace cuda = nmtools::array::cuda;
     namespace meta = nmtools::meta;
-    auto output = cuda::create_mutable_array<out_static_dim>(out,out_shape_ptr,out_dim);
+    namespace na = nmtools::array;
+    auto output = na::create_mutable_array<out_static_dim>(out,out_shape_ptr,out_dim);
     constexpr auto N = sizeof...(args_t);
-    auto args_pack = nmtools_tuple<const args_t...>(args...);
+    auto args_pack = nmtools_tuple<const args_t&...>(args...);
+    using args_0_t = meta::remove_cvref_pointer_t<decltype(nmtools::get<0>(args_pack))>;
     auto result = [&](){
         if constexpr (N == 0) {
             return fun();
+        } else if constexpr (meta::is_device_array_v<args_0_t>) {
+            return meta::template_reduce<sizeof...(args_t)>([&](auto fn, auto index){
+                // TODO: support constant shape, clipped shape, fixed dim, fixed size, bounded dim, size etc...
+                auto array = na::create_array(nmtools::at(args_pack,index));
+                return fn (array);
+            }, fun);
         } else {
-            return meta::template_reduce<sizeof...(args_t) / 3>([&](auto init, auto index){
+            // TODO: remove
+            return meta::template_reduce<sizeof...(args_t) / 3>([&](auto fn, auto index){
                 constexpr auto ptr_idx = (size_t)index * 3;
                 constexpr auto shp_idx = ptr_idx + 1;
                 constexpr auto dim_idx = ptr_idx + 2;
                 // TODO: support constant shape, clipped shape, fixed dim, fixed size, bounded dim, size etc...
-                auto array = cuda::create_array(
+                auto array = na::create_array(
                     nmtools::get<ptr_idx>(args_pack)
                     , nmtools::get<shp_idx>(args_pack)
                     , nmtools::get<dim_idx>(args_pack)
                 );
-                return init (array);
+                return fn (array);
             }, fun);
         }
     }();
@@ -48,6 +57,26 @@ __global__ void nm_cuda_run_function(const function_t fun
         static_assert( meta::is_ndarray_v<result_t> );
         cuda::assign_array(output,result);
     }
+}
+
+namespace nmtools::array
+{
+    class cuda_exception : public ::nmtools::exception
+    {
+        public:
+        cuda_exception(cudaError status, const std::string& message)
+            : nmtools::exception(message + " (" + std::to_string(static_cast<int>(status)) + "): " + cudaGetErrorString(status))
+            , status_(status)
+        {}
+
+        inline auto status() const
+        {
+            return status_;
+        }
+
+        protected:
+        cudaError status_;
+    };
 }
 
 namespace nmtools::array::cuda
@@ -71,29 +100,24 @@ namespace nmtools::array::cuda
         constexpr inline auto is_shared_ptr_v = is_shared_ptr<T>::value;
     }
 
-    class cuda_exception : public ::nmtools::exception
-    {
-        public:
-        cuda_exception(cudaError status, const std::string& message)
-            : nmtools::exception(message + " (" + std::to_string(static_cast<int>(status)) + "): " + cudaGetErrorString(status))
-            , status_(status)
-        {}
-
-        inline auto status() const
-        {
-            return status_;
-        }
-
-        protected:
-        cudaError status_;
-    };
-
     template <typename T>
     struct buffer_deleter_t
     {
         void operator()(T* ptr) const
         {
             auto status = cudaFree(ptr);
+            if (status != cudaSuccess) {
+                throw cuda_exception(status, "error when freeing device memory");
+            }
+        }
+    };
+
+    template <typename array_t>
+    struct array_deleter_t
+    {
+        void operator()(array_t* array_ptr) const
+        {
+            auto status = cudaFree(array_ptr->buffer);
             if (status != cudaSuccess) {
                 throw cuda_exception(status, "error when freeing device memory");
             }
@@ -141,7 +165,14 @@ namespace nmtools::array::cuda
         template <typename array_t>
         auto create_buffer(const array_t& array)
         {
-            static_assert( (meta::is_ndarray_v<array_t> || meta::is_num_v<array_t> || meta::is_constant_index_array_v<array_t>) && !meta::is_view_v<array_t>, "unsupported array type for create_buffer" );
+            static_assert((
+                meta::is_ndarray_v<array_t>
+                || meta::is_num_v<array_t>
+                || meta::is_constant_index_array_v<array_t>
+                ) &&
+                !meta::is_view_v<array_t>
+                , "unsupported array type for create_buffer"
+            );
             if constexpr (meta::is_ndarray_v<array_t>) {
                 auto data_ptr = nmtools::data(array);
                 auto numel    = nmtools::size(array);
@@ -152,6 +183,51 @@ namespace nmtools::array::cuda
             } else {
                 return create_buffer(&array,1);
             }
+        }
+
+        template <typename array_t>
+        auto create_array(const array_t& array)
+        {
+            static_assert(
+                meta::is_ndarray_v<array_t>
+                && !meta::is_view_v<array_t>
+                , "unsupported array type for create_array"
+            );
+            const auto buffer = nmtools::data(array);
+            const auto numel  = nmtools::size(array);
+            const auto shape  = nmtools::shape(array);
+            const auto dim    = nmtools::dim(array);
+
+            using element_t = meta::get_element_type_t<array_t>;
+            using dim_t     = meta::remove_cvref_t<decltype(dim)>;
+
+            element_t* device_raw_ptr;
+            {
+                auto n_bytes = numel * sizeof(element_t);
+                auto status = cudaMalloc((void**)&device_raw_ptr,n_bytes);
+                if (status != cudaSuccess) {
+                    throw cuda_exception(status, "error when allocating device memory");
+                }
+                status = cudaMemcpy(device_raw_ptr,buffer,n_bytes,cudaMemcpyHostToDevice);
+                if (status != cudaSuccess) {
+                    throw cuda_exception(status, "error when copying memory to device");
+                }
+            }
+
+            // TODO: keep src shape traits
+            using device_shape_t = nmtools_static_vector<size_t,8>;
+            auto device_shape = device_shape_t{};
+            device_shape.resize(dim);
+            for (size_t i=0; i<dim; i++) {
+                at(device_shape,i) = at(shape,i);
+            }
+
+            using device_array_t = device_array<element_t,device_shape_t,dim_t>;
+            using device_array_ptr = std::shared_ptr<device_array_t>;
+            auto array_raw_ptr = new device_array_t{device_raw_ptr,device_shape,dim};
+
+            auto array_ptr = device_array_ptr(array_raw_ptr,array_deleter_t<device_array_t>());
+            return array_ptr;
         }
 
         template <typename attribute_t>
@@ -187,13 +263,19 @@ namespace nmtools::array::cuda
         }
 
         template <typename arg_t>
-        auto get_(arg_t arg)
+        static auto get_(arg_t arg)
         {
             if constexpr (helper::is_shared_ptr_v<arg_t>) {
                 return arg.get();
             } else {
                 return arg;
             }
+        }
+
+        template <typename data_t, typename shape_t, typename dim_t>
+        static auto get_(std::shared_ptr<device_array<data_t,shape_t,dim_t>> arg)
+        {
+            return *arg.get();
         }
 
         template <typename output_array_t, typename function_t, typename...args_t, auto...Is, template<auto...>typename sequence>
@@ -222,12 +304,12 @@ namespace nmtools::array::cuda
 
             auto status = cudaDeviceSynchronize();
             if (status != cudaSuccess) {
-                throw cuda::cuda_exception(status, "cudaDeviceSynchronize");
+                throw cuda_exception(status, "cudaDeviceSynchronize");
             }
 
             status = cudaGetLastError();
             if (status != cudaSuccess) {
-                throw cuda::cuda_exception(status, "cudaGetLastError");
+                throw cuda_exception(status, "cudaGetLastError");
             }
 
             this->copy_buffer(output_buffer,output);
@@ -246,16 +328,13 @@ namespace nmtools::array::cuda
             }();
             constexpr auto N = meta::len_v<decltype(args_pack)>;
             auto gpu_args_pack = meta::template_reduce<N>([&](auto init, auto index){
-                const auto& arg_i = *nmtools::get<index>(args_pack);
-
-                // TODO: pass actual type (constant / clipped shape) as is to device
-                auto arg_shape = nmtools::shape<false,/*disable_clipped_index*/true>(arg_i);
-                auto arg_dim   = nmtools::len(arg_shape);
-
-                auto gpu_buffer = this->create_buffer(arg_i);
-                // TODO: allow constant/clipped index shape
-                auto gpu_shape  = this->create_buffer(arg_shape);
-                return utility::tuple_cat(init,nmtools_tuple{gpu_buffer,gpu_shape,arg_dim});
+                const auto& arg_i = nmtools::get<index>(args_pack);
+                if constexpr (meta::is_num_v<decltype(arg_i)>) {
+                    return utility::tuple_append(init,arg_i);
+                } else {
+                    auto device_array = create_array(*arg_i);
+                    return utility::tuple_append(init,device_array);
+                }
             }, nmtools_tuple<>{});
 
             using sequence_t = meta::make_index_sequence<meta::len_v<decltype(gpu_args_pack)>>;
