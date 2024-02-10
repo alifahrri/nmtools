e758ea729a95f9a2b1c0a7c1a0fdb3179d4edaee
Fahri Ali Rahman <fahri@nodeflux.io>
2024-02-10 16:21:22 +0700

    remove unused graph tests

--- tests/functional/src/graph/multiply_tanh.cpp
+++ tests/functional/src/graph/multiply_tanh.cpp
@@ -19,64 +19,6 @@ namespace utils = nmtools::utils;
 
 using namespace nmtools::literals;
 
-TEST_CASE("multiply_tanh" * doctest::test_suite("functional::graph"))
-{
-    auto lhs_shape  = nmtools_array{3,4};
-    auto lhs_buffer = na::arange(12);
-    auto lhs_array  = na::reshape(lhs_buffer,lhs_shape);
-
-    auto rhs_shape  = nmtools_array{4};
-    auto rhs_buffer = na::arange(4);
-    auto rhs_array  = na::reshape(rhs_buffer,rhs_shape);
-
-    auto a = view::multiply(lhs_array,rhs_array);
-    auto b = view::tanh(a);
-    
-    auto nodes = nmtools_tuple{
-          fn::alias
-        , fn::alias
-        , fn::multiply
-        , fn::tanh
-    };
-    auto edges = nmtools_tuple{
-          nmtools_tuple<>{}
-        , nmtools_tuple<>{}
-        , nmtools_tuple{meta::ct_v<(int)-2>,meta::ct_v<(int)-1>}
-        , nmtools_tuple{meta::ct_v<(int)-1>}
-    };
-
-    auto graph = fn::get_graph(b);
-
-    NMTOOLS_ASSERT_EQUAL( meta::len_v<decltype(graph.nodes)>, meta::len_v<decltype(nodes)> );
-    NMTOOLS_ASSERT_EQUAL( meta::len_v<decltype(graph.edges)>, meta::len_v<decltype(edges)> );
-
-    {
-        constexpr auto M = meta::len_v<decltype(graph.nodes)>;
-        constexpr auto N = meta::len_v<decltype(nodes)>;
-        constexpr auto LEN = M > N ? N : M;
-        meta::template_for<LEN>([&](auto i){
-            auto expect = nm::at(nodes,i);
-            auto result = nm::at(graph.nodes,i);
-            using expect_t = decltype(expect);
-            using result_t = decltype(result);
-            NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
-        });
-    }
-
-    {
-        constexpr auto M = meta::len_v<decltype(graph.edges)>;
-        constexpr auto N = meta::len_v<decltype(edges)>;
-        constexpr auto LEN = M > N ? N : M;
-        meta::template_for<LEN>([&](auto i){
-            auto expect = nm::at(edges,i);
-            auto result = nm::at(graph.edges,i);
-            using expect_t = decltype(expect);
-            using result_t = decltype(result);
-            NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
-        });
-    }
-}
-
 TEST_CASE("multiply_tanh" * doctest::test_suite("functional::get_compute_graph"))
 {
     auto lhs_shape  = nmtools_array{3,4};
--- tests/functional/src/graph/reduce_add_divide.cpp
+++ tests/functional/src/graph/reduce_add_divide.cpp
@@ -21,64 +21,6 @@ namespace utils = nm::utils;
 
 using namespace nmtools::literals;
 
-TEST_CASE("reduce_add_divide" * doctest::test_suite("functional::graph"))
-{
-    auto lhs_shape  = nmtools_array{3,4};
-    auto lhs_buffer = na::arange(12);
-    auto lhs_array  = na::reshape(lhs_buffer,lhs_shape);
-
-    auto divisor = 3;
-
-    auto axis = 0;
-    auto a = view::reduce_add(lhs_array,axis);
-    auto b = view::divide(a,divisor);
-    
-    [[maybe_unused]]
-    auto nodes = nmtools_tuple{
-          fn::alias
-        , fn::reduce_add[a.axis][nm::None][a.initial][a.keepdims]
-        , fn::alias
-        , fn::divide
-    };
-    [[maybe_unused]]
-    auto edges = nmtools_tuple{
-          nmtools_tuple<>{}
-        , nmtools_tuple{meta::ct_v<(int)-1>}
-        , nmtools_tuple<>{}
-        , nmtools_tuple{meta::ct_v<(int)-2>,meta::ct_v<(int)-1>}
-    };
-
-    [[maybe_unused]] auto graph = fn::get_graph(b);
-    NMTOOLS_ASSERT_EQUAL( meta::len_v<decltype(graph.nodes)>, meta::len_v<decltype(nodes)> );
-    NMTOOLS_ASSERT_EQUAL( meta::len_v<decltype(graph.edges)>, meta::len_v<decltype(edges)> );
-
-    {
-        constexpr auto M = meta::len_v<decltype(graph.nodes)>;
-        constexpr auto N = meta::len_v<decltype(nodes)>;
-        constexpr auto LEN = M > N ? N : M;
-        meta::template_for<LEN>([&](auto i){
-            auto expect = nm::at(nodes,i);
-            auto result = nm::at(graph.nodes,i);
-            using expect_t = decltype(expect);
-            using result_t = decltype(result);
-            NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
-        });
-    }
-
-    {
-        constexpr auto M = meta::len_v<decltype(graph.edges)>;
-        constexpr auto N = meta::len_v<decltype(edges)>;
-        constexpr auto LEN = M > N ? N : M;
-        meta::template_for<LEN>([&](auto i){
-            auto expect = nm::at(edges,i);
-            auto result = nm::at(graph.edges,i);
-            using expect_t = decltype(expect);
-            using result_t = decltype(result);
-            NMTOOLS_STATIC_CHECK_IS_SAME( result_t, expect_t );
-        });
-    }
-}
-
 TEST_CASE("reduce_add_divide" * doctest::test_suite("functional::get_compute_graph"))
 {
     auto lhs_shape  = nmtools_array{3,4};
