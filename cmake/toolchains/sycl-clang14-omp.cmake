f41b9698f48eb391f5b463190183227bf6e5e965
Fahri Ali Rahman <fahri@nodeflux.io>
2024-02-23 22:59:10 +0700

    update adaptivecpp/hipsycl

--- cmake/toolchains/sycl-clang14-omp.cmake
+++ cmake/toolchains/sycl-clang14-omp.cmake
@@ -2,4 +2,7 @@ set(CMAKE_C_COMPILER /usr/local/bin/syclcc)
 set(CMAKE_CXX_COMPILER /usr/local/bin/syclcc)
 
 SET (CMAKE_C_COMPILER_WORKS 1)
-add_compile_options(-W -Wall -Werror -Wextra -Wno-gnu-string-literal-operator-template --hipsycl-targets=omp --hipsycl-clang=/usr/bin/clang++-14)
+add_compile_options(-W -Wall -Werror -Wextra -Wno-gnu-string-literal-operator-template
+    --acpp-targets=omp
+    --acpp-clang=/usr/bin/clang++-14
+)
--- scripts/install_opensycl.sh
+++ scripts/install_opensycl.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/bash
 
-git clone --single-branch -b v0.9.4 https://github.com/OpenSYCL/OpenSYCL
+DIR=OpenSYCL
+GIT_REPOSITORY=https://github.com/OpenSYCL/OpenSYCL
+# VERSION=v0.9.4
+# DIR=AdaptiveCpp
+# GIT_REPOSITORY=https://github.com/AdaptiveCpp/AdaptiveCpp.git
+VERSION=v23.10.0
+if [[ -d "$DIR" ]]; then
+    echo "$DIR exists. skipping cloning"
+else
+    git clone --single-branch -b ${VERSION}  ${GIT_REPOSITORY}
+fi
 
-cd OpenSYCL
+cd ${DIR}
 
 mkdir -p build
 
 cd build
 
-cmake -DLLVM_DIR=/usr/lib/llvm-14/cmake -DWITH_CUDA_BACKEND=OFF -DWITH_CUDA_NVCXX_ONLY=ON ..
+cmake \
+    -DLLVM_DIR=/usr/lib/llvm-14/cmake \
+    -DWITH_ACCELERATED_CPU=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
 make -j2 VERBOSE=1
 make install
