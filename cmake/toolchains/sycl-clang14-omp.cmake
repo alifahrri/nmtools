f17de0b4e0cb492bbdb1cb23b0efb5e66865e727
Fahri Ali Rahman <fahri@nodeflux.io>
2024-03-24 04:34:39 +0700

    try to fix build

--- cmake/toolchains/sycl-clang14-omp.cmake
+++ cmake/toolchains/sycl-clang14-omp.cmake
@@ -2,7 +2,7 @@ set(CMAKE_C_COMPILER /usr/local/bin/syclcc)
 set(CMAKE_CXX_COMPILER /usr/local/bin/syclcc)
 
 SET (CMAKE_C_COMPILER_WORKS 1)
-add_compile_options(-W -Wall -Werror -Wextra -Wno-gnu-string-literal-operator-template
+add_compile_options(-W -Wall -Werror -Wextra -Wno-gnu-string-literal-operator-template -fopenmp
     --acpp-targets=omp
     --acpp-clang=/usr/bin/clang++-14
 )
