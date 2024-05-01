27edf01edebd35b959a3dc25f60c387a338daa06
Fahri Ali Rahman <fahri@nodeflux.io>
2024-05-01 15:30:04 +0700

    refactor reshape view to use indexing view

--- include/nmtools/array/view/reshape.hpp
+++ include/nmtools/array/view/reshape.hpp
@@ -2,176 +2,124 @@
 #define NMTOOLS_ARRAY_VIEW_RESHAPE_HPP
 
 #include "nmtools/meta.hpp"
+
 #include "nmtools/array/utility/at.hpp"
 #include "nmtools/array/shape.hpp"
-#include "nmtools/array/view/decorator.hpp"
-#include "nmtools/array/view/flatten.hpp"
+#include "nmtools/array/view/indexing.hpp"
+#include "nmtools/array/as_static.hpp"
 
 #include "nmtools/array/index/reshape.hpp"
-#include "nmtools/array/index/product.hpp"
 #include "nmtools/array/index/compute_indices.hpp"
 #include "nmtools/array/index/compute_offset.hpp"
 #include "nmtools/array/index/compute_strides.hpp"
-#include "nmtools/assert.hpp"
 
-#include "nmtools/utils/isequal.hpp"
+#include "nmtools/utils/isequal/isequal.hpp"
+#include "nmtools/utils/to_string/to_string.hpp"
+#include "nmtools/utility/unwrap.hpp"
 
 namespace nmtools::view
 {
-    /**
-     * @addtogroup view
-     * Collections of functions/class for view objects
-     * @{
-     */
-    
-    /**
-     * @brief reshape view type
-     * 
-     * @tparam array_t type of array to be reshaped
-     * @tparam shape_t type of new shape
-     */
-    template <typename array_t, typename shape_t>
+    template <typename src_shape_t, typename dst_shape_t, typename src_size_t>
     struct reshape_t
+        : base_indexer_t<reshape_t<src_shape_t,dst_shape_t,src_size_t>>
     {
-        using value_type = meta::get_element_type_t<array_t>;
-        using const_reference = const value_type&;
-        // array type as required by decorator
-        using array_type = resolve_array_type_t<array_t>;
-        using shape_type = resolve_attribute_type_t<shape_t>;
-        
-        array_type array;
-        shape_type new_shape;
+        using src_shape_type = meta::fwd_attribute_t<src_shape_t>;
+        using src_size_type  = meta::fwd_attribute_t<src_size_t>;
 
-        /**
-         * @brief construct reshape view
-         * 
-         */
-        constexpr reshape_t(const array_t& array, const shape_t& shape)
-            : array(initialize(array, meta::as_value_v<array_type>))
-            , new_shape(init_attribute(shape, meta::as_value_v<shape_type>)) {}
-        
-        constexpr auto operands() const noexcept
+        // TODO: refactor index::shape_reshape so that the result can be easily deduced
+        // using dst_shape_type = meta::resolve_optype_t<
+        //     index::shape_reshape_t, src_shape_type, dst_shape_t
+        // >;
+        using dst_shape_type = decltype(index::shape_reshape(meta::declval<src_shape_type>(),meta::declval<dst_shape_t>()));
+        using dst_strides_type = meta::resolve_optype_t<
+            index::compute_strides_t, dst_shape_type
+        >;
+
+        static constexpr auto n_inputs  = 1;
+        static constexpr auto n_outputs = 1;
+
+        const src_shape_type src_shape;
+        const dst_shape_type dst_shape;
+        const src_size_type  src_size;
+        const dst_strides_type dst_strides;
+
+        constexpr reshape_t(const src_shape_t& src_shape_
+            , const dst_shape_t& dst_shape_
+            , src_size_t src_size_
+        )
+            : src_shape(fwd_attribute(src_shape_))
+            , dst_shape(index::shape_reshape(src_shape,dst_shape_))
+            , src_size(fwd_attribute(src_size_))
+            , dst_strides(index::compute_strides(dst_shape))
+        {}
+
+        template <typename indices_t>
+        constexpr auto indices(const indices_t& indices) const
         {
-            return nmtools_tuple<array_type>{array};
+            if constexpr (is_none_v<src_shape_type>) {
+                return None;
+            } else {
+                auto dst_offset  = index::compute_offset(indices,unwrap(dst_strides));
+                auto src_indices = index::compute_indices(dst_offset,src_shape);
+                return unwrap(src_indices);
+            }
         }
 
-        constexpr auto attributes() const noexcept
+        template <typename...args_t>
+        constexpr auto operator==(reshape_t<args_t...> other) const
         {
-            return nmtools_tuple{new_shape};
+            return utils::isequal(src_shape,other.src_shape)
+                && utils::isequal(dst_shape,other.dst_shape)
+            ;
         }
-        
-        /**
-         * @brief simply return size of new_shape
-         * 
-         * @return constexpr auto 
-         */
-        constexpr auto dim() const noexcept
-        {
-            return len(new_shape);
-        } // dim
-        
-        /**
-         * @brief simply return new_shape
-         * 
-         * @return constexpr decltype(auto) 
-         */
-        constexpr decltype(auto) shape() const noexcept
-        {
-            // NOTE: must normalize raw bounded array
-            return new_shape;
-        } // shape
+    };
 
-        /**
-         * @brief index transformer for reshape view
-         * 
-         * @tparam size_types
-         * @param indices variadic of integral type or single packed indices
-         * @return constexpr auto transformed indices, mapped to original array shape
-         */
-        template <typename...size_types>
-        constexpr auto index(size_types...indices) const
-        {
-            auto indices_ = pack_indices(indices...);
-            auto shape_   = detail::shape(array); // src shape
-            auto strides  = index::compute_strides(new_shape);
-            auto offset   = index::compute_offset(indices_,strides);
-            auto tf_idx   = index::compute_indices(offset,shape_);
-            return tf_idx;
-        } // index
-    }; // reshape_t
-
-    /**
-     * @brief return reshape view
-     * 
-     * @tparam array_t 
-     * @tparam shape_t 
-     * @param array array to be reshaped
-     * @param new_shape 
-     * @return constexpr auto reshape view
-     */
-    template <typename array_t, typename shape_t>
-    nmtools_view_attribute
-    constexpr auto reshape(const array_t& array, const shape_t& new_shape)
+    template <typename array_t, typename dst_shape_t>
+    constexpr auto make_reshape(const array_t& array, const dst_shape_t& dst_shape)
     {
-        // TODO: fix this on opencl kernel
-        #ifndef __OPENCL_VERSION__
-        auto new_shape_ = index::shape_reshape(shape(array),new_shape);
-        using new_shape_t = decltype(new_shape_);
-        // TODO: consider to make the return type as maybe type instead, when runtime check is needed
-        if constexpr (meta::is_maybe_v<new_shape_t>) {
-            // TODO: better error handling
-            nmtools_assert( static_cast<bool>(new_shape_)
-                , "unsupported reshape, mismatched number of elements"
-            );
-            using m_shape_t = meta::get_maybe_type_t<new_shape_t>;
-            return decorator_t<reshape_t,array_t,m_shape_t>{{array,*new_shape_}};
-        } else {
-            return decorator_t<reshape_t,array_t,new_shape_t>{{array,new_shape_}};
-        }
-        #else
-        return decorator_t<reshape_t,array_t,shape_t>{{array,new_shape}};
-        #endif
-    } // reshape
+        auto src_shape = shape<true>(array);
+        auto src_size  = size<true>(array);
+        auto indexer = reshape_t{src_shape,dst_shape,src_size};
+        return indexing(array,indexer);
+    }
 
-    /** @} */ // end group view
+    template <typename array_t, typename dst_shape_t>
+    constexpr auto reshape(const array_t& array, const dst_shape_t& dst_shape)
+    {
+        auto f = [](const auto&...args){
+            return make_reshape(args...);
+        };
+        return lift_indexing(f,array,dst_shape);
+    }
 
 } // namespace nmtools::view
 
-namespace nmtools
-{
-    /**
-     * @brief specialization fo is_ndarray for reshape view
-     *
-     * Note that this specialization bypass the specialization from decorator_t
-     * by being more specific on view_t (reshape_t in this case)
-     * 
-     * @tparam array_t referenced array of reshape view
-     * @tparam shape_t type of new shape of reshape view
-     */
-    template <typename array_t, typename shape_t>
-    struct meta::is_ndarray< view::decorator_t<view::reshape_t, array_t, shape_t> >
-    {
-        static constexpr auto value = meta::is_ndarray_v<meta::remove_cvref_t<array_t>>;
-        using value_type = decltype(value);
-    }; // is_ndarray
-} // namespace nmtools
+#if NMTOOLS_HAS_STRING
 
-namespace nmtools::meta
+namespace nmtools::utils::impl
 {
-    template <typename array_t, typename shape_t>
-    struct fixed_dim<
-        view::decorator_t<view::reshape_t, array_t, shape_t>
-    >
-    {
-        static constexpr auto value = [](){
-            if constexpr (is_fixed_index_array_v<shape_t>) {
-                return len_v<shape_t>;
-            } else {
-                return detail::Fail;
-            }
-        }();
-        using value_type = decltype(value);
+    template <typename...args_t, auto...fmt_args>
+    struct to_string_t<
+        view::reshape_t<args_t...>, fmt_string_t<fmt_args...>
+    > {
+        using result_type = nmtools_string;
+
+        auto operator()(const view::reshape_t<args_t...>& kwargs) const noexcept
+        {
+            nmtools_string str;
+            str += "reshape{";
+            str += ".src_shape=";
+            str += to_string(kwargs.src_shape,Compact);
+            str += ".dst_shape=";
+            str += to_string(kwargs.dst_shape,Compact);
+            str += ".src_size=";
+            str += to_string(kwargs.src_size,Compact);
+            str += "}";
+            return str;
+        }
     };
-} // namespace nmtools::meta
+} // namespace nmtools::utils::impl
+
+#endif // NMTOOLS_HAS_STRING
 
 #endif // NMTOOLS_ARRAY_VIEW_RESHAPE_HPP
