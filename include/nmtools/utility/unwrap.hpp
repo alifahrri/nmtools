168d4948685670bcfafdd3bef641f853b4a3052c
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2024-05-09 14:21:04 +0700

    add unwrap return type resolver

--- include/nmtools/utility/unwrap.hpp
+++ include/nmtools/utility/unwrap.hpp
@@ -5,8 +5,11 @@
 
 namespace nmtools
 {
+    struct unwrap_t {};
+
     template <typename T>
     constexpr auto unwrap(const T& t)
+        -> meta::resolve_optype_t<unwrap_t,T>
     {
         if constexpr (meta::is_maybe_v<T>) {
             return *t;
@@ -15,4 +18,24 @@ namespace nmtools
         }
     }
 }
+
+namespace nmtools::meta
+{
+    template <typename T>
+    struct resolve_optype<void,unwrap_t,T>
+    {
+        static constexpr auto vtype = [](){
+            if constexpr (is_maybe_v<T>) {
+                using type = get_maybe_type_t<T>;
+                // TODO: handle nested maybe type
+                return as_value_v<type>;
+            } else {
+                using type = T;
+                return as_value_v<type>;
+            }
+        }();
+        using type = type_t<decltype(vtype)>;
+    };
+} // namespace nmtools::meta
+
 #endif // NMTOOLS_UTILITY_UNWRAP_HPP
