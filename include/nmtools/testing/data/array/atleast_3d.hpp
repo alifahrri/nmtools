f305249d285b9c0c33bd8b13b155df89458b6f5d
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2024-05-09 14:20:36 +0700

    move atleast_{1,2,3}d constexpr tests data to separate dir

--- include/nmtools/testing/data/array/atleast_3d.hpp
+++ include/nmtools/testing/data/array/atleast_3d.hpp
@@ -4,8 +4,6 @@
 #include "nmtools/array/ndarray.hpp"
 #include "nmtools/testing/testing.hpp"
 
-#ifndef NMTOOLS_TESTING_CONSTEXPR
-
 NMTOOLS_TESTING_DECLARE_CASE(array, atleast_3d)
 {
     NMTOOLS_TESTING_DECLARE_ARGS(case1)
@@ -67,71 +65,5 @@ NMTOOLS_TESTING_DECLARE_CASE(array, atleast_3d)
         inline int result[2][1][2][1] = {{{{1},{2}}},{{{3},{4}}}};
     }
 }
-
-#else // (NMTOOLS_TESTING_CONSTEXPR)
-
-NMTOOLS_TESTING_DECLARE_CASE(array, constexpr_atleast_3d)
-{
-    NMTOOLS_TESTING_DECLARE_ARGS(case1)
-    {
-        constexpr inline int a = 1;
-    }
-    NMTOOLS_TESTING_DECLARE_EXPECT(case1)
-    {
-        constexpr inline int dim = 3;
-        constexpr inline int shape[3] = {1,1,1};
-        constexpr inline int result[1][1][1] = {{{1}}};
-    }
-
-    NMTOOLS_TESTING_DECLARE_ARGS(case2)
-    {
-        constexpr inline int a[1] = {1};
-        NMTOOLS_CONSTEXPR_CAST_ARRAYS(a);
-    }
-    NMTOOLS_TESTING_DECLARE_EXPECT(case2)
-    {
-        constexpr inline int dim = 3;
-        constexpr inline int shape[3] = {1,1,1};
-        constexpr inline int result[1][1][1] = {{{1}}};
-    }
-
-    NMTOOLS_TESTING_DECLARE_ARGS(case3)
-    {
-        constexpr inline int a[2][1] = {{1},{2}};
-        NMTOOLS_CONSTEXPR_CAST_ARRAYS(a);
-    }
-    NMTOOLS_TESTING_DECLARE_EXPECT(case3)
-    {
-        constexpr inline int dim = 3;
-        constexpr inline int shape[3] = {1,2,1};
-        constexpr inline int result[1][2][1] = {{{1},{2}}};
-    }
-
-    NMTOOLS_TESTING_DECLARE_ARGS(case4)
-    {
-        constexpr inline int a[2][1][2] = {{{1,2}},{{3,4}}};
-        NMTOOLS_CONSTEXPR_CAST_ARRAYS(a);
-    }
-    NMTOOLS_TESTING_DECLARE_EXPECT(case4)
-    {
-        constexpr inline int dim = 3;
-        constexpr inline int shape[3] = {2,1,2};
-        constexpr inline int result[2][1][2] = {{{1,2}},{{3,4}}};
-    }
-
-    NMTOOLS_TESTING_DECLARE_ARGS(case5)
-    {
-        constexpr inline int a[2][1][2][1] = {{{{1},{2}}},{{{3},{4}}}};
-        NMTOOLS_CONSTEXPR_CAST_ARRAYS(a);
-    }
-    NMTOOLS_TESTING_DECLARE_EXPECT(case5)
-    {
-        constexpr inline int dim = 4;
-        constexpr inline int shape[4] = {2,1,2,1};
-        constexpr inline int result[2][1][2][1] = {{{{1},{2}}},{{{3},{4}}}};
-    }
-}
-
-#endif // (NMTOOLS_TESTING_CONSTEXPR)
 
 #endif // NMTOOLS_TESTING_DATA_ARRAY_ATLEAST_3D_HPP
---
+++ include/nmtools/testing/data/constexpr/atleast_3d.hpp
@@ -0,0 +1,69 @@
+#ifndef NMTOOLS_TESTING_DATA_CONSTEXPR_ATLEAST_3D_HPP
+#define NMTOOLS_TESTING_DATA_CONSTEXPR_ATLEAST_3D_HPP
+
+#include "nmtools/array/ndarray.hpp"
+#include "nmtools/testing/testing.hpp"
+
+NMTOOLS_TESTING_DECLARE_CASE(array, constexpr_atleast_3d)
+{
+    NMTOOLS_TESTING_DECLARE_ARGS(case1)
+    {
+        constexpr inline int a = 1;
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case1)
+    {
+        constexpr inline int dim = 3;
+        constexpr inline int shape[3] = {1,1,1};
+        constexpr inline int result[1][1][1] = {{{1}}};
+    }
+
+    NMTOOLS_TESTING_DECLARE_ARGS(case2)
+    {
+        constexpr inline int a[1] = {1};
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(a);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case2)
+    {
+        constexpr inline int dim = 3;
+        constexpr inline int shape[3] = {1,1,1};
+        constexpr inline int result[1][1][1] = {{{1}}};
+    }
+
+    NMTOOLS_TESTING_DECLARE_ARGS(case3)
+    {
+        constexpr inline int a[2][1] = {{1},{2}};
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(a);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case3)
+    {
+        constexpr inline int dim = 3;
+        constexpr inline int shape[3] = {1,2,1};
+        constexpr inline int result[1][2][1] = {{{1},{2}}};
+    }
+
+    NMTOOLS_TESTING_DECLARE_ARGS(case4)
+    {
+        constexpr inline int a[2][1][2] = {{{1,2}},{{3,4}}};
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(a);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case4)
+    {
+        constexpr inline int dim = 3;
+        constexpr inline int shape[3] = {2,1,2};
+        constexpr inline int result[2][1][2] = {{{1,2}},{{3,4}}};
+    }
+
+    NMTOOLS_TESTING_DECLARE_ARGS(case5)
+    {
+        constexpr inline int a[2][1][2][1] = {{{{1},{2}}},{{{3},{4}}}};
+        NMTOOLS_CONSTEXPR_CAST_ARRAYS(a);
+    }
+    NMTOOLS_TESTING_DECLARE_EXPECT(case5)
+    {
+        constexpr inline int dim = 4;
+        constexpr inline int shape[4] = {2,1,2,1};
+        constexpr inline int result[2][1][2][1] = {{{{1},{2}}},{{{3},{4}}}};
+    }
+}
+
+#endif // NMTOOLS_TESTING_DATA_CONSTEXPR_ATLEAST_3D_HPP
