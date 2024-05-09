8b76df61a78d9dd383ee63d0da91e9e97d766a37
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2024-05-09 14:18:52 +0700

    fix reshape for constexpr

--- include/nmtools/array/view/reshape.hpp
+++ include/nmtools/array/view/reshape.hpp
@@ -23,17 +23,20 @@ namespace nmtools::view
     struct reshape_t
         : base_indexer_t<reshape_t<src_shape_t,dst_shape_t,src_size_t>>
     {
-        using src_shape_type = meta::fwd_attribute_t<src_shape_t>;
-        using src_size_type  = meta::fwd_attribute_t<src_size_t>;
+        using src_shape_type = meta::fwd_attribute_t<decltype(unwrap(meta::declval<src_shape_t>()))>;
+        using src_size_type  = meta::fwd_attribute_t<decltype(unwrap(meta::declval<src_size_t>()))>;
 
         // TODO: refactor index::shape_reshape so that the result can be easily deduced
         // using dst_shape_type = meta::resolve_optype_t<
         //     index::shape_reshape_t, src_shape_type, dst_shape_t
         // >;
-        using dst_shape_type = decltype(index::shape_reshape(meta::declval<src_shape_type>(),meta::declval<dst_shape_t>()));
-        using dst_strides_type = meta::resolve_optype_t<
+        // avoid storing maybe type here since it is not constexpr-friendly (at least for maybe static_vector)
+        using dst_shape_type = decltype(unwrap(index::shape_reshape(meta::declval<src_shape_type>(),meta::declval<dst_shape_t>())));
+        using dst_strides_type = meta::resolve_optype_t<unwrap_t,meta::resolve_optype_t<
             index::compute_strides_t, dst_shape_type
-        >;
+        >>;
+        // reshape doesn't change the number of elements
+        using dst_size_type  = src_size_type;
 
         static constexpr auto n_inputs  = 1;
         static constexpr auto n_outputs = 1;
@@ -41,16 +44,18 @@ namespace nmtools::view
         const src_shape_type src_shape;
         const dst_shape_type dst_shape;
         const src_size_type  src_size;
+        const dst_size_type  dst_size;
         const dst_strides_type dst_strides;
 
         constexpr reshape_t(const src_shape_t& src_shape_
             , const dst_shape_t& dst_shape_
             , src_size_t src_size_
         )
-            : src_shape(fwd_attribute(src_shape_))
-            , dst_shape(index::shape_reshape(src_shape,dst_shape_))
-            , src_size(fwd_attribute(src_size_))
-            , dst_strides(index::compute_strides(dst_shape))
+            : src_shape(fwd_attribute(unwrap(src_shape_)))
+            , dst_shape(unwrap(index::shape_reshape(src_shape,dst_shape_)))
+            , src_size(fwd_attribute(unwrap(src_size_)))
+            , dst_size(fwd_attribute(unwrap(src_size_)))
+            , dst_strides(unwrap(index::compute_strides(dst_shape)))
         {}
 
         template <typename indices_t>
@@ -75,11 +80,29 @@ namespace nmtools::view
     };
 
     template <typename array_t, typename dst_shape_t>
+    constexpr auto make_reshaper(const array_t& array, const dst_shape_t& dst_shape)
+    {
+        static_assert( !meta::is_maybe_v<array_t> && !meta::is_maybe_v<dst_shape_t>, "invalid type for array" );
+        auto src_shape   = shape<true>(array);
+        auto src_size    = size<true>(array);
+        auto m_dst_shape = index::shape_reshape(src_shape,dst_shape);
+        if constexpr (meta::is_maybe_v<decltype(m_dst_shape)>) {
+            using result_t = reshape_t<decltype(src_shape),dst_shape_t,decltype(src_size)>;
+            using return_t = nmtools_maybe<result_t>;
+            if (static_cast<bool>(m_dst_shape)) {
+                return return_t{result_t{src_shape,dst_shape,src_size}};
+            } else {
+                return return_t{meta::Nothing};
+            }
+        } else {
+            return reshape_t{src_shape,dst_shape,src_size};
+        }
+    }
+
+    template <typename array_t, typename dst_shape_t>
     constexpr auto make_reshape(const array_t& array, const dst_shape_t& dst_shape)
     {
-        auto src_shape = shape<true>(array);
-        auto src_size  = size<true>(array);
-        auto indexer = reshape_t{src_shape,dst_shape,src_size};
+        auto indexer = make_reshaper(array,dst_shape);
         return indexing(array,indexer);
     }
 
