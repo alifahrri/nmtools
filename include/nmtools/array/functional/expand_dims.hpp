e5f5f1514b7145be621bea3b42754f5fc7b019a6
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2024-05-18 18:03:11 +0700

    refactor expand_dims using reshape

--- include/nmtools/array/functional/expand_dims.hpp
+++ include/nmtools/array/functional/expand_dims.hpp
@@ -2,6 +2,7 @@
 #define NMTOOLS_ARRAY_FUNCTIONAL_EXPAND_DIMS_HPP
 
 #include "nmtools/array/functional/functor.hpp"
+#include "nmtools/array/functional/indexing.hpp"
 #include "nmtools/array/view/expand_dims.hpp"
 
 namespace nmtools::functional
@@ -19,24 +20,6 @@ namespace nmtools::functional
     }
 
     constexpr inline auto expand_dims = functor_t(unary_fmap_t<fun::expand_dims_t>{});
-
-    template <typename...args_t>
-    struct get_function_t<
-        view::decorator_t<
-            view::expand_dims_t, args_t...
-        >
-    > {
-        using view_type = view::decorator_t<
-            view::expand_dims_t, args_t...
-        >;
-
-        view_type view;
-
-        constexpr auto operator()() const noexcept
-        {
-            return expand_dims[view.axis];
-        }
-    };
 } // namespace nmtools::functional
 
 
