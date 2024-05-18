4ca3b1897969ee0dffc869e3ea728bb7919e438c
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2024-05-18 18:09:36 +0700

    utility update: add fwd_mutable_operand, isclose for nested either/maybe, to string

--- include/nmtools/utils/apply_to_string.hpp
+++ include/nmtools/utils/apply_to_string.hpp
@@ -1,39 +1,7 @@
 #ifndef NMTOOLS_UTILS_APPLY_TO_STRING_HPP
 #define NMTOOLS_UTILS_APPLY_TO_STRING_HPP
 
+// TODO: remove this file
 #include "nmtools/utils/to_string.hpp"
-#include "nmtools/meta.hpp"
-
-namespace nmtools::utils
-{
-    /**
-     * @brief applicative to_string
-     * 
-     * @tparam T 
-     * @param array 
-     * @return nmtools_string 
-     */
-    template <typename T>
-    inline auto apply_to_string(const T& array) -> nmtools_string
-    {
-        if constexpr (meta::is_list_v<T>) {
-            auto str = nmtools_string();
-            for (size_t i=0; i<len(array); i++) {
-                str += nmtools::utils::to_string(at(array,i));
-                str += ";\n";
-            }
-            return str;
-        } else if constexpr (meta::is_tuple_v<T>) {
-            auto str = nmtools_string();
-            meta::template_for<meta::len_v<T>>([&](auto i){
-                str += nmtools::utils::to_string(at(array,i));
-                str += ";\n";
-            });
-            return str;
-        } else {
-            return nmtools::utils::to_string(array);
-        }
-    }
-} // namespace nmtools::utils
 
 #endif // NMTOOLS_UTILS_APPLY_TO_STRING_HPP
