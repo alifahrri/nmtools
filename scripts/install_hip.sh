86a34c499090ac923e866e06e0db3182025298fa
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2024-06-02 16:11:08 +0700

    add install hip script

---
+++ scripts/install_hip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE=amdgpu-install_6.1.60101-1_all.deb
+URL=https://repo.radeon.com/amdgpu-install/6.1.1/ubuntu/jammy/amdgpu-install_6.1.60101-1_all.deb
+
+apt update
+if [[ -f "$FILE" ]]; then
+    echo "$FILE exists skipping downloads"
+else
+    wget ${URL}
+fi
+apt install -y ./${FILE}
+amdgpu-install --usecase=hiplibsdk --no-dkms
