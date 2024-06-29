5dcfa50e82dd0ab08f03eea8819204811bcfe3de
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2024-06-29 18:01:31 +0700

    restructure compute_graph & functor files & deps

--- include/nmtools/array/functional/functor.hpp
+++ include/nmtools/array/functional/functor.hpp
@@ -4,6 +4,7 @@
 #include "nmtools/meta.hpp"
 #include "nmtools/array/view/decorator.hpp"
 #include "nmtools/array/view/alias.hpp"
+#include "nmtools/array/view/broadcast_to.hpp"
 #include "nmtools/utility/ct_map.hpp"
 #include "nmtools/utility/ct_digraph.hpp"
 #include "nmtools/utility/tuple_slice.hpp"
@@ -12,6 +13,7 @@
 #include "nmtools/utility/fwd.hpp"
 #include "nmtools/utility/unwrap.hpp"
 
+// TODO: remove, use utility/fwd
 namespace nmtools::meta
 {
     using view::resolve_attribute_type;
@@ -758,9 +760,6 @@ namespace nmtools::functional
     template <typename view_t>
     struct get_function_operands_t;
 
-    template <typename view_t>
-    struct get_function_composition_t;
-
     template <typename view_type>
     constexpr auto get_function_operands(const view_type& view)
     {
@@ -776,22 +775,6 @@ namespace nmtools::functional
             return get_op();
         }
     } // get_function_operands
-
-    template <typename view_type>
-    constexpr auto get_function_composition(const view_type& view)
-    {
-        if constexpr (meta::is_maybe_v<view_type>) {
-            using result_type = meta::remove_cvref_t<decltype(get_function_composition(*view))>;
-            using return_type = nmtools_maybe<result_type>;
-            return (view
-                ? return_type{get_function_composition(*view)}
-                : return_type{meta::Nothing}
-            );
-        } else {
-            auto get_fn = get_function_composition_t<view_type>{view};
-            return get_fn();
-        }
-    } // get_function_composition
 
     template <template<typename...>typename view_t, typename...args_t>
     struct get_function_operands_t<
@@ -829,43 +812,6 @@ namespace nmtools::functional
             return all_operands;
         }
     }; // get_function_operands_t
-
-    template <template<typename...>typename view_t, typename...args_t>
-    struct get_function_composition_t<
-        view::decorator_t<view_t,args_t...>
-    > {
-        using view_type = view::decorator_t<view_t,args_t...>;
-        view_type view;
-
-        constexpr auto operator()() const noexcept
-        {
-            auto function = get_function(view);
-            auto operands = get_operands(view);
-            static_assert( !meta::is_fail_v<decltype(operands)> );
-            constexpr auto N = view_type::arity;
-            auto composition = meta::template_reduce<N>([&](auto init, auto index){
-                constexpr auto I = decltype(index)::value;
-                constexpr auto r_index = meta::ct_v<(N-1)-I>;
-                const auto& operand = nmtools::at(operands,r_index);
-                using operand_t = meta::remove_pointer_t<meta::remove_cvref_t<decltype(operand)>>;
-                static_assert(
-                    (meta::is_view_v<operand_t>)
-                    || ((meta::is_num_v<operand_t> || meta::is_ndarray_v<operand_t>)
-                        && !meta::is_view_v<operand_t>
-                    )
-                );
-                if constexpr (meta::is_view_v<operand_t>) {
-                    auto sub_composition = get_function_composition(operand);
-                    return init * sub_composition;
-                } else if constexpr ((meta::is_num_v<operand_t> || meta::is_ndarray_v<operand_t>)
-                    && !meta::is_view_v<operand_t>
-                ) {
-                    return init;
-                }
-            }, function);
-            return composition;
-        }
-    }; // get_function_composition_t
 
     template <typename function_t, typename operands_t>
     nmtools_func_attribute
@@ -909,116 +855,40 @@ namespace nmtools::functional
 
     constexpr inline auto alias = functor_t{unary_fmap_t<fun::alias_t>{}};
 
-    template <typename view_t>
-    struct get_compute_graph_t;
+    template <typename T>
+    struct is_broadcast_view : meta::false_type {};
 
-    template <typename view_t>
-    constexpr auto get_compute_graph(const view_t& view)
-    {
-        if constexpr (meta::is_maybe_v<view_t>) {
-            using view_type = meta::get_maybe_type_t<view_t>;
-            using result_type = decltype(get_compute_graph(meta::declval<view_type>()));
-            using return_type = nmtools_maybe<result_type>;
-            if (static_cast<bool>(view)) {
-                return return_type{get_compute_graph(*view)};
-            } else {
-                return return_type{meta::Nothing};
-            }
-        } else {
-            using view_type = view_t;
-            auto get_graph = get_compute_graph_t<view_type>{view};
-            return get_graph();
-        }
-    } // get_graph
+    template <typename array_t, typename...broadcast_args_t>
+    struct is_broadcast_view<
+        view::decorator_t<view::indexing_t, array_t, view::broadcast_to_t<broadcast_args_t...>>
+    > : meta::true_type {};
 
-    template <typename functor_t, typename operands_t>
-    struct node_t
-    {
-        // TODO: assert functor_t is functor or functor composition
-        // TODO: assert operands_t is tuple of integral constant
-        // TODO: record out_shape
+    template <typename T>
+    constexpr inline auto is_broadcast_view_v = is_broadcast_view<T>::value;
+} // namespace nmtools::functional
 
-        using functor_type = functor_t;
-        using operands_type = operands_t;
+#if 0
+namespace nmtools::functional
+{
+    template <typename...args_t>
+    struct get_function_t<
+        view::decorator_t<
+            view::alias_t, args_t...
+        >
+    > {
+        using view_type = view::decorator_t<
+            view::alias_t, args_t...
+        >;
 
-        functor_type functor;
-        operands_type operands;
-    };
-
-    template <typename functor_t, typename operands_t>
-    node_t(const functor_t&, const operands_t&) -> node_t<functor_t,operands_t>;
-
-    template <typename nodes_t=nmtools_tuple<>, typename edges_t=nmtools_tuple<>, typename node_data_t=nmtools_tuple<>>
-    struct compute_graph_t : utility::ct_digraph<nodes_t,edges_t,node_data_t>
-    {
-        // TODO: validate that node_data_t is tuple of node_t
-        using base_type = utility::ct_digraph<nodes_t,edges_t,node_data_t>;
-    };
-
-    template <template <typename...> typename view_t, typename...args_t>
-    struct get_compute_graph_t<view::decorator_t<view_t,args_t...>>
-    {
-        using view_type = view::decorator_t<view_t,args_t...>;
         view_type view;
 
         constexpr auto operator()() const noexcept
         {
-            auto operands = get_operands(view);
-            constexpr auto operand_ids = view_type::operands_ids;
-
-            constexpr auto N = meta::len_v<decltype(operands)>;
-            auto sub_graph = meta::template_reduce<N>([&](auto graph, auto index){
-
-                constexpr auto I = decltype(index)::value;
-                const auto& operand = nmtools::get<I>(operands);
-                using operand_t = meta::remove_cvref_pointer_t<decltype(operand)>;
-                static_assert(
-                    meta::is_pointer_v<operand_t>
-                    || meta::is_num_v<operand_t>
-                    || meta::is_view_v<operand_t>
-                    , "expect operand to be pointer, number or view for get_compute_graph"
-                );
-                if constexpr (meta::is_same_view_v<view::alias_t,operand_t>) {
-                    constexpr auto NODE_ID = typename operand_t::id_type{};
-                    // static_assert( meta::is_pointer_v<decltype(operand)> );
-                    return graph.add_node(NODE_ID,operand.array);
-                } else if constexpr (meta::is_view_v<operand_t>) {
-                    auto sub_graph = get_compute_graph(operand);
-                    auto sub_keys = sub_graph.digraph.keys();
-                    constexpr auto N_SUB = meta::len_v<decltype(sub_keys)>;
-                    // MERGE Graph
-                    auto result_graph = meta::template_reduce<N_SUB>([&](auto g, auto index){
-                        auto node_id = nmtools::get<decltype(index)::value>(sub_keys);
-                        auto node = sub_graph.nodes(node_id);
-                        auto out_edges = sub_graph.out_edges(node_id);
-                        constexpr auto N_OUT = meta::len_v<decltype(out_edges)>;
-                        return meta::template_reduce<N_OUT>([&](auto g, auto out_idx){
-                            auto out_edge = nmtools::get<decltype(out_idx)::value>(out_edges);
-                            return g.add_edge(node_id,out_edge);
-                        }, g.add_node(node_id,node));
-                    }, graph);
-                    return result_graph;
-                } else /* if constexpr (meta::is_ndarray_v<operand_t>) */ {
-                    constexpr auto N_NODES = decltype(graph.size())::value;
-                    constexpr auto NODE_ID = meta::ct_v<N_NODES>;
-                    return graph.add_node(NODE_ID,operand);
-                }
-            }, compute_graph_t<>());
-
-            // TODO: support aliasing
-            constexpr auto node_id = typename view_type::id_type{};
-            auto functor = get_function(view);
-
-            auto graph = sub_graph
-                .add_node(node_id,node_t{functor,operand_ids})
-            ;
-            return meta::template_reduce<N>([&](auto graph, auto index){
-                auto operand_id = nmtools::get<decltype(index)::value>(operand_ids);
-                return graph.add_edge(operand_id,node_id);
-            }, graph);
+            return alias[view.id];
         }
     };
 } // namespace nmtools::functional
+#endif
 
 namespace nmtools::utils
 {
@@ -1144,3 +1014,6 @@ namespace nmtools::utils
 }
 
 #endif // NMTOOLS_ARRAY_FUNCTIONAL_FUNCTOR_HPP
+
+#include "nmtools/array/functional/compute_graph.hpp"
+#include "nmtools/array/functional/function_composition.hpp"
