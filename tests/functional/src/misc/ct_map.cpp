6ea821062286015c13d535f396bc5cf7881aad33
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2024-12-23 08:52:32 +0700

    update tests

--- tests/functional/src/misc/ct_map.cpp
+++ tests/functional/src/misc/ct_map.cpp
@@ -5,6 +5,8 @@ namespace nm = nmtools;
 namespace meta = nm::meta;
 namespace utility = nm::utility;
 
+using namespace nmtools::literals;
+
 TEST_CASE("ct_map")
 {
     auto a = nmtools_array{1,2,3,4};
@@ -100,4 +102,65 @@ TEST_CASE("ct_map")
     NMTOOLS_ASSERT_EQUAL( new_map.size(), 2 );
     NMTOOLS_ASSERT_EQUAL( new_map.at(meta::ct_v<0>), a );
     NMTOOLS_ASSERT_EQUAL( new_map.at(meta::ct_v<1>), c );
+}
+
+TEST_CASE("ct_map")
+{
+    auto a = nmtools_tuple{1_ct,3_ct};
+    auto b = nmtools_tuple{3_ct};
+
+    auto map = utility::ct_map()
+        .insert(meta::ct_v<0>,a)
+        .insert(meta::ct_v<1>,b)
+        .insert(meta::ct_v<3>,nmtools_tuple{})
+    ;
+    NMTOOLS_ASSERT_EQUAL( map.size(), 3 );
+    NMTOOLS_ASSERT_EQUAL( map.at(meta::ct_v<0>), a );
+    NMTOOLS_ASSERT_EQUAL( map.at(meta::ct_v<1>), b );
+    static_assert( meta::is_same_v<decltype(map.at(meta::ct_v<3>)),decltype(nmtools_tuple{})> );
+    
+    auto map_1 = map.erase(3_ct);
+    NMTOOLS_ASSERT_EQUAL( map_1.size(), 2 );
+    NMTOOLS_ASSERT_EQUAL( map_1.at(meta::ct_v<0>), a );
+    NMTOOLS_ASSERT_EQUAL( map_1.at(meta::ct_v<1>), b );
+
+    auto map_2 = map_1.erase(1_ct);
+    NMTOOLS_ASSERT_EQUAL( map_2.size(), 1 );
+    NMTOOLS_ASSERT_EQUAL( map_2.at(meta::ct_v<0>), a );
+
+    auto map_3 = map_2.insert(4_ct,nmtools::None);
+    NMTOOLS_ASSERT_EQUAL( map_3.size(), 2 );
+    NMTOOLS_ASSERT_EQUAL( map_3.at(0_ct), a );
+    NMTOOLS_ASSERT_EQUAL( map_3.at(4_ct), nmtools::None );
+}
+
+TEST_CASE("tuple_filter")
+{
+    auto init = nmtools_tuple{0_ct,1_ct,2_ct,3_ct};
+    auto filter = [](auto element){
+        constexpr auto ELEMENT = decltype(element)::value;
+        if constexpr ((ELEMENT % 2) == 0) {
+            return meta::false_type{};
+        } else {
+            return meta::true_type{};
+        }
+    };
+    auto result = utility::tuple_filter(init,filter);
+    static_assert( meta::is_same_v<decltype(result),decltype(nmtools_tuple{1_ct,3_ct})>);
+}
+
+TEST_CASE("tuple_index")
+{
+    auto init = nmtools_tuple{0_ct,2_ct,3_ct,5_ct};
+    auto result = utility::tuple_index(init,3_ct);
+    auto expect = 2_ct;
+    NMTOOLS_ASSERT_EQUAL( result, expect );
+}
+
+TEST_CASE("tuple_update")
+{
+    auto init = nmtools_tuple{0_ct,2_ct,3_ct,5_ct};
+    auto result = utility::tuple_update(init,1_ct,7_ct);
+    auto expect = nmtools_tuple{0_ct,7_ct,3_ct,5_ct};
+    NMTOOLS_ASSERT_EQUAL( result, expect );
 }
