6c33216f0d0a2945efcd6fd130aaab5a3166c120
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2025-01-18 16:31:39 +0700

    update compact to_string to avoid null

--- tests/functional/src/graph/layer_norm.cpp
+++ tests/functional/src/graph/layer_norm.cpp
@@ -17,8 +17,7 @@ namespace utils = nmtools::utils;
 using namespace nmtools::literals;
 using nm::unwrap;
 
-// TODO: fix runtime crash (failed unwrap?)
-TEST_CASE("layer_norm" * doctest::test_suite("functional::get_compute_graph") * doctest::skip())
+TEST_CASE("layer_norm" * doctest::test_suite("functional::get_compute_graph"))
 {
     auto input_shape = nmtools_array{1,2,5,5};
     auto gamma_shape = nmtools_array{2,5,5};
@@ -28,10 +27,8 @@ TEST_CASE("layer_norm" * doctest::test_suite("functional::get_compute_graph") * 
     auto gamma = na::reshape(na::arange(ix::product(gamma_shape)),gamma_shape);
     auto beta  = na::reshape(na::arange(ix::product(beta_shape)),beta_shape);
 
-    auto axis = nmtools_array{-3,-2,-1};
-
     // TODO: support maybe type for layer_norm
-    auto layer_norm = view::layer_norm(unwrap(input),unwrap(gamma),unwrap(beta),axis);
+    auto layer_norm = view::layer_norm(unwrap(input),unwrap(gamma),unwrap(beta));
     auto graph = fn::get_compute_graph(unwrap(layer_norm));
 
     CHECK_MESSAGE( true, utils::to_string(graph,utils::Graphviz));
