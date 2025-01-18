cd1ff50f9667ea0f78082c97a71daf72c421d3f3
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2025-01-18 16:17:45 +0700

    restructure view includes

---
+++ include/nmtools/array/view/ufunc-core.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "nmtools/array/view/ufunc-core/outer.hpp"
+#include "nmtools/array/view/ufunc-core/detail.hpp"
+#include "nmtools/array/view/ufunc-core/ufunc.hpp"
+#include "nmtools/array/view/ufunc-core/accumulate.hpp"
+#include "nmtools/array/view/ufunc-core/reduce.hpp"
