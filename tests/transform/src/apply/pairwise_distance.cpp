7306c3946611ac21334f3fd28986745fb097939b
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2025-02-09 15:31:02 +0700

    add linearize & apply test for pairwise_distance

---
+++ tests/transform/src/apply/pairwise_distance.cpp
@@ -0,0 +1,38 @@
+#include "nmtools/core/transform/linearize.hpp"
+#include "nmtools/array/pairwise_distance.hpp"
+#include "nmtools/array/random.hpp"
+#include "nmtools/testing/doctest.hpp"
+
+namespace nm = nmtools;
+namespace fn = nmtools::functional;
+namespace meta = nmtools::meta;
+namespace view = nmtools::view;
+namespace na = nmtools::array;
+namespace ix = nmtools::index;
+namespace utl = nmtools::utl;
+namespace utils = nmtools::utils;
+namespace utility = nmtools::utility;
+
+using namespace nmtools::literals;
+
+using nmtools_array, nmtools_tuple, nmtools::unwrap;
+
+TEST_CASE("apply_graph(pairwise_distance)" * doctest::test_suite("transform"))
+{
+    auto gen   = na::random_engine();
+    auto dtype = nm::float32;
+
+    auto lhs = na::random(array{3,4},dtype,gen);
+    auto rhs = na::random(array{2,1,4},dtype,gen);
+
+    auto res = view::pairwise_distance(lhs,rhs);
+
+    auto graph = fn::linearize(fn::get_compute_graph(unwrap(res)));
+    auto operands = fn::get_operands(graph);
+    auto function = fn::get_function(graph);
+    auto functor  = function.functor;
+
+    auto result = fn::apply(functor,operands);
+    auto expected = res;
+    NMTOOLS_ASSERT_CLOSE( result, expected );
+}
