cf5367da88471a6d1d8dd4412fcc48e142e04f85
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2025-02-15 00:08:53 +0700

    add hip & sycl stddev tests

---
+++ tests/hip/array/stddev.cpp
@@ -0,0 +1,55 @@
+#include "nmtools/evaluator/hip.hpp"
+#include "nmtools/array/stddev.hpp"
+#include "nmtools/array/random.hpp"
+#include "nmtools/testing/doctest.hpp"
+
+namespace nm = nmtools;
+namespace na = nmtools::array;
+namespace fn = nmtools::functional;
+namespace meta = nm::meta;
+namespace view = nmtools::view;
+
+// TODO: fix to accept maybe type
+using nmtools_array, nmtools_tuple, nmtools::unwrap;
+
+#define HIP_TEST(fn,...) \
+{ \
+    auto expect = na::fn(__VA_ARGS__); \
+    auto result = na::fn(__VA_ARGS__,na::hip::default_context()); \
+    NMTOOLS_ASSERT_EQUAL( nm::shape(result), nm::shape(expect) ); \
+    NMTOOLS_ASSERT_CLOSE( result, expect ); \
+}
+
+#define HIP_LINEAR_TEST(fun,...) \
+{ \
+    auto ctx = na::hip::default_context(); \
+    auto expect = na::fun(__VA_ARGS__); \
+    auto view   = view::fun(__VA_ARGS__); \
+    auto result = ctx->eval(view); \
+    NMTOOLS_ASSERT_EQUAL( nm::shape(result), nm::shape(expect) ); \
+    NMTOOLS_ASSERT_CLOSE( result, expect ); \
+}
+
+#if 1
+TEST_CASE("stddev(case1)" * doctest::test_suite("array::stddev") * doctest::skip())
+{
+    auto gen   = na::random_engine();
+    auto dtype = nm::float32;
+
+    auto input = na::random(array{1,5,2,2},dtype,gen);
+
+    HIP_TEST(stddev,input,-1,nm::None,0,nm::False);
+}
+#endif
+
+#if 1
+TEST_CASE("stddev(case1b)" * doctest::test_suite("array::stddev"))
+{
+    auto gen   = na::random_engine();
+    auto dtype = nm::float32;
+
+    auto input = na::random(array{1,5,2,2},dtype,gen);
+
+    HIP_LINEAR_TEST(stddev,input,-1,nm::None,0,nm::False);
+}
+#endif
