8abddf336269cfabcb0410f2b27c73029265883f
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2025-02-23 03:21:08 +0700

    try to fix ci

--- cmake/toolchains/sycl-clang14-cuda.cmake
+++ cmake/toolchains/sycl-clang14-cuda.cmake
@@ -3,18 +3,26 @@ set(CMAKE_CXX_COMPILER /usr/local/bin/acpp)
 
 SET (CMAKE_C_COMPILER_WORKS 1)
 SET (CMAKE_CXX_COMPILER_WORKS 1)
+if (ACPP_CLANG_VERSION)
+    message("Using clang version for acpp: '${ACPP_CLANG_VERSION}'")
+else ()
+    set(ACPP_CLANG_VERSION 18)
+endif ()
+if (ACPP_TARGETS)
+    message("Using acpp-targets='${ACPP_TARGETS}'")
+else ()
+    set(ACPP_TARGETS "cuda:sm_86")
+endif ()
 add_compile_options(-W -Wall -Wextra -Werror
     -Wno-gnu-string-literal-operator-template
     -Wno-unknown-cuda-version
     -Wno-deprecated-declarations # acpp error (v24.10.0) with -Werror
-    --acpp-targets="cuda:sm_86"
-    # TODO: parametrize this
-    --acpp-clang=/usr/bin/clang++
+    --acpp-targets="${ACPP_TARGETS}"
+    --acpp-clang=/usr/bin/clang++-${ACPP_CLANG_VERSION}
 )
 add_link_options(
-    --acpp-targets="cuda:sm_86"
-    # TODO: parametrize this
-    --acpp-clang=/usr/bin/clang++
+    --acpp-targets="${ACPP_TARGETS}"
+    --acpp-clang=/usr/bin/clang++-${ACPP_CLANG_VERSION}
 )
 set(NMTOOLS_TEST_CUDA_PATH "/usr/local/cuda/" CACHE STRING "manually set cuda path")
 link_directories("${NMTOOLS_TEST_CUDA_PATH}/lib64")
--- cmake/toolchains/sycl-clang14-omp.cmake
+++ cmake/toolchains/sycl-clang14-omp.cmake
@@ -2,13 +2,25 @@ set(CMAKE_C_COMPILER /usr/local/bin/acpp)
 set(CMAKE_CXX_COMPILER /usr/local/bin/acpp)
 
 SET (CMAKE_C_COMPILER_WORKS 1)
+
+if (ACPP_CLANG_VERSION)
+    message("Using clang version for acpp: '${ACPP_CLANG_VERSION}'")
+else ()
+    set(ACPP_CLANG_VERSION 18)
+endif ()
+
+if (ACPP_TARGETS)
+    message("Using acpp-targets='${ACPP_TARGETS}'")
+else ()
+    set(ACPP_TARGETS "omp")
+endif ()
+
 add_compile_options(-W -Wall -Werror -Wextra -Wno-gnu-string-literal-operator-template -fopenmp
     -Wno-deprecated-declarations # acpp error (v24.10.0) with -Werror
-    --acpp-targets=omp
-    # TODO: make clang version configurable?
-    --acpp-clang=/usr/bin/clang++-18
+    --acpp-targets="${ACPP_TARGETS}"
+    --acpp-clang=/usr/bin/clang++-${ACPP_CLANG_VERSION}
 )
 add_link_options(
-    --acpp-targets=omp
-    --acpp-clang=/usr/bin/clang++-18
+    --acpp-targets="${ACPP_TARGETS}"
+    --acpp-clang=/usr/bin/clang++-${ACPP_CLANG_VERSION}
 )
