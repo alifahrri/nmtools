e620c21b4ee71b2718f17a12d0bbd8026a00e946
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2025-04-15 09:57:11 +0700

    add where args for view::reduce_left_shift
    Signed-off-by: Fahri Ali Rahman <ali.rahman.fahri@gmail.com>

--- include/nmtools/array/ufuncs/left_shift.hpp
+++ include/nmtools/array/ufuncs/left_shift.hpp
@@ -41,8 +41,8 @@ namespace nmtools::view
         return broadcast_binary_ufunc(left_shift_t<>{},a,b);
     } // left_shift
 
-    template <typename left_t, typename axis_t, typename dtype_t, typename initial_t, typename keepdims_t>
-    constexpr auto reduce_left_shift(const left_t& a, const axis_t& axis, dtype_t dtype, initial_t initial, keepdims_t keepdims)
+    template <typename left_t, typename axis_t, typename dtype_t=none_t, typename initial_t=none_t, typename keepdims_t=meta::false_type, typename where_t=none_t>
+    constexpr auto reduce_left_shift(const left_t& a, const axis_t& axis, dtype_t dtype=dtype_t{}, initial_t initial=initial_t{}, keepdims_t keepdims=keepdims_t{}, const where_t& where=where_t{})
     {
         static_assert( meta::is_integral_v<axis_t>
             , "reduce_left_shift only support single axis with integral type"
@@ -52,47 +52,15 @@ namespace nmtools::view
         // to match the signature of reduce_t
         using res_t = get_dtype_t<dtype_t>;
         using op_t  = left_shift_t<none_t,none_t,res_t>;
-        return reduce(op_t{},a,axis,dtype,initial,keepdims);
+        return reduce(op_t{},a,axis,dtype,initial,keepdims,where);
     } // reduce_left_shift
 
-    template <typename left_t, typename axis_t, typename dtype_t, typename initial_t>
-    constexpr auto reduce_left_shift(const left_t& a, const axis_t& axis, dtype_t dtype, initial_t initial)
-    {
-        static_assert( meta::is_integral_v<axis_t>
-            , "reduce_left_shift only support single axis with integral type"
-        );
-        // note that reduce_t takes reference, to support multiple axis
-        // while reduce_left_shift only support single axis, here axis is const ref
-        // to match the signature of reduce_t
-        using res_t = get_dtype_t<dtype_t>;
-        using op_t  = left_shift_t<none_t,none_t,res_t>;
-        return reduce(op_t{},a,axis,dtype,initial);
-    } // reduce_left_shift
-
-    template <typename left_t, typename axis_t, typename dtype_t>
-    constexpr auto reduce_left_shift(const left_t& a, const axis_t& axis, dtype_t dtype)
-    {
-        return reduce_left_shift(a,axis,dtype,None);
-    } // reduce_left_shift
-
-    template <typename left_t, typename axis_t>
-    constexpr auto reduce_left_shift(const left_t& a, const axis_t& axis)
-    {
-        return reduce_left_shift(a,axis,None,None);
-    } // reduce_left_shift
-
-    template <typename left_t, typename axis_t, typename dtype_t>
-    auto accumulate_left_shift(const left_t& a, const axis_t& axis, dtype_t dtype)
+    template <typename left_t, typename axis_t, typename dtype_t=none_t>
+    auto accumulate_left_shift(const left_t& a, const axis_t& axis, dtype_t dtype=dtype_t{})
     {
         using res_t = get_dtype_t<dtype_t>;
         using op_t  = left_shift_t<none_t,none_t,res_t>;
         return accumulate(op_t{},a,axis,dtype);
-    } // accumulate_left_shift
-
-    template <typename left_t, typename axis_t>
-    auto accumulate_left_shift(const left_t& a, const axis_t& axis)
-    {
-        return accumulate_left_shift(a,axis,None);
     } // accumulate_left_shift
 
     template <typename left_t, typename right_t, typename dtype_t=none_t>
