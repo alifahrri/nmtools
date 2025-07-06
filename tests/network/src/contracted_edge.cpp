0ae7fc35b6261c5c665970063cc2a4b029ce237b
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2025-07-06 15:27:57 +0700

    update tests
    Signed-off-by: Fahri Ali Rahman <ali.rahman.fahri@gmail.com>

--- tests/network/src/contracted_edge.cpp
+++ tests/network/src/contracted_edge.cpp
@@ -1,4 +1,5 @@
 #include "nmtools/network/contracted_edge.hpp"
+#include "nmtools/network/digraph.hpp"
 #include "nmtools/testing/data/network/contracted_edge.hpp"
 #include "nmtools/testing/doctest.hpp"
 
@@ -20,6 +21,24 @@ SUBCASE(#case_name) \
     NMTOOLS_ASSERT_EQUAL( result, expect::contracted_list ); \
 }
 
+#define DIGRAPH_CONTRACTED_EDGE_SUBCASE(case_name, adj_list, src_node_ids, src_node_attributes, edge_id, self_loop) \
+SUBCASE(#case_name) \
+{ \
+    NMTOOLS_TESTING_USE_CASE( network, contracted_edge, case_name ); \
+    auto digraph = nmtools::network::digraph(adj_list,src_node_ids,src_node_attributes); \
+    auto edge = nmtools::network::map_ids(edge_id,src_node_ids); \
+    auto contracted = nmtools::network::contracted_edge(digraph,edge,nmtools::None,self_loop); \
+    const auto& result_list = nmtools::unwrap(contracted).adjacency_list; \
+    const auto& result_ids  = nmtools::unwrap(contracted).node_ids; \
+    NMTOOLS_ASSERT_EQUAL( result_list, expect::contracted_list); \
+    NMTOOLS_ASSERT_EQUAL( result_ids, expect::dst_node_ids); \
+    auto graphviz = nmtools::utils::to_string(nmtools::unwrap(contracted),nmtools::utils::Graphviz); \
+    CHECK_MESSAGE( true, graphviz ); \
+    std::ofstream outputFile; \
+    outputFile.open("graphviz.dot", std::ios::out | std::ios::trunc); \
+    outputFile << graphviz << std::endl; \
+}
+
 TEST_CASE("contracted_edge(case1)" * doctest::test_suite("network::contracted_edge"))
 {
     CONTRACTED_EDGE_SUBCASE( case1, list_a_sv, edge_to_contract, self_loops_flag )
@@ -36,6 +55,16 @@ TEST_CASE("constexpr_contracted_edge(case1)" * doctest::test_suite("network::con
 {
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case1, list_a_sv_ct, edge_to_contract, self_loops_flag )
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case1, list_sv_sv_ct, edge_to_contract, self_loops_flag )
+}
+
+TEST_CASE("digraph_contracted_edge(case1)" * doctest::test_suite("network::contracted_edge"))
+{
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case1, list_a_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case1, list_a_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case1, list_sv_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case1, list_sv_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case1, list_v_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case1, list_v_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
 }
 
 #if 1
@@ -58,6 +87,16 @@ TEST_CASE("constexpr_contracted_edge(case2)" * doctest::test_suite("network::con
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case2, list_sv_sv_ct, edge_to_contract, self_loops_flag )
 }
 
+TEST_CASE("digraph_contracted_edge(case2)" * doctest::test_suite("network::contracted_edge"))
+{
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case2, list_a_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case2, list_a_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case2, list_sv_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case2, list_sv_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case2, list_v_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case2, list_v_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+}
+
 #if 1
 TEST_CASE("contracted_edge(case3)" * doctest::test_suite("network::contracted_edge"))
 {
@@ -76,6 +115,16 @@ TEST_CASE("constexpr_contracted_edge(case3)" * doctest::test_suite("network::con
 {
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case3, list_a_sv_ct, edge_to_contract, self_loops_flag )
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case3, list_sv_sv_ct, edge_to_contract, self_loops_flag )
+}
+
+TEST_CASE("digraph_contracted_edge(case3)" * doctest::test_suite("network::contracted_edge"))
+{
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case3, list_a_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case3, list_a_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case3, list_sv_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case3, list_sv_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case3, list_v_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case3, list_v_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
 }
 
 #if 1
@@ -98,6 +147,16 @@ TEST_CASE("constexpr_contracted_edge(case4)" * doctest::test_suite("network::con
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case4, list_sv_sv_ct, edge_to_contract, self_loops_flag )
 }
 
+TEST_CASE("digraph_contracted_edge(case4)" * doctest::test_suite("network::contracted_edge"))
+{
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case4, list_a_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case4, list_a_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case4, list_sv_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case4, list_sv_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case4, list_v_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case4, list_v_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+}
+
 #if 1
 TEST_CASE("contracted_edge(case5)" * doctest::test_suite("network::contracted_edge"))
 {
@@ -118,6 +177,16 @@ TEST_CASE("constexpr_contracted_edge(case5)" * doctest::test_suite("network::con
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case5, list_sv_sv_ct, edge_to_contract, self_loops_flag )
 }
 
+TEST_CASE("digraph_contracted_edge(case5)" * doctest::test_suite("network::contracted_edge"))
+{
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case5, list_a_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case5, list_a_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case5, list_sv_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case5, list_sv_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case5, list_v_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case5, list_v_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+}
+
 TEST_CASE("contracted_edge(case6)" * doctest::test_suite("network::contracted_edge"))
 {
     CONTRACTED_EDGE_SUBCASE( case6, list_a_sv, edge_to_contract, self_loops_flag )
@@ -134,6 +203,16 @@ TEST_CASE("constexpr_contracted_edge(case6)" * doctest::test_suite("network::con
 {
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case6, list_a_sv_ct, edge_to_contract, self_loops_flag )
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case6, list_sv_sv_ct, edge_to_contract, self_loops_flag )
+}
+
+TEST_CASE("digraph_contracted_edge(case6)" * doctest::test_suite("network::contracted_edge"))
+{
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case6, list_a_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case6, list_a_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case6, list_sv_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case6, list_sv_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case6, list_v_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case6, list_v_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
 }
 
 #if 1
@@ -154,4 +233,14 @@ TEST_CASE("constexpr_contracted_edge(case7)" * doctest::test_suite("network::con
 {
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case7, list_a_sv_ct, edge_to_contract, self_loops_flag )
     CONSTEXPR_CONTRACTED_EDGE_SUBCASE( case7, list_sv_sv_ct, edge_to_contract, self_loops_flag )
+}
+
+TEST_CASE("digraph_contracted_edge(case7)" * doctest::test_suite("network::contracted_edge"))
+{
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case7, list_a_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case7, list_a_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case7, list_sv_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case7, list_sv_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case7, list_v_sv, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
+    DIGRAPH_CONTRACTED_EDGE_SUBCASE( case7, list_v_v, src_node_ids, node_attributes, edge_to_contract, self_loops_flag )
 }
