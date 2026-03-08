e1a8858ed8f9b8bdac9bf15c9eb9c968d7ee458f
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2026-03-08 21:43:37 +0700

    try to fix gcc 9 bit cast
    Signed-off-by: Fahri Ali Rahman <ali.rahman.fahri@gmail.com>

--- cmake/toolchains/gcc9-werror.cmake
+++ cmake/toolchains/gcc9-werror.cmake
@@ -2,4 +2,4 @@ set(CMAKE_C_COMPILER gcc-9)
 set(CMAKE_CXX_COMPILER g++-9)
 
 # ignore strict-aliasing warning in gcc9
-add_compile_options(-W -Wall -Werror -Wextra -Wno-error=unknown-pragmas -Wno-error=bool-compare -wno-error=strict-aliasing)
+add_compile_options(-W -Wall -Werror -Wextra -Wno-error=unknown-pragmas -Wno-error=bool-compare -Wno-error=strict-aliasing)
