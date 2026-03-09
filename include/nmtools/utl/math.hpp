329f6e6d3ec2edae82a376253c9f63df0026d53e
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2026-03-10 00:09:23 +0700

    try to fix clang constexpr error
    Signed-off-by: Fahri Ali Rahman <ali.rahman.fahri@gmail.com>

--- include/nmtools/utl/math.hpp
+++ include/nmtools/utl/math.hpp
@@ -48,8 +48,15 @@ namespace nmtools::utl
     #define nmtools_bit_cast_constexpr constexpr
     #define nmtools_has_constexpr_bit_cast (1)
 
-    #ifdef __GNUC__
+    // in clang, at least 20.x, __GNUC__ is defined to 4 when the gcc version is 14
+    // so skip in clang
+    #if defined(__GNUC__) && !defined(__clang__)
     #if __GNUC__ < 10
+    // #define STR(x) #x
+    // #define XSTR(x) STR(x)
+    // #pragma message("Compiling with GCC Version: " XSTR(__GNUC__) "." XSTR(__GNUC_MINOR__) "." XSTR(__GNUC_PATCHLEVEL__))
+    // #pragma message "disabling some utl math constexpr"
+
     #undef nmtools_bit_cast
     #undef nmtools_bit_cast_constexpr
     #undef nmtools_has_constexpr_bit_cast
