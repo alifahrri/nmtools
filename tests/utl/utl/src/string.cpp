948239c9558d02246bdab3f4c80544b49a63cc29
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2026-09-09 12:23:16 +0700

    fix riscv compile
    Signed-off-by: Fahri Ali Rahman <ali.rahman.fahri@gmail.com>

--- tests/utl/utl/src/string.cpp
+++ tests/utl/utl/src/string.cpp
@@ -80,7 +80,7 @@ TEST_CASE("string" * doctest::test_suite("utl"))
         for (nm_size_t i=0; i<str.size() && i<expected.size(); i++) {
             CHECK( str[i] == expected[i] );
         }
-        #if defined(__arm__) && defined(__ARM_ARCH)
+        #if defined(__arm__) || defined(__riscv)
         auto is_same = (str == "hello");
         CHECK( is_same );
         #else
@@ -98,7 +98,7 @@ TEST_CASE("string" * doctest::test_suite("utl"))
         for (nm_size_t i=0; i<str.size() && i<expected.size(); i++) {
             CHECK( str[i] == expected[i] );
         }
-        #if defined(__arm__) && defined(__ARM_ARCH)
+        #if defined(__arm__) || defined(__riscv)
         auto is_same = (str == "hello");
         CHECK( is_same );
         #else
@@ -121,7 +121,7 @@ TEST_CASE("string" * doctest::test_suite("utl"))
         for (nm_size_t i=0; i<str.size() && i<expected.size(); i++) {
             CHECK( str[i] == expected[i] );
         }
-        #if defined(__arm__) && defined(__ARM_ARCH)
+        #if defined(__arm__) || defined(__riscv)
         auto is_same = (str == "hello");
         CHECK( is_same );
         #else
@@ -144,7 +144,7 @@ TEST_CASE("string" * doctest::test_suite("utl"))
         for (nm_size_t i=0; i<str.size() && i<expected.size(); i++) {
             CHECK( str[i] == expected[i] );
         }
-        #if defined(__arm__) && defined(__ARM_ARCH)
+        #if defined(__arm__) || defined(__riscv)
         auto is_same = (str == "hello");
         CHECK( is_same );
         #else
